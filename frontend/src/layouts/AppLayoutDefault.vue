acf56adc3f9208912ac045f5e2ccadc94f472d10
Pau4ok <pavel_mal99@mail.ru>
2024-03-17 19:01:58 +0300

    Adding Icons to project

--- frontend/src/layouts/AppLayoutDefault.vue
+++ frontend/src/layouts/AppLayoutDefault.vue
@@ -1,9 +1,11 @@
 <template>
   <BasicHeader />
+  <IconsDeclaration/>
 </template>
 
 <script setup lang="ts">
 import BasicHeader from '@/components/base/BasicHeader.vue'
+import IconsDeclaration from "@/components/base/IconsDeclaration.vue";
 
 </script>
 
