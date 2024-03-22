c1d2de975a60a3e745ddf9f35ba91d0a7a247ce8
Pau4ok <pavel_mal99@mail.ru>
2024-03-22 11:02:30 +0300

    Adding loader spinner

---
+++ frontend/src/components/UI/Loader.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="loader__wrapper">
+    <div class="loader"></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Loader',
+}
+</script>
+
+<style scoped>
+  .loader__wrapper {
+    z-index: 9999;
+    background-color: #1b222d;
+    opacity: 0.8;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+  }
+  .loader,
+  .loader:before,
+  .loader:after {
+    background: #fff;
+    -webkit-animation: load1 1s infinite ease-in-out;
+    animation: load1 1s infinite ease-in-out;
+    width: 1em;
+    height: 4em;
+  }
+  .loader {
+    color: #fff;
+    text-indent: -9999em;
+    margin: 88px auto;
+    position: relative;
+    font-size: 11px;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation-delay: -.16s;
+    animation-delay: -.16s;
+  }
+  .loader:before,
+  .loader:after {
+    position: absolute;
+    top: 0;
+    content: '';
+  }
+  .loader:before {
+    left: -1.5em;
+    -webkit-animation-delay: -.32s;
+    animation-delay: -.32s;
+  }
+  .loader:after {
+    left: 1.5em;
+  }
+  @-webkit-keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
+  @keyframes load1 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 0;
+      height: 4em;
+    }
+    40% {
+      box-shadow: 0 -2em;
+      height: 5em;
+    }
+  }
+</style>
