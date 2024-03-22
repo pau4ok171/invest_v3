c0dd1adae6f96b61a40acfc0dc93fb727ad2c05d
Pau4ok <pavel_mal99@mail.ru>
2024-03-22 11:02:18 +0300

    Adding drop down menu for nav user

---
+++ frontend/src/components/base/header/navigation/NavUserDropDown.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="account-access__dropdown">
+    <nav class="account-access__navigation">
+      <div class="account-access-navigation__inner">
+        <ul class="account-access__list">
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Plans & Pricing</a></li>
+          <li><a href="#">Notifications</a></li>
+          <li><a href="#">Help Center</a></li>
+          <li><button @click="logout">Logout</button></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import store from "@/store";
+
+
+export default {
+  name: 'NavUserDropDown',
+  methods: {
+    logout() {
+      axios.defaults.headers.common['Authorization'] = ''
+
+      localStorage.removeItem('token')
+      localStorage.removeItem('username')
+      localStorage.removeItem('userid')
+
+      store.commit('removeToken')
+    },
+    clickHandler(event) {
+      if (event.target.parentNode.parentNode !== this.$el.parentNode) {
+        this.$emit('closeDropDownMenu')
+        document.removeEventListener('click', this.clickHandler)
+      }
+    }
+  },
+  mounted() {
+    document.addEventListener('click', this.clickHandler)
+  },
+
+
+
+
+}
+</script>
+
+<style scoped>
+  .account-access__dropdown {
+    transition: visibility 0s .6s, opacity .6s ease;
+  }
+  .account-access__navigation {
+    width: 200px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+  .account-access-navigation__inner {
+    position: absolute;
+    top: -6px;
+    left: 0;
+    width: 100%;
+    background-color: #262e3a;
+    border-radius: 4px;
+  }
+  .account-access__list {
+    list-style: none;
+  }
+  .account-access__list li {
+    font-size: 1.4rem;
+    line-height: 1.5;
+  }
+  .account-access__list li:last-child {
+    border-top: 1px solid #3c434e;
+  }
+  .account-access__list li > * {
+    display: block;
+    text-align: left;
+    color: #fff;
+    width: 100%;
+    padding: 12px 16px;
+    transition: all .3s ease 0s;
+  }
+  .account-access__list li:first-child > * {
+    border-top-right-radius: 4px;
+    border-top-left-radius: 4px;
+  }
+  .account-access__list li:last-child > * {
+    border-bottom-right-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+  .account-access__list li > *:hover {
+    background-color: rgb(35, 148, 223);
+  }
+</style>
