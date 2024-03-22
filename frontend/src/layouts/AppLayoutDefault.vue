c13623f811c2768519e5ba3f288c114c340e850e
Pau4ok <pavel_mal99@mail.ru>
2024-03-22 10:57:03 +0300

    Initialisation store to hold auth credentials, and adding loader spinner

--- frontend/src/layouts/AppLayoutDefault.vue
+++ frontend/src/layouts/AppLayoutDefault.vue
@@ -1,9 +1,28 @@
 <template>
-  <BasicHeader />
+  <BasicHeader :class="{'screen__blured' : store.state.authModalMenuIsActive}"/>
+  <AuthModalMenu v-if="store.state.authModalMenuIsActive"/>
+  <Loader v-if="store.state.isLoading"/>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import BasicHeader from '@/components/base/header/BasicHeader.vue'
+import AuthModalMenu from "@/components/base/auth/AuthModalMenu.vue";
+import store from "@/store";
+import Loader from "@/components/UI/Loader.vue";
+
+export default {
+  name: 'AppLayoutDefault',
+  components: {
+    Loader,
+    BasicHeader,
+    AuthModalMenu
+  },
+  computed: {
+    store () {
+      return store
+    },
+  },
+}
 </script>
 
 <style scoped>
