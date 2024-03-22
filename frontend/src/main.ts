5e1b8c1153f721f4038f5cc3efc7f50829b56bd2
Pau4ok <pavel_mal99@mail.ru>
2024-03-22 10:57:55 +0300

    Initialisation store to hold auth credentials, and adding loader spinner

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from "axios";
+import store from './store'
+import axios from "axios"
 
 axios.defaults.baseURL = 'http://localhost:8000'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router, store, axios)
 
 app.mount('#app')
