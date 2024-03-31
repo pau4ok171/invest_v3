e8035cdd75e7da090341f664e8210798ab88a151
Pau4ok <pavel_mal99@mail.ru>
2024-03-31 20:28:34 +0300

    Adding icons for company list

---
+++ frontend/src/components/icons/TileModeIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24">
+	  <path fill-rule="evenodd" clip-rule="evenodd" d="M11 4H4V11H11V4ZM11 13H4V20H11V13ZM13 4H20V11H13V4ZM20 13H13V20H20V13Z"></path>
+  </svg>
+</template>
