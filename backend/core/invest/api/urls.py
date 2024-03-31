ae69cab014813171b198657a4d0a4de00d6afefd
Pau4ok <pavel_mal99@mail.ru>
2024-03-31 20:22:46 +0300

    Adding urls for company list data

--- backend/core/invest/api/urls.py
+++ backend/core/invest/api/urls.py
@@ -4,6 +4,11 @@ from invest.api import views
 app_name = 'invest_api'
 
 urlpatterns = [
+    path('companies/', views.CompanyListView.as_view()),
+    path('companies/<slug:country_slug>/<slug:sector_slug>', views.CompanyListView.as_view()),
+    path('filters/', views.CompanyListFilters.as_view()),
+    path('filters/sector/<slug:country_slug>', views.CompanyListSectorFilters.as_view()),
+    path('toggle_to_watchlist/', views.WatchlistedCompanyAPIView.as_view()),
     path('search/', views.SearchList.as_view()),
     path('validate_username/', views.validate_username),
     path('price_chart/<slug:company_slug>/', views.PriceChartList.as_view(), name='price_chart'),
