6c1eb8438a533d4f79a520cee69cdcd1653134eb
Pau4ok <pavel_mal99@mail.ru>
2024-03-31 20:38:43 +0300

    Adding drop down menu component

--- frontend/src/components/UI/DropDownMenu.vue
+++ frontend/src/components/UI/DropDownMenu.vue
@@ -1,15 +1,37 @@
 <template>
-<div>
-  <slot/>
-</div>
+  <div>
+    <slot/>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'DropDownMenu',
+  methods: {
+    clickHandler(event) {
+      if (event.target.parentNode !== this.$el.parentNode) {
+        this.$emit('closeDropDownMenu')
+        document.removeEventListener('click', this.clickHandler)
+      }
+    }
+  },
+  mounted() {
+    document.addEventListener('click', this.clickHandler)
+  },
 }
 </script>
 
-<style scoped>
-
+<style>
+  .dropdown-menu::-webkit-scrollbar {
+      width: 10px;
+    }
+    .dropdown-menu::-webkit-scrollbar-track {
+      -webkit-box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, .2);
+      background-color: #f9f9fd;
+      border-radius: 10px;
+    }
+    .dropdown-menu::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background: linear-gradient(180deg, #00c6fb, #005bea);
+    }
 </style>
