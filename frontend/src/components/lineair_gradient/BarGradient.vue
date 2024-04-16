1ffeac6f0a56daf1275d549f6d282c56d66b441c
Pau4ok <pavel_mal99@mail.ru>
2024-04-16 13:05:13 +0300

    Adding UI elements for charts

---
+++ frontend/src/components/lineair_gradient/BarGradient.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "BarGradient"
+})
+</script>
+
+<template>
+  <svg height="0" width="0">
+    <linearGradient id="BarGradient">
+      <stop offset="0%" stop-color="rgb(21, 27, 36)" stop-opacity="0.5"></stop>
+      <stop offset="100%" stop-color="rgb(21, 27, 36)" stop-opacity="0.8"></stop>
+    </linearGradient>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/lineair_gradient/ChartGradient.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg height="0" width="0">
+    <linearGradient id="Chart_01_Gradient_01" x1="0" x2="0" y1="0" y2="1">
+      <stop offset="0%" stop-color="#2394df" stop-opacity="0.6"></stop>
+      <stop offset="100%" stop-color="#2394df" stop-opacity="0.2"></stop>
+    </linearGradient>
+  </svg>
+</template>
