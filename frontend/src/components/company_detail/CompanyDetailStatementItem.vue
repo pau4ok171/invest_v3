6edc2c396d5053144e9f43b7b84e0d4c0a517aeb
Pau4ok <pavel_mal99@mail.ru>
2024-04-16 10:15:25 +0300

    Creating CompanyDetailStatementItem component

---
+++ frontend/src/components/company_detail/CompanyDetailStatementItem.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "CompanyDetailStatementItem",
+  props: {
+    statement: {
+      type: Object,
+      required: true,
+    }
+  }
+})
+</script>
+
+<template>
+  <blockquote class="detail-statement-item">
+    <p class="detail-statement-item__text">
+      <span class="text--error">{{  statement.title }}:</span>
+      <span> {{ statement.description }}</span>
+    </p>
+  </blockquote>
+</template>
+
+<style scoped>
+  .detail-statement-item {
+    position: relative;
+    margin-top: 8px;
+    text-align: left;
+  }
+  .detail-statement-item__text {
+    font-size: 1.6rem;
+    line-height: 1.5;
+  }
+</style>
