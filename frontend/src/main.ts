3e0b26991ea3edf269eddfcb1267357fae791d2b
Pau4ok <pavel_mal99@mail.ru>
2024-04-16 13:16:46 +0300

    Adding highcharts to project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from "axios"
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from "axios";
 import Vintersection from "@/directives/Vintersection";
+import HighchartsVue from 'highcharts-vue';
+import Highcharts from 'highcharts'
+import StockChart from 'highcharts/modules/stock';
+import highchartsMore from 'highcharts/highcharts-more'
+import loadSolidGauge from 'highcharts/modules/solid-gauge'
 
 axios.defaults.baseURL = 'http://localhost:8000'
 
@@ -11,8 +16,21 @@ const app = createApp(App)
 
 app.directive('intersection', Vintersection)
 
+highchartsMore(Highcharts)
+loadSolidGauge(Highcharts)
+StockChart(Highcharts)
+
+Highcharts.Templating.helpers.abs = value => Math.abs(value);
+
+Highcharts.setOptions({
+    lang: {
+        rangeSelectorZoom: ''
+    }
+});
+
 app
     .use(router)
     .use(store)
     .use(axios)
+    .use(HighchartsVue, {tagName: 'charts'})
     .mount('#app')
