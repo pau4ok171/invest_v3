660809bb7883cfcba3d7195dacc3daf2809715d1
Pau4ok <pavel_mal99@mail.ru>
2024-04-16 09:54:41 +0300

    Creating CompanyDetailContent

---
+++ frontend/src/components/company_detail/CompanyDetailContent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="detail__content">
+
+    <CompanyDetailSidebar
+      :company
+    />
+
+    <CompanyDetailContentList
+      :company
+    />
+
+  </div>
+</template>
+
+<script lang="ts">
+import CompanyDetailSidebar from "@/components/company_detail/CompanyDetailSidebar.vue";
+import CompanyDetailContentList from "@/components/company_detail/CompanyDetailContentList.vue";
+
+export default {
+  name: 'CompanyDetailContent',
+  components: {CompanyDetailContentList, CompanyDetailSidebar},
+  props: {
+    company: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style>
+  .detail__content {
+    display: flex;
+    flex-flow: wrap;
+    justify-content: left;
+  }
+  .detail__content-item {
+    position: relative;
+    flex: 0 0 100%;
+    max-width: 100%;
+    width: 100%;
+    min-height: 0;
+  }
+  .detail__point-list {
+    margin-top: 3.2rem;
+  }
+</style>
