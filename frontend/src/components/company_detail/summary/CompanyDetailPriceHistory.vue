17eea2db19d69603c5de0828a491c7b797a4baec
Pau4ok <pavel_mal99@mail.ru>
2024-04-16 10:10:14 +0300

    Creating CompanyDetailPriceHistory

---
+++ frontend/src/components/company_detail/summary/CompanyDetailPriceHistory.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import PriceChart from "@/components/charts/PriceChart.vue";
+import DetailSectionTitle from "@/components/UI/text/DetailSectionTitle.vue";
+
+export default defineComponent({
+  name: "CompanyDetailPriceHistory",
+  components: {DetailSectionTitle, PriceChart}
+})
+</script>
+
+<template>
+   <DetailSectionTitle>Price History & Performance</DetailSectionTitle>
+
+    <PriceChart/>
+</template>
+
+<style scoped>
+
+</style>
