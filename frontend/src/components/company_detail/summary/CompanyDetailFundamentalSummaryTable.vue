bb099cc11ae2898dbeb60e35d56966715e31afd4
Pau4ok <pavel_mal99@mail.ru>
2024-04-16 10:08:36 +0300

    Creating table on CompanyDetailFundamentalSummary

---
+++ frontend/src/components/company_detail/summary/CompanyDetailFundamentalSummaryTable.vue
@@ -0,0 +1,101 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import ArrowDownIcon from "@/components/icons/ArrowDownIcon.vue";
+import QuestionText from "@/components/UI/text/QuestionText.vue";
+import QuestionLink from "@/components/UI/text/QuestionLink.vue";
+
+export default defineComponent({
+  name: "CompanyDetailFundamentalSummaryTable",
+  components: {QuestionLink, QuestionText, ArrowDownIcon}
+})
+</script>
+
+<template>
+  <div class="detail-fundamental-summary-table">
+
+    <div class="detail-fundamental-summary-table__in-row-item-box">
+      <div class="detail-fundamental-summary-table__in-row-item">
+        <h3 class="detail-fundamental-summary-table__title">2.6x</h3>
+        <span class="detail-fundamental-summary-table__text">P/E Ratio</span>
+      </div>
+      <div class="detail-fundamental-summary-table__in-row-item detail-fundamental-summary-table__flex-end">
+        <div>
+          <h3 class="detail-fundamental-summary-table__subtitle">0.5x</h3>
+          <span class="detail-fundamental-summary-table__subtext">P/B Ratio</span>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <QuestionText>Is SBER overvalued?</QuestionText>
+      <QuestionLink>See Fair Value and valuation analysis</QuestionLink>
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+  .detail-fundamental-summary-table {
+    margin-top: 50px;
+  }
+  .detail-fundamental-summary-table__in-row-item-box {
+    display: flex;
+    flex-flow: wrap;
+    justify-content: left;
+  }
+  .detail-fundamental-summary-table__in-row-item {
+    padding-bottom: 24px;
+    margin-top: 32px;
+    max-width: 100%;
+    flex: 0 0 50%;
+  }
+  .detail-fundamental-summary-table__flex-end {
+    display: flex;
+    align-items: flex-end;
+  }
+  .detail-fundamental-summary-table__title {
+    font-size: 3.6rem;
+    line-height: 1.25;
+    font-weight: 500;
+    margin-bottom: 2px;
+  }
+  .detail-fundamental-summary-table__subtitle {
+    font-size: 2rem;
+    line-height: 1.25;
+    font-weight: 500;
+    margin-bottom: 2px;
+  }
+  .detail-fundamental-summary-table__text {
+    font-size: 1.6rem;
+    line-height: 1.5;
+    font-weight: 500;
+    color: rgba(255, 255, 255, .7);
+  }
+  .detail-fundamental-summary-table__subtext {
+    font-size: 1.4rem;
+    line-height: 1.5;
+    font-weight: 500;
+    color: rgba(255, 255, 255, .7);
+  }
+  .detail-fundamental-summary-table__question {
+    font-size: 1.6rem;
+    line-height: 1.5;
+    font-weight: 500;
+    color: rgba(255, 255, 255, .5);
+    margin-top: 35px;
+  }
+  .detail-fundamental-summary-table__link {
+    display: flex;
+    font-size: 1.6rem;
+    line-height: 1.5;
+    font-weight: 500;
+    color: var(--blue);
+    text-decoration: underline;
+  }
+  .detail-fundamental-summary-table__link svg {
+    transform: rotate(-90deg);
+    margin-left: -6px;
+    fill: var(--blue);
+  }
+</style>
