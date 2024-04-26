6f1a4a8fec81e835be922d7029427b05364c93bc
Pau4ok <pavel_mal99@mail.ru>
2024-04-26 12:54:58 +0300

    Adding ordering by -updated in Note Model

--- backend/core/notes/models.py
+++ backend/core/notes/models.py
@@ -8,3 +8,6 @@ class Note(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated']
