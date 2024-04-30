249d09005c13a43ce0f2474452f186c20a0395b5
Pau4ok <pavel_mal99@mail.ru>
2024-04-30 21:45:02 +0300

    Creating BaseModalMenuContainer

---
+++ frontend/src/components/UI/modal_menu/BaseModalMenuContainer.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "BaseModalMenuContainer",
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    close() {
+      this.isOpen=false
+    },
+  },
+})
+</script>
+
+<template>
+<div>
+
+  <div @click="isOpen=true">
+    <slot name="button"/>
+  </div>
+
+  <teleport v-if="isOpen" to="body">
+    <div>
+      <slot :close="close" name="menu"/>
+    </div>
+  </teleport>
+
+</div>
+</template>
