b738a665dca7c8504c8881f6cd091adedd149336
Pau4ok <pavel_mal99@mail.ru>
2024-05-05 13:24:48 +0300

    Refactoring URL routes

--- backend/core/config/urls.py
+++ backend/core/config/urls.py
@@ -1,25 +1,8 @@
-"""
-URL configuration for finargo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from invest import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,10 +10,18 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls.authtoken')),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('register/', views.RegistrationView.as_view(), name='register'),
+    # Invest
     path('invest/', include('invest.urls', namespace='invest')),
-    path('watchlist/', include('watchlist.urls', namespace='watchlist')),
-    path('portfolio/', include('portfolio.urls', namespace='portfolio')),
+    path('api/v1/invest/', include('invest.api.urls', namespace='invest_api')),
+    # Notes
     path('notes/', include('notes.urls', namespace='notes')),
+    path('api/v1/notes/', include('notes.api.urls', namespace='notes_api')),
+    # Portfolio
+    path('portfolio/', include('portfolio.urls', namespace='portfolio')),
+    path('api/v1/portfolio/', include('portfolio.api.urls', namespace='portfolio_api')),
+    # Watchlist
+    path('watchlist/', include('watchlist.urls', namespace='watchlist')),
+    path('api/v1/watchlist/', include('watchlist.api.urls', namespace='watchlist_api')),
     path('', include('django.contrib.auth.urls')),
     path('', views.HomePageView.as_view(), name='home'),
 ]
