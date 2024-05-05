85e84f480bafcbfa94e673b6bb14fcf1cacf323a
Pau4ok <pavel_mal99@mail.ru>
2024-05-05 14:13:57 +0300

    Refactoring URL routes

--- backend/core/invest/api/urls.py
+++ backend/core/invest/api/urls.py
@@ -4,14 +4,14 @@ from invest.api import views
 app_name = 'invest_api'
 
 urlpatterns = [
+    # Objects
     path('companies/', views.CompanyListView.as_view()),
-    path('companies/<slug:country_slug>/<slug:sector_slug>', views.CompanyListView.as_view()),
-    path('company/<slug:company_slug>', views.CompanyDetailAPIView.as_view()),
+    path('companies/<slug:company_slug>', views.CompanyDetailAPIView.as_view()),
     path('filters/', views.CompanyListFilters.as_view()),
-    path('filters/sector/<slug:country_slug>', views.CompanyListSectorFilters.as_view()),
+    path('filters/sector/<country_slug>', views.CompanyListSectorFilters.as_view()),
+    path('price_data/<slug:company_slug>/', views.PriceChartList.as_view()),
     path('toggle_to_watchlist/', views.WatchlistedCompanyAPIView.as_view()),
-    path('search/', views.SearchList.as_view()),
+    # Functions
+    path('search_query/', views.search_query),
     path('validate_username/', views.validate_username),
-    path('price_chart/<slug:company_slug>/', views.PriceChartList.as_view(), name='price_chart'),
-    path('price_chart_new/<slug:company_slug>/', views.PriceChartList.as_view(), name='price_chart_new'),
 ]
