d76c0b5aaa870055df37d249e74c7a67dcc6f357
Pau4ok <pavel_mal99@mail.ru>
2024-05-14 17:46:41 +0300

    Introducing static data for passing statement checks of Value, Future, Past, Health, Dividends areas Initialising classes for Future, Past, Health, Dividends areas checks

--- backend/core/statements/services/analysis.py
+++ backend/core/statements/services/analysis.py
@@ -8,6 +8,38 @@ from statements.checks.value import (
     PriceToEarningsVsIndustryCheck,
     PriceToEarningsVsFairRatioCheck,
 )
+from statements.checks.future import (
+    EarningsVsSavingRateCheck,
+    EarningsVsMarketCheck,
+    HighGrowthEarningsCheck,
+    RevenueVsMarketCheck,
+    HighGrowthRevenueCheck,
+    FutureROECheck,
+)
+from statements.checks.past import (
+    QualityEarningsCheck,
+    GrowingProfitMarginCheck,
+    EarningsTrendCheck,
+    AcceleratingGrowthCheck,
+    EarningsVsIndustryCheck,
+    HighROECheck,
+)
+from statements.checks.health import (
+    ShortTermLiabilitiesCheck,
+    LongTermLiabilitiesCheck,
+    DebtLevelCheck,
+    ReducingDebtCheck,
+    DebtCoverageCheck,
+    InterestCoverageCheck,
+)
+from statements.checks.dividend import (
+    NotableDividendCheck,
+    HighDividendCheck,
+    StableDividendCheck,
+    GrowingDividendCheck,
+    EarningsCoverageCheck,
+    CashFlowCoverageCheck,
+)
 
 
 def main(companies: list[Company] = None):
@@ -20,12 +52,42 @@ def main(companies: list[Company] = None):
         fields = get_fields(company)
 
         objects.append([
+            # INIT
             HasFinancialDataCheck(fields),
+            # VALUE
             BelowFairValueCheck(fields),
             SignificantlyBelowFairValueCheck(fields),
             PriceToEarningsVsPeersCheck(fields),
             PriceToEarningsVsIndustryCheck(fields),
             PriceToEarningsVsFairRatioCheck(fields),
+            # FUTURE
+            EarningsVsSavingRateCheck(fields),
+            EarningsVsMarketCheck(fields),
+            HighGrowthEarningsCheck(fields),
+            RevenueVsMarketCheck(fields),
+            HighGrowthRevenueCheck(fields),
+            FutureROECheck(fields),
+            # PAST
+            QualityEarningsCheck(fields),
+            GrowingProfitMarginCheck(fields),
+            EarningsTrendCheck(fields),
+            AcceleratingGrowthCheck(fields),
+            EarningsVsIndustryCheck(fields),
+            HighROECheck(fields),
+            # HEALTH
+            ShortTermLiabilitiesCheck(fields),
+            LongTermLiabilitiesCheck(fields),
+            DebtLevelCheck(fields),
+            ReducingDebtCheck(fields),
+            DebtCoverageCheck(fields),
+            InterestCoverageCheck(fields),
+            # DIVIDEND
+            NotableDividendCheck(fields),
+            HighDividendCheck(fields),
+            StableDividendCheck(fields),
+            GrowingDividendCheck(fields),
+            EarningsCoverageCheck(fields),
+            CashFlowCoverageCheck(fields),
         ])
 
     return objects
@@ -33,12 +95,52 @@ def main(companies: list[Company] = None):
 
 def get_fields(company_object):
     return Fields(
+        # Common
         company_object=company_object,
         slug=company_object.slug,
-        current_price=company_object.candles.order_by('-time')[0].close,
-        fair_price=329.48,
+        currency='₽',
+        market_country_adjectif='russian',
+        sector_company_name='banks',
+        # Value
+        company_current_price=company_object.candles.order_by('-time')[0].close,
+        company_fair_price=329.48,
         company_pe=5.0,
         peers_pe=4.8,
         industry_pe=4.8,
-        fair_pe=4.8,
+        company_fair_pe=4.8,
+        # Future
+        company_forecast_earnings_growth=0.082,
+        country_saving_rate=0.075,
+        market_forecast_earnings_growth=0.092,
+        company_forecast_revenue_growth=0.0125,
+        market_forecast_revenue_growth=0.033,
+        company_future_roe_3y=0.0199,
+        # Past
+        company_earnings=1_158_600_000_000,
+        company_profit_margins=0.476,
+        past_profit_margins=0.419,
+        company_earnings_growth_5y=0.101,
+        company_earnings_growth=0.524,
+        industry_earnings_growth=0.146,
+        company_roe=0.209,
+        # Health
+        company_short_term_assets=6_271_400_000_000,
+        company_short_term_liabilities=31_154_700_000_000,
+        company_long_term_liabilities=4_366_300_000_000,
+        company_net_debt_to_equity_ratio=0.027,
+        company_debt=9_700_000_000,
+        company_debt_to_equity_ratio=0.903,
+        company_debt_to_equity_ratio_5Y_ago=0.99,
+        company_operating_cash_flow=-100_000_000,
+        company_interest_rate=0.09,
+        company_ebit=100_000_000,
+        # Dividend
+        company_dividend_yield=0.1403,
+        market_dividend_yield_p25=0.0443,
+        market_dividend_yield_p75=0.1315,
+        company_dividend_is_volatile_in_10y=True,
+        company_dividend_amount=539_673_700_000,
+        company_dividend_amount_10y_ago=55_678_456_360,
+        company_payout_ratio=0.347,
+        company_cash_payout_ratio=0,
     )
