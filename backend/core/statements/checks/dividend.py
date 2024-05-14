cdb64dfaa34b06b24924b8a61a345cacbfcb9384
Pau4ok <pavel_mal99@mail.ru>
2024-05-14 17:48:03 +0300

    Some little fixing

--- backend/core/statements/checks/dividend.py
+++ backend/core/statements/checks/dividend.py
@@ -10,7 +10,7 @@ severity = types.Severity
 
 class NotableDividendCheck(BaseCheck):
     def check(self) -> None:
-        if self.dividend_yield > self.market_dividend_yield_p25:
+        if self.company_dividend_yield > self.market_dividend_yield_p25:
             self.statement['status'] = status.PASS
             self.statement['description'] = self.descriptions['success']
         else:
@@ -30,21 +30,26 @@ class NotableDividendCheck(BaseCheck):
             status=status.FAIL,
             severity=severity.NONE,
         )
-        self.description = {
-            'success': f'{self.slug.upper()}\'s dividend ({self.dividend_yield * 100}%) is higher than '
+        formatted_slug = self.slug.upper()
+        formatted_country_adjectif = self.market_country_adjectif.capitalize()
+        formatted_dividend_yield = self.get_percent_format(self.company_dividend_yield)
+        formatted_market_dividend_yield_p25 = self.get_percent_format(self.market_dividend_yield_p25)
+
+        self.descriptions = {
+            'success': f'{formatted_slug}\'s dividend ({formatted_dividend_yield}) is higher than '
                        f'the bottom 25% of dividend payers in '
-                       f'the {self.market_country_adjectif.capitalize()} market '
-                       f'({self.market_dividend_yield_p25 * 100}%).',
-            'error': f'{self.slug.upper()}\'s dividend ({self.dividend_yield * 100}%) is lower than '
+                       f'the {formatted_country_adjectif} market '
+                       f'({formatted_market_dividend_yield_p25}).',
+            'error': f'{formatted_slug}\'s dividend ({formatted_dividend_yield}) is lower than '
                      f'the bottom 25% of dividend payers in '
-                     f'the {self.market_country_adjectif.capitalize()} market '
-                     f'({self.market_dividend_yield_p25 * 100}%).',
+                     f'the {formatted_country_adjectif} market '
+                     f'({formatted_market_dividend_yield_p25}).',
         }
 
 
 class HighDividendCheck(BaseCheck):
     def check(self) -> None:
-        if self.dividend_yield > self.market_dividend_yield_p75:
+        if self.company_dividend_yield > self.market_dividend_yield_p75:
             self.statement['status'] = status.PASS
             self.statement['description'] = self.descriptions['success']
         else:
@@ -64,26 +69,31 @@ class HighDividendCheck(BaseCheck):
             status=status.FAIL,
             severity=severity.NONE,
         )
-        self.description = {
-            'success': f'{self.slug.upper()}\'s dividend '
-                       f'({self.dividend_yield*100}%) is in the top 25% of dividend payers in '
-                       f'the {self.market_country_adjectif.capitalize()} market '
-                       f'({self.market_dividend_yield_p75*100}%)',
-            'error': f'{self.slug.upper()}\'s dividend '
-                     f'({self.dividend_yield*100}%) is low compared to the top 25% of dividend payers in '
-                     f'the {self.market_country_adjectif.capitalize()} market '
-                     f'({self.market_dividend_yield_p75*100}%).',
+        formatted_slug = self.slug.upper()
+        formatted_country_adjectif = self.market_country_adjectif.capitalize()
+        formatted_dividend_yield = self.get_percent_format(self.company_dividend_yield)
+        formatted_market_dividend_yield_p75 = self.get_percent_format(self.market_dividend_yield_p75)
+
+        self.descriptions = {
+            'success': f'{formatted_slug}\'s dividend '
+                       f'({formatted_dividend_yield}) is in the top 25% of dividend payers in '
+                       f'the {formatted_country_adjectif} market '
+                       f'({formatted_market_dividend_yield_p75})',
+            'error': f'{formatted_slug}\'s dividend '
+                     f'({formatted_dividend_yield}) is low compared to the top 25% of dividend payers in '
+                     f'the {formatted_country_adjectif} market '
+                     f'({formatted_market_dividend_yield_p75}).',
         }
 
 
 class StableDividendCheck(BaseCheck):
     def check(self) -> None:
-        if self.dividend_is_volatile_in_10y:
+        if self.company_dividend_is_volatile_in_10y:
             self.statement['status'] = status.PASS
-            self.statement['description'] = self.description['success']
+            self.statement['description'] = self.descriptions['success']
         else:
             self.statement['status'] = status.FAIL
-            self.statement['description'] = self.description['error']
+            self.statement['description'] = self.descriptions['error']
 
     def populate(self) -> None:
         self.statement = types.Statement(
@@ -98,7 +108,7 @@ class StableDividendCheck(BaseCheck):
             status=status.FAIL,
             severity=severity.NONE,
         )
-        self.description = {
+        self.descriptions = {
             'success': f'{self.slug.upper()}\'s dividends per share have been stable in the past 10 years.',
             'error': f'{self.slug.upper()}\'s dividend payments have been volatile in the past 10 years.',
         }
@@ -106,12 +116,12 @@ class StableDividendCheck(BaseCheck):
 
 class GrowingDividendCheck(BaseCheck):
     def check(self) -> None:
-        if self.dividend_amount > self.dividend_amount_10y_ago:
+        if self.company_dividend_amount > self.company_dividend_amount_10y_ago:
             self.statement['status'] = status.PASS
-            self.statement['description'] = self.description['success']
+            self.statement['description'] = self.descriptions['success']
         else:
             self.statement['status'] = status.FAIL
-            self.statement['description'] = self.description['error']
+            self.statement['description'] = self.descriptions['error']
 
     def populate(self) -> None:
         self.statement = types.Statement(
@@ -126,7 +136,7 @@ class GrowingDividendCheck(BaseCheck):
             status=status.FAIL,
             severity=severity.NONE,
         )
-        self.description = {
+        self.descriptions = {
             'success': f'{self.slug.upper()}\'s dividend payments have increased over the past 10 years.',
             'error': f'{self.slug.upper()}\'s dividend payments have declined over the past 10 years.',
         }
@@ -134,12 +144,12 @@ class GrowingDividendCheck(BaseCheck):
 
 class EarningsCoverageCheck(BaseCheck):
     def check(self) -> None:
-        if self.payout_ratio < 1:
+        if self.company_payout_ratio < 1:
             self.statement['status'] = status.PASS
-            self.statement['description'] = self.description['success']
+            self.statement['description'] = self.descriptions['success']
         else:
             self.statement['status'] = status.FAIL
-            self.statement['description'] = self.description['error']
+            self.statement['description'] = self.descriptions['error']
 
     def populate(self) -> None:
         self.statement = types.Statement(
@@ -154,22 +164,25 @@ class EarningsCoverageCheck(BaseCheck):
             status=status.FAIL,
             severity=severity.NONE,
         )
-        self.description = {
-            'success': f'With its reasonably low payout ratio ({self.payout_ratio*100}%), '
-                       f'{self.slug.upper()}\'s dividend payments are well covered by earnings.',
-            'error': f'With its high payout ratio ({self.payout_ratio*100}%), '
-                     f'{self.slug.upper()}\'s dividend payments are not well covered by earnings',
+        formatted_slug = self.slug.upper()
+        formatted_payout_ratio = self.get_percent_format(self.company_payout_ratio)
+
+        self.descriptions = {
+            'success': f'With its reasonably low payout ratio ({formatted_payout_ratio}), '
+                       f'{formatted_slug}\'s dividend payments are well covered by earnings.',
+            'error': f'With its high payout ratio ({formatted_payout_ratio}), '
+                     f'{formatted_slug}\'s dividend payments are not well covered by earnings',
         }
 
 
 class CashFlowCoverageCheck(BaseCheck):
     def check(self) -> None:
-        if self.cash_payout_ratio < 1:
+        if self.company_cash_payout_ratio < 1:
             self.statement['status'] = status.PASS
-            self.statement['description'] = self.description['success']
+            self.statement['description'] = self.descriptions['success']
         else:
             self.statement['status'] = status.FAIL
-            self.statement['description'] = self.description['error']
+            self.statement['description'] = self.descriptions['error']
 
     def populate(self) -> None:
         self.statement = types.Statement(
@@ -184,9 +197,12 @@ class CashFlowCoverageCheck(BaseCheck):
             status=status.FAIL,
             severity=severity.NONE,
         )
-        self.description = {
-            'success': f'With its reasonably low payout ratio ({self.payout_ratio * 100}%), '
-                       f'{self.slug.upper()}\'s dividend payments are well covered by cash flows.',
-            'error': f'With its high cash payout ratio ({self.payout_ratio * 100}%), '
-                     f'{self.slug.upper()}\'s dividend payments are not well covered by cash flows.',
+        formatted_slug = self.slug.upper()
+        formatted_payout_ratio = self.get_percent_format(self.company_cash_payout_ratio)
+
+        self.descriptions = {
+            'success': f'With its reasonably low payout ratio ({formatted_payout_ratio}), '
+                       f'{formatted_slug}\'s dividend payments are well covered by cash flows.',
+            'error': f'With its high cash payout ratio ({formatted_payout_ratio}), '
+                     f'{formatted_slug}\'s dividend payments are not well covered by cash flows.',
         }
