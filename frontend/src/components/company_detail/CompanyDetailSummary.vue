d7fac1386ee5ae410c324be5a13ec8e15c554813
Pau4ok <pavel_mal99@mail.ru>
2024-05-15 15:34:24 +0300

    dry

--- frontend/src/components/company_detail/CompanyDetailSummary.vue
+++ frontend/src/components/company_detail/CompanyDetailSummary.vue
@@ -20,7 +20,7 @@
 import {defineComponent} from 'vue'
 import CompanyDetailStockOverview from "@/components/company_detail/summary/CompanyDetailStockOverview.vue";
 import CompanyDetailNotes from "@/components/company_detail/summary/notes/CompanyDetailNotes.vue";
-import CompanyDetailCompetitors from "@/components/company_detail/summary/CompanyDetailCompetitors.vue";
+import CompanyDetailCompetitors from "@/components/company_detail/summary/competitors/CompanyDetailCompetitors.vue";
 import CompanyDetailPriceHistoryPerformance
   from "@/components/company_detail/summary/CompanyDetailPriceHistoryPerformance.vue";
 import CompanyDetailAboutCompany from "@/components/company_detail/summary/CompanyDetailAboutCompany.vue";
