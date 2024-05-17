14ebf0d46bb4820849b56b11ed294cf8de3d6e12
Pau4ok <pavel_mal99@mail.ru>
2024-05-17 22:36:41 +0300

    Fixing some small bugs

--- frontend/src/views/CompanyDetailView.vue
+++ frontend/src/views/CompanyDetailView.vue
@@ -14,6 +14,10 @@ import CompanyDetailContent from "@/components/company_detail/CompanyDetailConte
 import store from "@/store";
 import axios from "axios";
 import {mapGetters, mapMutations} from "vuex";
+import type {Competitor, DetailCompany} from "@/types/invest";
+import type {Portfolio} from "@/types/portfolios";
+import type {Note} from "@/types/notes";
+import type {Statement} from "@/types/statements";
 
 export default {
   name: 'CompanyDetail',
@@ -30,18 +34,18 @@ export default {
    async  fetchCompany() {
      this.setIsLoading(true)
 
-     const company_slug = this.$route.params.company_slug
+     const company_slug = this.$route.params.company_slug as String
 
      axios
        .get(`api/v1/invest/companies/${company_slug}`)
        .then(response => {
-         this.setCompany(response.data.company)
-         this.setPortfolios(response.data.portfolios)
-         this.setNotes(response.data.notes)
-         this.setStatements(response.data.statements)
-         const snowflake = Object.values(response.data.snowflake)
+         this.setCompany(response.data.company as DetailCompany)
+         this.setPortfolios(response.data.portfolios as Array<Portfolio>)
+         this.setNotes(response.data.notes as Array<Note>)
+         this.setStatements(response.data.statements as Array<Statement>)
+         const snowflake: Array<Number> = Object.values(response.data.snowflake as Array<Number>)
          this.setSnowflake(snowflake)
-         this.setCompetitors(response.data.peers)
+         this.setCompetitors(response.data.peers as Array<Competitor>)
          document.title = `${this.company.title} (${this.company.market.title}:${this.company.ticker}) - Обзор компании, Новости, Аналитика - Finargo`
          this.isFetched = true
        })
