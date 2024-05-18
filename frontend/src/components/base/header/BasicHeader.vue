b122eef28d59910eeb530fcfb3a1d24ed8845b82
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 17:40:59 +0300

    Typing BasicHeader

--- frontend/src/components/base/header/BasicHeader.vue
+++ frontend/src/components/base/header/BasicHeader.vue
@@ -1,3 +1,14 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import Navigation from "@/components/base/header/navigation/Navigation.vue";
+
+export default defineComponent({
+  components: {
+    Navigation
+  }
+})
+</script>
+
 <template>
   <div class="header">
     <div class="header__inner">
@@ -6,36 +17,26 @@
   </div>
 </template>
 
-<script lang="ts">
-  import {defineComponent} from "vue";
-  import Navigation from "@/components/base/header/navigation/Navigation.vue";
-
-  export default defineComponent({
-    components: {Navigation}
-  })
-</script>
-
 <style scoped>
-  .header {
-    background-color: var(--bg-color);
-    width: 100%;
-    position: fixed;
-    z-index: 1000;
-    display: flex;
-    justify-content: center;
-  }
-  .header__inner {
-    width: 1200px;
-    font-size: 1.6rem;
-    font-weight: normal;
-    line-height: 1.5;
-    color: #fff;
-    height: 64px;
-    padding: 0 24px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
+.header {
+  background-color: var(--bg-color);
+  width: 100%;
+  position: fixed;
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+}
+.header__inner {
+  width: 1200px;
+  font-size: 1.6rem;
+  font-weight: normal;
+  line-height: 1.5;
+  color: #fff;
+  height: 64px;
+  padding: 0 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
