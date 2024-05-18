7d529a997c5e9de1e7f65e29d814de9856ac28c5
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 15:51:14 +0300

    Creating types for Auth

---
+++ frontend/src/types/auth.d.ts
@@ -0,0 +1,10 @@
+export interface registerAuthData {
+  new_username: String,
+  new_password1: String,
+  new_password2: String,
+}
+
+export interface loginAuthData {
+  current_username: String,
+  current_password: String,
+}
