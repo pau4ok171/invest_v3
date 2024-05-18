0cbb6e2965ca1501127bcbfc08c5dc39fee34147
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 16:19:23 +0300

    Typing SocialAuthForm

--- frontend/src/components/base/auth/SocialAuthForm.vue
+++ frontend/src/components/base/auth/SocialAuthForm.vue
@@ -1,5 +1,18 @@
+<script lang="ts">
+  import GoogleIcon from "@/components/icons/GoogleIcon.vue";
+  import {defineComponent} from "vue";
+
+  export default defineComponent({
+    name: 'SocialAuthForm',
+    components: {
+      GoogleIcon,
+    }
+  })
+</script>
+
 <template>
 <div class="auth-form__socials">
+
   <div class="auth-form__socials-desc">Или с помощью:</div>
 
   <div class="auth-form__socials-items">
@@ -21,67 +34,57 @@
       </button>
     </div>
   </div>
+
 </div>
 </template>
 
-<script lang="ts">
-  import GoogleIcon from "@/components/icons/GoogleIcon.vue";
-
-  export default {
-    name: 'SocialAuthForm',
-    components: {
-      GoogleIcon,
-    }
-  }
-</script>
-
 <style scoped>
-  .auth-form__socials {
-    padding-top: 24px;
-  }
-  .auth-form__socials-desc {
-    margin-bottom: 16px;
-    font-size: 1.6rem;
-    line-height: 1.5;
-    text-align: left;
-    font-weight: normal;
-    color: #000;
-  }
-  .auth-form__socials-items {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 24px;
-  }
-  .auth-form__socials-item {
-    position: relative;
-  }
-  .auth-form__socials-button {
-    position: relative;
-    border: 0;
-    border-radius: 6px;
-    vertical-align: top;
-    padding: 0 12px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 8px;
-    width: 42px;
-    height: 42px;
-    background-color: #fff;
-    box-shadow: rgba(0, 0, 0, .1) 0 2px 3px;
-    transition: background-color .2s ease 0s, box-shadow .2s ease 0s, color .4s ease 0s, padding .4s ease 0s;
-  }
-  .auth-form__socials-button:hover {
-    box-shadow: rgba(0, 0, 0, .2) 0 2px 3px;
-  }
-  .auth-form__socials-button img {
-    left: 10px;
-    top: 10px;
-    width: 22px;
-    height: 22px;
-    display: inline;
-    vertical-align: middle;
-    margin: 0 -4px;
-  }
+.auth-form__socials {
+  padding-top: 24px;
+}
+.auth-form__socials-desc {
+  margin-bottom: 16px;
+  font-size: 1.6rem;
+  line-height: 1.5;
+  text-align: left;
+  font-weight: normal;
+  color: #000;
+}
+.auth-form__socials-items {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 24px;
+}
+.auth-form__socials-item {
+  position: relative;
+}
+.auth-form__socials-button {
+  position: relative;
+  border: 0;
+  border-radius: 6px;
+  vertical-align: top;
+  padding: 0 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 8px;
+  width: 42px;
+  height: 42px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, .1) 0 2px 3px;
+  transition: background-color .2s ease 0s, box-shadow .2s ease 0s, color .4s ease 0s, padding .4s ease 0s;
+}
+.auth-form__socials-button:hover {
+  box-shadow: rgba(0, 0, 0, .2) 0 2px 3px;
+}
+.auth-form__socials-button img {
+  left: 10px;
+  top: 10px;
+  width: 22px;
+  height: 22px;
+  display: inline;
+  vertical-align: middle;
+  margin: 0 -4px;
+}
 </style>
