f9983e1882a817e52fb5d2d9010a74ed91f79e59
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 17:36:02 +0300

    Typing NavMenu

--- frontend/src/components/base/header/navigation/NavMenu.vue
+++ frontend/src/components/base/header/navigation/NavMenu.vue
@@ -1,33 +1,41 @@
-<template>
-  <div class="navigation__nav-menu" >
-    <RouterLink
-        v-for="routeName in RoutesHeaderEnum"
-        :key="routeName"
-        class="nav-menu__item"
-        :to="{ name: routeName }"
-    >
-      {{ routeName }}
-    </RouterLink>
-  </div>
-</template>
+<script lang="ts">
+import {RoutesHeaderEnum} from "@/router/routes.types";
+import {defineComponent} from "vue";
 
-<script setup lang="ts">
-  import { RoutesHeaderEnum } from "@/router/routes.types";
+export default defineComponent({
+  computed: {
+    RoutesHeaderEnum() {
+      return RoutesHeaderEnum
+    }
+  }
+})
 </script>
 
-<style scoped>
-  .navigation__nav-menu {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+<template>
+<div class="navigation__nav-menu" >
+  <RouterLink
+      v-for="routeName in RoutesHeaderEnum"
+      :key="routeName"
+      class="nav-menu__item"
+      :to="{ name: routeName }"
+  >
+    {{ routeName }}
+  </RouterLink>
+</div>
+</template>
 
-  .nav-menu__item {
-    font-size: 1.4rem;
-    padding: 0 8px;
-    display: inline-block;
-    text-transform: capitalize;
-    height: 64px;
-    line-height: 64px;
-  }
+<style scoped>
+.navigation__nav-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.nav-menu__item {
+  font-size: 1.4rem;
+  padding: 0 8px;
+  display: inline-block;
+  text-transform: capitalize;
+  height: 64px;
+  line-height: 64px;
+}
 </style>
