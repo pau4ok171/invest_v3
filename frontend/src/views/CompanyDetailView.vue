909e4eaa4d29196c7015c1ed6292cb84c9e2ca59
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 12:33:46 +0300

    Typing views/CompanyDetailView

--- frontend/src/views/CompanyDetailView.vue
+++ frontend/src/views/CompanyDetailView.vue
@@ -1,75 +1,42 @@
-<template>
-
-  <CompanyDetailHeader v-if="isFetched"/>
-
-  <CompanyDetailContent v-if="isFetched"/>
-
-</template>
-
 <script lang="ts">
 import CompanyDetailHeader from "@/components/company_detail/CompanyDetailHeader.vue";
 import CompanyDetailContent from "@/components/company_detail/CompanyDetailContent.vue";
+import {mapActions, mapGetters, mapMutations} from "vuex";
+import {defineComponent} from "vue";
 
-
-import store from "@/store";
-import axios from "axios";
-import {mapGetters, mapMutations} from "vuex";
-import type {Competitor, DetailCompany} from "@/types/invest";
-import type {Portfolio} from "@/types/portfolios";
-import type {Note} from "@/types/notes";
-import type {Statement} from "@/types/statements";
-
-export default {
+export default defineComponent({
   name: 'CompanyDetail',
   components: {
     CompanyDetailContent,
     CompanyDetailHeader
   },
-  data() {
-    return {
-      isFetched: false
-    }
+  async mounted() {
+    const company_slug = this.$route.params.company_slug as String
+    await this.fetchCompany(company_slug)
+    await this.fetchPriceData(company_slug)
+    document.title = `${this.company.title} (${this.company.market.title}:${this.company.ticker}) - Обзор компании, Новости, Аналитика - Finargo`
+    this.setPageIsReady(true)
   },
   methods: {
-   async  fetchCompany() {
-     this.setIsLoading(true)
-
-     const company_slug = this.$route.params.company_slug as String
-
-     axios
-       .get(`api/v1/invest/companies/${company_slug}`)
-       .then(response => {
-         this.setCompany(response.data.company as DetailCompany)
-         this.setPortfolios(response.data.portfolios as Array<Portfolio>)
-         this.setNotes(response.data.notes as Array<Note>)
-         this.setStatements(response.data.statements as Array<Statement>)
-         const snowflake: Array<Number> = Object.values(response.data.snowflake as Array<Number>)
-         this.setSnowflake(snowflake)
-         this.setCompetitors(response.data.peers as Array<Competitor>)
-         document.title = `${this.company.title} (${this.company.market.title}:${this.company.ticker}) - Обзор компании, Новости, Аналитика - Finargo`
-         this.isFetched = true
-       })
-
-     this.setIsLoading(false)
-    },
     ...mapMutations({
-      setIsLoading: 'setIsLoading',
-      setCompany: 'companyDetail/setCompany',
-      setPortfolios: 'companyDetail/setPortfolios',
-      setNotes: "companyDetail/setNotes",
-      setStatements: "companyDetail/setStatements",
-      setSnowflake: "companyDetail/setSnowflake",
-      setCompetitors: "companyDetail/setCompetitors",
-    })
-  },
-  async mounted() {
-    await this.fetchCompany()
-    await store.dispatch("companyDetail/fetchPriceData", this.$route.params.company_slug)
+      setPageIsReady: 'companyDetail/setPageIsReady'
+    }),
+    ...mapActions({
+      fetchCompany: 'companyDetail/fetchCompany',
+      fetchPriceData: 'companyDetail/fetchPriceData',
+    }),
   },
   computed: {
     ...mapGetters({
       company: 'companyDetail/getCompany',
+      pageIsReady: 'companyDetail/getPageIsReady',
     })
   },
-}
+})
 </script>
+
+<template>
+<CompanyDetailHeader v-if="pageIsReady"/>
+
+<CompanyDetailContent v-if="pageIsReady"/>
+</template>
