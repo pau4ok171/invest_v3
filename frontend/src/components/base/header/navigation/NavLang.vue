309f5a92b937a67f5bfb2d86f44b4159c2de452a
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 17:34:18 +0300

    Typing NavLang

--- frontend/src/components/base/header/navigation/NavLang.vue
+++ frontend/src/components/base/header/navigation/NavLang.vue
@@ -1,42 +1,36 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div class="navigation__nav-lang">
-    <button class="nav-lang__nav-button">
-      <span class="nav-button__flag"><img src="../../../../assets/img/flags/ru.svg" alt="RU"></span>
-      <span class="nav-button__langcode">RU</span>
-    </button>
-  </div>
+<div class="navigation__nav-lang">
+
+  <button class="nav-lang__nav-button">
+    <span class="nav-button__flag"><img src="../../../../assets/img/flags/ru.svg" alt="RU"></span>
+    <span class="nav-button__lang-code">RU</span>
+  </button>
+
+</div>
 </template>
 
 <style scoped>
-  .navigation__nav-lang {
-    position: relative;
-    display: flex;
-    align-items: center;
-    padding: 0 16px;
-  }
-
-  .nav-lang__nav-button {
-    display: flex;
-    padding: 0 16px;
-    line-height: 1.5;
-  }
-
-  .nav-button__flag {
-    display: inline-block;
-    font-size: 1.4rem;
-  }
-
-  .nav-button__flag img {
-    height: 12px;
-  }
-
-  .nav-button__langcode {
-    padding-left: 8px;
-    color: #fff;
-    font-size: 1.4rem;
-  }
+.navigation__nav-lang {
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+}
+.nav-lang__nav-button {
+  display: flex;
+  padding: 0 16px;
+  line-height: 1.5;
+}
+.nav-button__flag {
+  display: inline-block;
+  font-size: 1.4rem;
+}
+.nav-button__flag img {
+  height: 12px;
+}
+.nav-button__lang-code {
+  padding-left: 8px;
+  color: #fff;
+  font-size: 1.4rem;
+}
 </style>
