b89ac9acfef7c1e31d30a4ffc00c6f34bdf90468
Pau4ok <pavel_mal99@mail.ru>
2024-05-18 17:34:39 +0300

    Typing NavLogo

--- frontend/src/components/base/header/navigation/NavLogo.vue
+++ frontend/src/components/base/header/navigation/NavLogo.vue
@@ -1,26 +1,21 @@
-<script setup lang="ts">
-
-import {RoutesHeaderEnum} from "@/router/routes.types";
-</script>
-
 <template>
-  <div class="default-logo navigation__logo">
-    <RouterLink
-      :key="`home`"
-      :to="{ name: `home` }"
-    >
-      Finargo
-    </RouterLink>
-  </div>
+<div class="default-logo navigation__logo">
+  <RouterLink
+    :key="`home`"
+    :to="{ name: `home` }"
+  >
+    Finargo
+  </RouterLink>
+</div>
 </template>
 
 <style scoped>
-  .navigation__logo {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    font-size: 30px;
-    font-weight: 900;
-    user-select: none;
-  }
+.navigation__logo {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 30px;
+  font-weight: 900;
+  user-select: none;
+}
 </style>
