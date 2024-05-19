3fcd528e689e1444c95895f6c87eb6a751acc330
Pau4ok <pavel_mal99@mail.ru>
2024-05-19 11:37:13 +0300

    Typing AppLayout

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-  import {useRoute} from "vue-router";
-  const route = useRoute();
-</script>
-
 <template>
-  <component :is="route.meta.layoutComponent">
-    <slot/>
-  </component>
+<component :is="$route.meta.layoutComponent">
+  <slot/>
+</component>
 </template>
-
-<style scoped>
-
-</style>
