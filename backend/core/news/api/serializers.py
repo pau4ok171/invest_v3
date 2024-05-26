baa76ceb10e8eb17be7e8471909a8ac4f69c49d1
Pau4ok <pavel_mal99@mail.ru>
2024-05-26 18:22:03 +0300

    Creating NewsSerializer

---
+++ backend/core/news/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from news.models import News
+
+
+class NewsSerializer(ModelSerializer):
+    class Meta:
+        model = News
+        fields = (
+            'id',
+            'title',
+            'content',
+            'type',
+            'date',
+        )
