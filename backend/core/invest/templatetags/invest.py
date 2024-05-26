282cbddaff58c1d39ad360b17ec8cdb9d04eee92
Pau4ok <pavel_mal99@mail.ru>
2024-05-26 20:20:14 +0300

    Deleting unused templates, template tags and statics

--- backend/core/invest/templatetags/invest.py
+++
@@ -1,9 +0,0 @@
-from django.template.defaultfilters import register
-
-
-@register.filter(name='get_dict_value')
-def get_dict_value(d, key):
-    try:
-        return d[key]
-    except KeyError:
-        return 'n/a'
