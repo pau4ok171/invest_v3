0980e13de9cf23c36b73feac49721a11c761e1cc
Pau4ok <pavel_mal99@mail.ru>
2024-06-07 15:15:59 +0300

    Creating dividend processing task

--- backend/core/invest/tasks.py
+++ backend/core/invest/tasks.py
@@ -1,8 +1,14 @@
 from celery import shared_task
 import asyncio
 from invest.tinkoff_api import main
+from invest.services.dividend import main as dividend_main
 
 
 @shared_task(name='Tinkoff_API')
 def tinkoff_task():
     return main()
+
+
+@shared_task(name='dividends')
+def dividend_task():
+    return dividend_main()
