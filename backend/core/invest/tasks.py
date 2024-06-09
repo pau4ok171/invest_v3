21aaeb219d32b9ac12f4783af8de2492a4aee17f
Pau4ok <pavel_mal99@mail.ru>
2024-06-09 20:44:03 +0300

    Adding tasks for calculating returns

--- backend/core/invest/tasks.py
+++ backend/core/invest/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 import asyncio
 from invest.tinkoff_api import main
 from invest.services.dividend import main as dividend_main
+from invest.services.returns import main as returns_main
 
 
 @shared_task(name='Tinkoff_API')
@@ -12,3 +13,8 @@ def tinkoff_task():
 @shared_task(name='dividends')
 def dividend_task():
     return dividend_main()
+
+
+@shared_task(name='returns')
+def returns_task():
+    return returns_main()
