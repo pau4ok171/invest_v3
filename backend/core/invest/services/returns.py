a978e3317b3bbb91d07145347f3ac905b4c3ca65
Pau4ok <pavel_mal99@mail.ru>
2024-06-09 20:41:15 +0300

    Refactoring returns service

--- backend/core/invest/services/returns.py
+++ backend/core/invest/services/returns.py
@@ -20,70 +20,31 @@ def main():
     companies = get_company_list(market=None, sector=None)
 
     # Посчитать данные
-    market_price_data = [calculate_aggregate_price_data(market) for market in markets]
+    company_price_data = [get_company_price_data(company) for company in companies]
     sector_market_price_data = [
-        calculate_aggregate_price_data(market, sector) for market in markets for sector in sectors
+        get_aggregate_price_data(company_price_data, market=market, sector=sector)
+        for market in markets for sector in sectors
     ]
-    company_price_data = [calculate_company_price_data(company) for company in companies]
+    market_price_data = [get_aggregate_price_data(company_price_data, market=market) for market in markets]
 
     update_or_create(market_price_data, sector_market_price_data, company_price_data)
 
 
-def get_market_list():
-    return Market.objects.all()
-
-
-def get_sector_list():
-    return Sector.objects.all()
-
-
-def calculate_aggregate_price_data(market, sector=None):
-    # Получить список компаний по рынку и сектору
-    companies = get_company_list(market, sector)
-
-    # Посчитать данные по компании
-    companies_market_cap = [calculate_company_market_cap(company) for company in companies]
-
-    market_returns = get_aggregate_returns(companies_market_cap)
-    market_returns['market_id'] = market.id
-    if sector:
-        market_returns['sector_id'] = sector.id
-    return market_returns
-
-
-def calculate_company_price_data(company):
-    company_market_cap = calculate_company_market_cap(company)
-    price_data = get_aggregate_returns([company_market_cap])
-    average_weekly_mouvement = 0
+def get_company_price_data(company):
+    price_data = get_company_returns(company)
+    average_weekly_mouvement = get_average_weekly_mouvement(company)
     price_data['company_id'] = company.id
+    price_data['company'] = company
     price_data['average_weekly_mouvement'] = average_weekly_mouvement
     return price_data
 
 
-def get_company_list(market, sector):
-    companies = Company.objects.filter(is_visible=True)
-
-    if market:
-        companies = companies.filter(market=market)
-    if sector:
-        companies = companies.filter(sector=sector)
-    return companies
-
-
-def calculate_company_market_cap(company):
-    reports = company.reports
-    if not reports.exists():
-        return {k: 0 for k in PERIODS.keys()}
-    share_outstanding = reports.latest('year', 'quarter').share_outstanding
+def get_company_returns(company) -> dict:
     prices = get_prices(company)
-
-    return {k: prices[k] * share_outstanding for k in PERIODS.keys()}
-
-
-def get_aggregate_returns(market_caps):
-    cap = {key: sum(map(lambda x: x[key], market_caps)) for key in PERIODS.keys()}
-    returns = {f'return_{k}': (cap['now'] - cap[k]) / cap[k] if cap[k] != 0 else 0 for k in cap.keys()}
-    returns.pop('return_now')
+    returns = {
+        f'return_{k}': (prices['now'].close - prices[k].close) / prices[k].close
+        if prices[k] != 0 and prices[k].close != 0 else 0 for k in prices.keys()
+    }
     return returns
 
 
@@ -94,8 +55,47 @@ def get_prices(company):
 def get_price(company, days):
     time = datetime.datetime.now(tz=datetime.UTC) - datetime.timedelta(days=days)
     if company.candles.filter(time__lte=time).exists():
-        return company.candles.filter(time__lte=time).latest('time').close
+        return company.candles.filter(time__lte=time).latest('time')
     return 0
+
+
+def get_aggregate_price_data(companies, market, sector=None):
+    companies = list(filter(lambda x: x['company'].market.id == market.id, companies))
+    if sector:
+        companies = list(filter(lambda x: x['company'].sector.id == sector.id, companies))
+
+    price_data = {
+        f'return_{key}': sum(map(lambda x: x[f'return_{key}'], companies)) / len(companies)
+        if len(companies) != 0 else 0 for key in PERIODS.keys()
+    }
+    average_weekly_mouvement = 0
+    if companies:
+        average_weekly_mouvement = sum(map(lambda x: x['average_weekly_mouvement'], companies)) / len(companies)
+
+    price_data['average_weekly_mouvement'] = average_weekly_mouvement
+    price_data['market_id'] = market.id
+    if sector:
+        price_data['sector_id'] = sector.id
+
+    return price_data
+
+
+def get_market_list():
+    return Market.objects.all()
+
+
+def get_sector_list():
+    return Sector.objects.all()
+
+
+def get_company_list(market, sector):
+    companies = Company.objects.filter(is_visible=True)
+
+    if market:
+        companies = companies.filter(market=market)
+    if sector:
+        companies = companies.filter(sector=sector)
+    return companies
 
 
 def update_or_create(market_price_data, sector_market_price_data, company_price_data):
@@ -114,3 +114,11 @@ def update_or_create(market_price_data, sector_market_price_data, company_price_
         pk=price_data['company_id'],
         defaults=price_data
     ) for price_data in company_price_data]
+
+
+def get_average_weekly_mouvement(company) -> int:
+    time = datetime.datetime.now(tz=datetime.UTC) - datetime.timedelta(days=7)
+    candles = company.candles.filter(time__gte=time)
+    if candles.exists():
+        return sum(map(lambda x: (x.high - x.low) / x.high, candles)) / len(candles)
+    return 0
