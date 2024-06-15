60dd3202f980024ec7aeb90274ed3da74b6d260d
Pau4ok <pavel_mal99@mail.ru>
2024-06-15 15:20:09 +0300

    Creating PageNotFound

---
+++ frontend/src/views/PageNotFoundView.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import PageNotFound from "@/components/base/page_not_found/PageNotFound.vue";
+
+export default defineComponent({
+  name: "PageNotFoundView",
+  components: {PageNotFound}
+})
+</script>
+
+<template>
+<PageNotFound/>
+</template>
