03aca021e954b5abaa2b6fb8a0928a8192f1098f
Pau4ok <pavel_mal99@mail.ru>
2024-06-16 14:55:51 +0300

    Creating FetchingData.vue component for wait when chart is loading

---
+++ frontend/src/components/charts/FetchingData.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {AtomSpinner} from "epic-spinners";
+
+export default defineComponent({
+  name: "FetchingData",
+  components: {AtomSpinner}
+})
+</script>
+
+<template>
+<div class="chart-data-not-available">
+  <atom-spinner
+    :animation-duration="1250"
+    :size="30"
+    color="#ff1d5e"
+  />
+  <p>Fetching data from server...</p>
+</div>
+</template>
+
+<style scoped>
+.chart-data-not-available {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+  height: 100%;
+  background-color: var(--bg-color);
+  border-radius: 8px;
+}
+.chart-data-not-available p {
+  font-size: 1.2rem;
+}
+</style>
