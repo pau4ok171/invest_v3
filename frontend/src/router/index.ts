90dac4f453cd7ee31ef1f9b17ebfdae245e021ae
Pau4ok <pavel_mal99@mail.ru>
2024-06-16 14:58:39 +0300

    Async HomeView in routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,23 +2,17 @@ import {createRouter, createWebHistory} from 'vue-router';
 import {loadLayoutMiddleware} from "@/router/middleware/loadLayout.middleware";
 import {RouteNamesEnum} from "@/router/routes.types";
 
-// VIEWS
-import HomeView from "@/views/HomeView.vue";
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: RouteNamesEnum.home,
-      component: HomeView,
+      component: () => import('@/views/HomeView.vue'),
     },
     {
       path: '/dashboard',
       name: RouteNamesEnum.dashboard,
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('@/views/HomeView.vue')
     },
     {
