e53b76b5b958ecb6c5330bdca836e7931a9af9ac
Pau4ok <pavel_mal99@mail.ru>
2024-06-18 21:01:53 +0300

    Creating CompanyDetailNotesLateralMenu.vue

---
+++ frontend/src/components/company_detail/content_list/summary/notes/CompanyDetailNotesLateralMenu.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import BaseLateralMenu from "@/components/UI/lateral_menu/BaseLateralMenu.vue";
+import CompanyDetailNotesNoteItem
+  from "@/components/company_detail/content_list/summary/notes/CompanyDetailNotesNoteItem.vue";
+import {mapGetters} from "vuex";
+
+export default defineComponent({
+  name: "CompanyDetailNotesLateralMenu",
+  components: {CompanyDetailNotesNoteItem, BaseLateralMenu},
+  computed: {
+    ...mapGetters({
+      notes: 'companyDetail/getNotes',
+    }),
+  },
+})
+</script>
+
+<template>
+<BaseLateralMenu>
+  <template #title>
+    Notes
+  </template>
+  <template #content>
+    <div class="base-lateral-menu-content">
+      <CompanyDetailNotesNoteItem
+        v-for="note in notes.slice(0, 3)"
+        :note
+        :key="note.id"
+      />
+    </div>
+
+  </template>
+</BaseLateralMenu>
+</template>
+
+<style scoped>
+.base-lateral-menu-content {
+  display: grid;
+  gap: 8px;
+}
+</style>
