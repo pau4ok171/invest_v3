e1325a83464d53ef29ff8170467e7e96f79bd5d4
Pau4ok <pavel_mal99@mail.ru>
2024-06-18 21:04:13 +0300

    Creating lateralMenu statement

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -6,10 +6,14 @@ import {authModule} from "@/store/modules/authModule";
 const store = createStore({
   state: () => ({
     isLoading: false,
+    lateralMenuIsOpen: false,
   }),
   mutations: {
     setIsLoading(state, status: boolean) {
       state.isLoading = status
+    },
+    setLateralMenuIsOpen(state, status: boolean) {
+      state.lateralMenuIsOpen = status
     },
   },
   modules: {
