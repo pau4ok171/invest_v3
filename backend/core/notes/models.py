706dcc88428896d5e8f9d8c8047659bf871a7dce
Pau4ok <pavel_mal99@mail.ru>
2024-06-18 16:25:30 +0300

    Adding text fields for holding Adding text field for holding text format of note

--- backend/core/notes/models.py
+++ backend/core/notes/models.py
@@ -6,6 +6,7 @@ class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     company = models.ForeignKey('invest.Company', on_delete=models.PROTECT)
     body = models.TextField()
+    text = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
