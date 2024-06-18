62460cc2c0c65954198015d721a39b59141988d7
Pau4ok <pavel_mal99@mail.ru>
2024-06-18 16:24:59 +0300

    Adding test fields for holding Adding text field for holding text format of note

--- backend/core/notes/api/forms.py
+++ backend/core/notes/api/forms.py
@@ -4,9 +4,11 @@ from django import forms
 class NotesCreateForm(forms.Form):
     company_id = forms.IntegerField(min_value=0)
     content = forms.CharField()
+    text = forms.CharField(required=False)
 
 
 class NotesUpdateForm(forms.Form):
     note_id = forms.IntegerField(min_value=0)
     content = forms.CharField()
+    text = forms.CharField(required=False)
     updated = forms.DateTimeField()
