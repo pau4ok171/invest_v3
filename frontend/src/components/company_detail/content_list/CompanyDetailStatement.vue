afc9aa5dd871a451304cfdbff9edf0ea2e353d20
Pau4ok <pavel_mal99@mail.ru>
2024-06-19 17:16:46 +0300

    Refactoring statement item

--- frontend/src/components/company_detail/content_list/CompanyDetailStatement.vue
+++ frontend/src/components/company_detail/content_list/CompanyDetailStatement.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
-import {defineComponent} from 'vue'
-import CompanyDetailStatementItem from "@/components/company_detail/content_list/CompanyDetailStatementItem.vue";
+import {defineComponent} from 'vue';
+import type {PropType} from 'vue';
+import type {Statement} from "@/types/statements";
 
 export default defineComponent({
   name: "CompanyDetailStatement",
-  components: {CompanyDetailStatementItem},
   props: {
-    statement_list: {
-      type: Object,
+    statement: {
+      type: Object as PropType<Statement>,
       required: true,
     }
   }
@@ -17,7 +17,12 @@ export default defineComponent({
 <template>
 <section class="detail-statement">
 
-  <CompanyDetailStatementItem v-for="statement in statement_list" :key="statement.id" :statement/>
+  <blockquote class="detail-statement-item">
+    <p class="detail-statement-item__text">
+      <span class="text--error">{{  statement.title }}: </span>
+      <span>{{ statement.description }}</span>
+    </p>
+  </blockquote>
 
 </section>
 </template>
@@ -25,5 +30,14 @@ export default defineComponent({
 <style scoped>
 .detail-statement {
   margin-top: 24px;
+}
+.detail-statement-item {
+  position: relative;
+  margin-top: 8px;
+  text-align: left;
+}
+.detail-statement-item__text {
+  font-size: 1.6rem;
+  line-height: 1.5;
 }
 </style>
