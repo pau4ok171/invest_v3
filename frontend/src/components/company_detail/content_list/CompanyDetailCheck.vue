acdc1075366311f96b720882cbdb0db700f92192
Pau4ok <pavel_mal99@mail.ru>
2024-06-26 21:33:25 +0300

    Refactoring CompanyDetailCheck.vue

--- frontend/src/components/company_detail/content_list/CompanyDetailCheck.vue
+++ frontend/src/components/company_detail/content_list/CompanyDetailCheck.vue
@@ -2,6 +2,8 @@
 import {defineComponent} from 'vue'
 import CheckIcon from "@/components/icons/CheckIcon.vue";
 import CrossIcon from "@/components/icons/CrossIcon.vue";
+import {mapGetters} from "vuex";
+import type {Statement} from "@/types/statements";
 
 export default defineComponent({
   name: "CompanyDetailCheck",
@@ -10,22 +12,51 @@ export default defineComponent({
     CheckIcon
   },
   props: {
-    check: {
-      type: Object,
+    name: {
+      type: String,
       required: true,
+    },
+  },
+  data() {
+    return {
+      check: {
+        area: '',
+        company: 0,
+        description: '',
+        id: 0,
+        level: '',
+        name: '',
+        question: '',
+        severity: '',
+        status: '',
+        title: '',
+        type: '',
+        outcome: 0,
+      } as Statement
     }
-  }
+  },
+  computed: {
+    ...mapGetters({
+      statements: 'companyDetail/getStatements',
+    }),
+    get_check() {
+      const check: Statement = this.statements[this.name]
+      if (check) {
+        return check
+      }
+    },
+  },
 })
 </script>
 
 <template>
 <blockquote class="analysis-section__point">
-  <CheckIcon v-if="check.status === 'passed'" class="icon--success"/>
+  <CheckIcon v-if="check.status === 'PASS'" class="icon--success"/>
   <CrossIcon v-else class="icon--error"/>
 
   <p class="analysis-section__point-text">
-    <span class="analysis-section__point-title">{{ check.title }}: </span>
-    <span>{{ check.description }}</span>
+    <span class="analysis-section__point-title">{{ get_check?.title }}: </span>
+    <span>{{ get_check?.description }}</span>
   </p>
 </blockquote>
 </template>
