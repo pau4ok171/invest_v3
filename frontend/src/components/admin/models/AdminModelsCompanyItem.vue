b11a99fb13796821bc177c8dad61c7892ea66ff7
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 13:25:59 +0300

    Create AdminModelsCompanyItem.vue

---
+++ frontend/src/components/admin/models/AdminModelsCompanyItem.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import type {PropType} from "vue";
+import type {AdminCompany} from "@/types/admin";
+
+export default defineComponent({
+  name: "AdminModelsCompanyItem",
+  props: {
+    company: {
+      type: Object as PropType<AdminCompany>,
+      required: true
+    }
+  }
+})
+</script>
+
+<template>
+<tr>
+  <td>{{ company.id }}</td>
+  <td>{{ company.title }}</td>
+  <td>{{ company.slug.toUpperCase() }}</td>
+  <td>
+    <img
+        :src="company.logo"
+        class="admin-models-company-item__logo"
+        alt="company"
+    >
+  </td>
+  <td>{{ company.market_name }}</td>
+  <td>{{ company.sector_name }}</td>
+  <td>{{ company.is_visible }}</td>
+</tr>
+</template>
+
+<style scoped>
+.admin-models-company-item__logo {
+  background-color: #fff;
+  border-radius: 8px;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 4px;
+}
+</style>
