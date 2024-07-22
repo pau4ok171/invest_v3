d87b51f610d07eae56775843ebe0575a0c77b828
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 13:49:03 +0300

    delete absolute url from serializers.py

--- backend/core/invest/api/serializers.py
+++ backend/core/invest/api/serializers.py
@@ -86,7 +86,6 @@ class CompanySearchSerializer(serializers.ModelSerializer):
     market = MarketSerializer(read_only=True)
     sector = SectorSerializer(read_only=True)
     logo_url = serializers.CharField(source='get_logo', read_only=True)
-    absolute_url = serializers.CharField(source='get_absolute_url', read_only=True)
 
     class Meta:
         model = Company
@@ -97,7 +96,6 @@ class CompanySearchSerializer(serializers.ModelSerializer):
             'country',
             'sector',
             'market',
-            'absolute_url',
         )
 
 
