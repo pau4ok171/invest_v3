ffb22e0d3497fc06ffe3dc6dffd9a6e9bd0ca8a5
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 13:22:49 +0300

    Add admin types

---
+++ frontend/src/types/admin.d.ts
@@ -0,0 +1,13 @@
+export interface AdminCompany {
+  id: number,
+  ticker: string,
+  slug: string,
+  uid: string,
+  title: string,
+  short_title: string,
+  is_visible: boolean,
+  logo: string,
+  is_fund: boolean,
+  market_name: string,
+  sector_name: string,
+}
