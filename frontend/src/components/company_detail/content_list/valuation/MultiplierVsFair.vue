58665f706867d681dc2aa15532507d9c5851e2da
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 15:04:22 +0300

    Refactor Description into MultiplierVsFair.vue

--- frontend/src/components/company_detail/content_list/valuation/MultiplierVsFair.vue
+++ frontend/src/components/company_detail/content_list/valuation/MultiplierVsFair.vue
@@ -4,6 +4,7 @@ import DetailAnalysisTitle from "@/components/UI/text/DetailAnalysisTitle.vue";
 import DetailAnalysisDesc from "@/components/UI/text/DetailAnalysisDesc.vue";
 import CompanyDetailCheck from "@/components/company_detail/content_list/CompanyDetailCheck.vue";
 import MultiplierVsFairChart from "@/components/charts/MultiplierVsFairChart.vue";
+import {mapGetters} from "vuex";
 
 export default defineComponent({
   name: "MultiplierVsFair",
@@ -13,13 +14,15 @@ export default defineComponent({
     DetailAnalysisDesc,
     DetailAnalysisTitle
   },
-  data() {
-    return {
-      checks: [
-          {id: 5, status: 'passed', title: 'Price-To-Earnings vs Fair Ratio', description: 'SBER is good value based on its Price-To-Earnings Ratio (2.6x) compared to the estimated Fair Price-To-Earnings Ratio (4.3x)'},
-      ]
-    }
-  }
+  computed: {
+    ...mapGetters({
+      company: 'companyDetail/getCompany',
+    }),
+    get_section_desc() {
+      if (!this.company.slug) return ''
+      return `What is ${this.company.slug.toUpperCase()}'s PE Ratio compared to its Fair PE Ratio? This is the expected PE Ratio taking into account the company's forecast earnings growth, profit margins and other risk factors.`
+    },
+  },
 })
 </script>
 
@@ -31,7 +34,7 @@ export default defineComponent({
   </DetailAnalysisTitle>
 
   <DetailAnalysisDesc>
-    What is SBER's PE Ratio compared to its Fair PE Ratio? This is the expected PE Ratio taking into account the company's forecast earnings growth, profit margins and other risk factors.
+    {{ get_section_desc }}
   </DetailAnalysisDesc>
 
   <div class="detail__content">
