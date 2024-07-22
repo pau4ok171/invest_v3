f0790aebb375767a005d491d72023ffcbfa4f867
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 16:15:08 +0300

    Create type for AdminDetailCompany

--- frontend/src/types/admin.d.ts
+++ frontend/src/types/admin.d.ts
@@ -11,3 +11,23 @@ export interface AdminCompany {
   market_name: string,
   sector_name: string,
 }
+
+export interface AdminDetailCompany {
+  id: number | null,
+  ticker: string | null,
+  slug: string | null,
+  uid: string | null,
+  title: string | null,
+  shortTitle: string | null,
+  isVisible: boolean | null,
+  logo: string | null,
+  isFund: boolean | null,
+  marketName: string | null,
+  sectorName: string | null,
+  industryName: string | null,
+  absoluteURL: string | null,
+  country_flag: string | null,
+  currencySymbol: string | null,
+  created: string | null,
+  updated: string | null,
+}
