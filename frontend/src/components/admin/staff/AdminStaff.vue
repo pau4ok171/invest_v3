57c9b5e2da0c17097ce72522565ddeb4b7993aa2
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 13:25:22 +0300

    Create AdminStaff.vue

---
+++ frontend/src/components/admin/staff/AdminStaff.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AdminStaff"
+})
+</script>
+
+<template>
+<div class="page-not-found">
+  ADMIN STAFF
+</div>
+</template>
+
+<style scoped>
+.page-not-found {
+  display: flex;
+  font-size: 5rem;
+  align-items: center;
+  justify-content: center;
+  margin: 100px 0;
+}
+</style>
