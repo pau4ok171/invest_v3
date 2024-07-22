79f02a7058a9588895cf76ee52d934b4ed332cf6
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 15:06:57 +0300

    Refactor checks into MultiplierVsPeers.vue

--- frontend/src/components/company_detail/content_list/valuation/MultiplierVsPeers.vue
+++ frontend/src/components/company_detail/content_list/valuation/MultiplierVsPeers.vue
@@ -79,13 +79,7 @@ export default defineComponent({
     </div>
 
     <div class="detail__content-item detail__point-list">
-
-      <CompanyDetailCheck
-        v-for="check in checks"
-        :check
-        :key="check.id"
-      />
-
+      <CompanyDetailCheck name="IsGoodValueComparingPriceToEarningsToPeersAverageValue"/>
     </div>
 
   </div>
