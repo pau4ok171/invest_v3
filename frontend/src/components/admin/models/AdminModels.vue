90c10049d5e7f969935fb8bd8771cfac9f396c53
Pau4ok <pavel_mal99@mail.ru>
2024-07-22 13:24:06 +0300

    Create AdminModels.vue

---
+++ frontend/src/components/admin/models/AdminModels.vue
@@ -0,0 +1,161 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import RoundedDarkBlueButton from "@/components/UI/buttons/RoundedDarkBlueButton.vue";
+import AdminModelsCompanyItem from "@/components/admin/models/AdminModelsCompanyItem.vue";
+import axios from "axios";
+import type {AdminCompany} from "@/types/admin";
+
+
+export default defineComponent({
+  name: "AdminModels",
+  components: {
+    AdminModelsCompanyItem,
+    RoundedDarkBlueButton
+  },
+  data() {
+    return {
+      companies: {} as Array<AdminCompany>,
+      isFetched: false,
+    }
+  },
+  mounted() {
+    this.fetchCompanies()
+    this.isFetched = true
+  },
+  methods: {
+    fetchCompanies() {
+      axios
+          .get('api/v1/admin/companies/')
+          .then(response => this.companies = response.data)
+          .catch(error => console.log(error))
+    }
+  },
+})
+</script>
+
+<template>
+<div class="admin-models">
+  <div class="admin-models__header">
+    <h1>Select Company to change</h1>
+    <RoundedDarkBlueButton>
+      <span>ADD COMPANY</span>
+    </RoundedDarkBlueButton>
+  </div>
+  <div v-if="isFetched" class="admin-models__content-list">
+    <table>
+      <thead>
+        <tr>
+          
+          <th>Id</th>
+          <th>Title</th>
+          <th>Slug</th>
+          <th>Logo</th>
+          <th>Market</th>
+          <th>Sector</th>
+          <th>Visible</th>
+      
+        </tr>
+      </thead>
+      <tbody>
+        <AdminModelsCompanyItem
+          v-for="company in companies"
+          :key="company.id"
+          :company
+          @click="$emit('openModel', company.uid)"
+        />
+      </tbody>
+      
+    </table>
+  </div>
+
+</div>
+</template>
+
+<style>
+.admin-models {
+  padding: 20px 0;
+}
+.admin-models__header {
+  display: flex;
+  justify-content: space-between;
+}
+.admin-models__content-list {
+  width: 100%;
+  margin-top: 20px;
+}
+.admin-models__content-list table {
+  width: 100%;
+  table-layout: auto;
+  border-spacing: 0;
+  color: #fff;
+}
+.admin-models__content-list table thead {
+  white-space: nowrap;
+  text-align: left;
+}
+.admin-models__content-list table th {
+  font-size: 1.2rem;
+  line-height: 1.5;
+  font-weight: 500;
+  color: #fff;
+  opacity: .5;
+  border-bottom: 1px solid rgba(255, 255, 255, .1);
+  position: sticky;
+  cursor: pointer;
+  background-color: var(--bg-color);
+  padding: 10px 8px;
+}
+.admin-models__content-list table th > * {
+  display: inline-block;
+}
+.admin-models__content-list table th.admin-models__content-list table__empty {
+  cursor: default;
+}
+.admin-models__content-list table th:hover {
+  background-color: var(--header-color);
+}
+.admin-models__content-list table th.admin-models__content-list table__empty:hover {
+  background-color: var(--bg-color);
+}
+.admin-models__content-list table th:first-child {
+  height: 55px;
+  width: 56px;
+}
+.admin-models__content-list table th:nth-child(2) {
+  width: 116px;
+}
+.admin-models__content-list .table__icon-box {
+  width: 16px;
+  height: 16px;
+  margin-left: 2px;
+}
+.admin-models__content-list table th svg {
+  width: 16px;
+  height: 16px;
+}
+.admin-models__content-list table tr {
+  height: auto;
+}
+.admin-models__content-list table tbody tr  {
+  font-size: 1.4rem;
+  font-weight: normal;
+  line-height: 1.5;
+  transition: background-color .2s cubic-bezier(.23, 1, .32, 1) 0s;
+}
+.admin-models__content-list table tbody tr:hover {
+  background-color: rgba(255, 255, 255, 0.05);
+}
+.admin-models__content-list table td {
+  background-color: transparent;
+  font-size: 1.4rem;
+  height: 64px;
+  border-top: none;
+  border-bottom: 1px solid rgba(255, 255, 255, .1);
+  vertical-align: middle;
+  padding: 10px 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  cursor: pointer;
+}
+</style>
