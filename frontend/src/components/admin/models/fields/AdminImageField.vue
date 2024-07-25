995564f63494032ab976d8b797af3025fb7760c4
Pau4ok <pavel_mal99@mail.ru>
2024-07-25 09:50:50 +0300

    Create AdminImageField.vue

---
+++ frontend/src/components/admin/models/fields/AdminImageField.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AdminImageField",
+  props: {
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<template>
+<div class="admin-image-field">
+  <input
+      type="file"
+      accept="image/*"
+      :required="isRequired"
+      :disabled="isDisabled"
+      @change="(event: Event) => $emit('update:modelValue', (event.target as HTMLInputElement).files[0])"
+  >
+</div>
+</template>
+
+<style scoped>
+
+</style>
