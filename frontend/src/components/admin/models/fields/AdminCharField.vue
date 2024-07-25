3e72fe502e52b74ce25c5f10d155fc18f69ab8a3
Pau4ok <pavel_mal99@mail.ru>
2024-07-25 16:25:11 +0300

    Style admin-char fieldset

--- frontend/src/components/admin/models/fields/AdminCharField.vue
+++ frontend/src/components/admin/models/fields/AdminCharField.vue
@@ -42,5 +42,61 @@ export default defineComponent({
 </template>
 
 <style scoped>
+.admin-char-fieldset {
+  margin: 16px 0;
+}
+.admin-char-field {
+  position: relative;
+  line-height: 1.4rem;
+}
+.admin-char-field__input {
+  position: relative;
+  width: 400px;
+  font-size: 1.4rem;
+  color: #fff;
+  outline: none;
+  padding: 10px 20px;
+  border: 3px solid transparent;
+  border-radius: 20px;
+  background-image: linear-gradient(#1b222d, #1b222d), linear-gradient(315deg, #ee4297, #9176c6);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
 
+  &:disabled {
+    background: #1b222d;
+    border-color: #92969c;
+  }
+  &:placeholder-shown + label {
+    transform: translateY(0);
+  }
+  &:focus {
+    border-color: #2b96f1;
+            box-shadow: inset 1px 2px 4px 0 rgb(179 30 30 / 10%), 3px 5px 12px 2px rgb(43 150 241 / 40%);
+
+    & + label {
+      color: #2b96f1;
+      transform: translateY(-20px);
+    }
+  }
+}
+.admin-char-field__label {
+  color: #fff;
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  position: absolute;
+  z-index: 2;
+  left: 20px;
+  top: 14px;
+  padding: 0 5px;
+  pointer-events: none;
+  background: #1b222d;
+  transition: transform .1s ease;
+  transform: translateY(-20px);
+}
+.admin-char-field__help-text {
+  color: #92969c;
+  font-size: 1.2rem;
+  padding-left: 20px;
+  margin-top: 4px;
+}
 </style>
