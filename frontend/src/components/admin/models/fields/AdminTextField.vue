f0559c136b09444e013e03ab75ccdd0a0c196221
Pau4ok <pavel_mal99@mail.ru>
2024-07-25 09:51:35 +0300

    Create AdminTextField.vue

---
+++ frontend/src/components/admin/models/fields/AdminTextField.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AdminTextField",
+  props: {
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<template>
+<div class="admin-text-field">
+  <textarea
+      :required="isRequired"
+      :disabled="isDisabled"
+      @input="(event: Event) => $emit('update:modelValue', (event.target as HTMLInputElement).value)"
+      :value="($attrs.modelValue as any)"
+      placeholder="Введите несколько строчек"
+  />
+</div>
+</template>
+
+<style scoped>
+
+</style>
