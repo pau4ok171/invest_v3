e86c886ca6c2e7ee41ade0fc6c9d9cee703c03ac
Pau4ok <pavel_mal99@mail.ru>
2024-07-25 09:50:40 +0300

    Create AdminCharField.vue

---
+++ frontend/src/components/admin/models/fields/AdminCharField.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AdminCharField",
+  props: {
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<template>
+<div class="admin-char-field">
+  <input
+      :required="isRequired"
+      :disabled="isDisabled"
+      :value="$attrs.modelValue"
+      @input="(event: Event) => $emit('update:modelValue', (event.target as HTMLInputElement).value)"
+      placeholder="Введите несколько строчек"
+  />
+</div>
+</template>
+
+<style scoped>
+
+</style>
