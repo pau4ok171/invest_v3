6c38f2fd9f9fe22a80f27cce57fe65fa3b355a66
Pau4ok <pavel_mal99@mail.ru>
2024-07-26 16:22:04 +0300

    Add styles

--- frontend/src/components/admin/models/fields/AdminSelectorField.vue
+++ frontend/src/components/admin/models/fields/AdminSelectorField.vue
@@ -88,5 +88,65 @@ export default defineComponent({
 </template>
 
 <style scoped>
+.admin-selector-fieldset {
+  margin: 16px 0;
+}
+.admin-selector-field {
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+  width: 280px;
+  padding: 6px 20px;
 
+  outline: none;
+  border: 3px solid transparent;
+  border-radius: 20px;
+  background-image: linear-gradient(#1b222d, #1b222d), linear-gradient(315deg, #ee4297, #9176c6);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+
+  font-size: 1.4rem;
+  color: #fff;
+
+  & > * {
+    pointer-events: none;
+  }
+  &:disabled {
+    background: #1b222d;
+    border-color: #92969c;
+  }
+  &:focus {
+    border-color: #2b96f1;
+    box-shadow: inset 1px 2px 4px 0 rgb(179 30 30 / 10%), 3px 5px 12px 2px rgb(43 150 241 / 40%);
+
+    & + label {
+      color: #2b96f1;
+      transform: translateY(-20px);
+    }
+  }
+}
+.admin-selector-field__label {
+  color: #fff;
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  position: absolute;
+  z-index: 2;
+  left: 20px;
+  top: 14px;
+  padding: 0 5px;
+  pointer-events: none;
+  background: #1b222d;
+  transition: transform .1s ease;
+  transform: translateY(-20px);
+}
+.admin-selector-field__label--active {
+    transform: translateY(0);
+  }
+.admin-selector-field__help-text {
+  color: #92969c;
+  font-size: 1.2rem;
+  padding-left: 20px;
+  margin-top: 4px;
+}
 </style>
