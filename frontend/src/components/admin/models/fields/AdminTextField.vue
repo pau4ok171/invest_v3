965a56c47b942f63271361f4a08b322bfffb6e3a
Pau4ok <pavel_mal99@mail.ru>
2024-07-26 13:46:37 +0300

    Create text field

--- frontend/src/components/admin/models/fields/AdminTextField.vue
+++ frontend/src/components/admin/models/fields/AdminTextField.vue
@@ -24,14 +24,23 @@ export default defineComponent({
 </script>
 
 <template>
-<div class="admin-text-field">
-  <textarea
-      :required="isRequired"
-      :disabled="isDisabled"
-      @input="(event: Event) => $emit('update:modelValue', (event.target as HTMLInputElement).value)"
-      :value="($attrs.modelValue as any)"
-      placeholder="Введите несколько строчек"
-  />
+<div class="admin-text-fieldset">
+  <div class="admin-text-field">
+    <div class="admin-text-field__tab"></div>
+    <textarea
+        :required="isRequired"
+        :disabled="isDisabled"
+        @input="(event: Event) => $emit('update:modelValue', (event.target as HTMLInputElement).value)"
+        :value="($attrs.modelValue as any)"
+        class="admin-text-field__textarea"
+        cols="50"
+        rows="15"
+        placeholder=" "
+    />
+    <label class="admin-text-field__label">{{ label }}{{ isRequired?'*':'' }}</label>
+  </div>
+
+  <div v-if="helpText" class="admin-text-field__help-text">{{ helpText }}</div>
 </div>
 </template>
 
