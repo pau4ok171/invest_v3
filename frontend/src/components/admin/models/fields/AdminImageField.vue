b6ec7aa353b52714ee1db4737d0d8f9f1684a3bc
Pau4ok <pavel_mal99@mail.ru>
2024-08-01 21:51:28 +0300

    Create image field element

--- frontend/src/components/admin/models/fields/AdminImageField.vue
+++ frontend/src/components/admin/models/fields/AdminImageField.vue
@@ -30,14 +30,45 @@ export default defineComponent({
 </script>
 
 <template>
-<div class="admin-image-field">
-  <input
-      type="file"
-      accept="image/*"
-      :required="isRequired"
-      :disabled="isDisabled"
-      @change="(event: Event) => $emit('update:modelValue', (event.target as HTMLInputElement).files[0])"
-  >
+<div class="admin-image-fieldset">
+  <div class="admin-image-field">
+
+    <div
+        class="admin-image-field__input-box"
+        :class="{'admin-image-field__input-box--drag': isDrugOver, 'admin-image-field__input-box--filled': modelValue.size}"
+        @dragover="isDrugOver = true"
+        @dragleave="isDrugOver = false"
+    >
+
+      <template v-if="!modelValue?.size">
+        <input
+            class="admin-image-field__input"
+            id="admin-image-field"
+            type="file"
+            title=""
+            accept="image/*"
+            :required="isRequired"
+            :disabled="isDisabled"
+            @change="uploadFile"
+        >
+        <UploadIcon class="admin-image-field__icon"/>
+      </template>
+
+      <template v-else>
+        <img class="admin-image-field__logo" :src="getLogoURL()" alt="logo">
+        <RoundedBlueButton
+            class="admin-image-field__remove-button"
+            @click="removeLogo()"
+        >
+          <DeleteIcon/>
+        </RoundedBlueButton>
+      </template>
+
+    </div>
+
+    <label class="admin-image-field__label">{{ label }}{{ isRequired?'*':'' }}</label>
+  </div>
+  <div v-if="helpText" class="admin-image-field__help-text">{{ helpText }}</div>
 </div>
 </template>
 
