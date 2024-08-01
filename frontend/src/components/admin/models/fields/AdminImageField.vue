d47ddd34abcc113d2237cbd1327e3c84c5aa8102
Pau4ok <pavel_mal99@mail.ru>
2024-08-01 21:51:42 +0300

    Create styles

--- frontend/src/components/admin/models/fields/AdminImageField.vue
+++ frontend/src/components/admin/models/fields/AdminImageField.vue
@@ -72,6 +72,111 @@ export default defineComponent({
 </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.admin-image-fieldset {
+  margin: 16px 0;
+}
+.admin-image-field {
+  position: relative;
+  line-height: 1.4rem;
+}
+.admin-image-field__input {
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  position: absolute;
+  z-index: 2;
+  cursor: pointer;
+}
+.admin-image-field__input-box {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  outline: 2px dashed rgba(35, 148, 223, .5);
+  outline-offset: -16px;
 
+  width: 280px;
+  height: 280px;
+  padding: 10px 20px;
+
+  border: 3px solid transparent;
+  border-radius: 20px;
+  background-image: linear-gradient(#1b222d, #1b222d), linear-gradient(315deg, #ee4297, #9176c6);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  overflow: hidden;
+
+  font-size: 1.4rem;
+  color: #fff;
+  transition: outline .2s, width .5s, height .5s;
+
+  &--drag {
+    outline: 2px dashed rgba(35, 148, 223, 1);
+
+    & > .admin-image-field__icon {
+      fill: rgba(35, 148, 223, 1);
+    }
+  }
+
+  &--filled {
+    width: 166px;
+    height: 166px;
+    outline: none;
+  }
+
+  &:disabled {
+    background: #1b222d;
+    border-color: #92969c;
+  }
+  &:focus {
+    border-color: #2b96f1;
+    box-shadow: inset 1px 2px 4px 0 rgb(179 30 30 / 10%), 3px 5px 12px 2px rgb(43 150 241 / 40%);
+
+    & + label {
+      color: #2b96f1;
+      transform: translateY(-20px);
+    }
+  }
+}
+.admin-image-field__icon {
+  width: 192px;
+  height: 192px;
+  fill: rgba(35, 148, 223, .5);
+  margin-bottom: 20px;
+  user-select: none;
+  transition: outline .2s;
+}
+.admin-image-field__label {
+  color: #fff;
+  border-radius: 20px;
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  position: absolute;
+  z-index: 2;
+  left: 20px;
+  top: 14px;
+  padding: 0 5px;
+  pointer-events: none;
+  background: #1b222d;
+  transition: transform .1s ease;
+  transform: translateY(-20px);
+}
+.admin-image-field__help-text {
+  color: #92969c;
+  font-size: 1.2rem;
+  padding-left: 20px;
+  margin-top: 4px;
+}
+.admin-image-field__logo {
+  width: 160px;
+  height: 160px;
+  background-color: white;
+}
+.admin-image-field__remove-button {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+}
 </style>
