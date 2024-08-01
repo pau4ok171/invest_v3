a222ad5a0b12d021be0ef1a89c3e53aa874cfabc
Pau4ok <pavel_mal99@mail.ru>
2024-08-01 21:53:11 +0300

    Create form data

--- frontend/src/components/admin/models/AdminModelForm.vue
+++ frontend/src/components/admin/models/AdminModelForm.vue
@@ -2,7 +2,39 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "AdminModelForm"
+  name: "AdminModelForm",
+  components: {
+    RoundedDarkBlueButton,
+    AdminImageField,
+    AdminSelectorField,
+    AdminCheckBoxField,
+    AdminTextField,
+    AdminCharField
+  },
+  data() {
+    return {
+      isVisible: false,
+      companyName: '',
+      ticker: '',
+      slug: '',
+      uid: '',
+      country: {name: '', slug: ''} as SelectorOption,
+      market: {name: '', slug: ''} as SelectorOption,
+      sector: {name: '', slug: ''} as SelectorOption,
+      industry: {name: '', slug: ''} as SelectorOption,
+      logo: {} as File,
+      description: '',
+      isFund: false,
+      city: '',
+      website: '',
+      founded: '',
+    }
+  },
+  watch: {
+    ticker(value) {
+      this.slug = value.toLowerCase()
+    },
+  },
 })
 </script>
 
