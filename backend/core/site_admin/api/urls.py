c80d99f2379f265167777c1e5ebdecf6e0411e1d
Pau4ok <pavel_mal99@mail.ru>
2024-08-04 23:16:59 +0300

    Add urls to Company and Selectors fetch

--- backend/core/site_admin/api/urls.py
+++ backend/core/site_admin/api/urls.py
@@ -5,4 +5,6 @@ app_name = 'admin_api'
 
 urlpatterns = [
     path('companies/', views.CompaniesListAPIView.as_view()),
+    path('companies/<slug:company_uid>/', views.CompanyAPIView.as_view()),
+    path('selector_options/', views.SelectorOptionsAPIView.as_view()),
 ]
