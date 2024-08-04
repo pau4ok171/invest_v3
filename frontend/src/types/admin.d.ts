b676baa4b2d3484eada7b462eab0090d15b671a4
Pau4ok <pavel_mal99@mail.ru>
2024-08-04 23:06:32 +0300

    Add types

--- frontend/src/types/admin.d.ts
+++ frontend/src/types/admin.d.ts
@@ -12,27 +12,105 @@ export interface AdminCompany {
   sector_name: string,
 }
 
-export interface AdminDetailCompany {
-  id: number | null,
-  ticker: string | null,
-  slug: string | null,
-  uid: string | null,
-  title: string | null,
-  shortTitle: string | null,
-  isVisible: boolean | null,
-  logo: File | null,
-  isFund: boolean | null,
-  marketName: string | null,
-  sectorName: string | null,
-  industryName: string | null,
-  absoluteURL: string | null,
-  country_flag: string | null,
-  currencySymbol: string | null,
-  created: string | null,
-  updated: string | null,
+export interface FetchedDetailCompany {
+  ticker: string,
+  slug: string,
+  uid: string,
+  title: string,
+  short_title: string,
+  short_title_genitive: string,
+  description: string,
+  short_description: string,
+  city: string,
+  country: FetchedCountry,
+  market: FetchedMarket,
+  sector: FetchedSector,
+  industry: FetchedIndustry,
+  created: string,
+  updated: string,
+  is_visible: boolean,
+  logo: string,
+  is_fund: boolean,
+  website: string,
+  year_founded: string,
 }
 
-export interface SelectorOption {
+export interface FetchedCountry {
+  id: number,
+  name: string,
+  name_iso: string,
+  currency: FetchedCurrency,
+  flag_url: string,
+}
+
+export interface FetchedMarket {
+  title: string,
+  slug: string,
+  country: number,
+}
+
+export interface FetchedSector {
+  id: number,
+  title: string,
+  slug: string,
+}
+
+export interface FetchedIndustry {
+  title: string,
+  slug: string,
+  sector: number,
+}
+
+export interface FetchedCurrency {
+  name: string,
+  symbol: string,
+}
+
+export interface FormattedDetailCompany {
+  ticker: string,
+  slug: string,
+  uid: string,
+  companyName: string,
+  shortCompanyName: string,
+  shortCompanyNameGenitive: string,
+  description: string,
+  shortDescription: string,
+  city: string,
+  country: FormattedCountry,
+  market: FormattedMarket,
+  sector: FormattedSector,
+  industry: FormattedIndustry,
+  created: string,
+  updated: string,
+  isVisible: boolean,
+  logo: File,
+  isFund: boolean,
+  website: string,
+  founded: string,
+}
+
+export interface FormattedSelector {
+  key: number,
   name: string,
   slug: string,
+}
+
+export interface FormattedCountry extends FormattedSelector{
+  flagURL: string,
+  currency: FormattedCurrency,
+}
+
+export interface FormattedMarket extends FormattedSelector{
+
+}
+
+export interface FormattedSector extends FormattedSelector{
+}
+
+export interface FormattedIndustry extends FormattedSelector{
+}
+
+export interface FormattedCurrency {
+  name: string,
+  symbol: string,
 }
