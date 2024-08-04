44087b09fa5a827c983e444904a3609dd3c595df
Pau4ok <pavel_mal99@mail.ru>
2024-08-04 23:08:42 +0300

    Add CompanyFormData for Company object and fetch models from server

--- frontend/src/store/modules/adminModule.ts
+++ frontend/src/store/modules/adminModule.ts
@@ -14,13 +14,148 @@ import _ from "lodash";
 export const adminModule = {
   state: () => ({
       companyUID: '',
+      companyFormData: {
+        ticker: '',
+        slug: '',
+        uid: '',
+        companyName: '',
+        shortCompanyName: '',
+        shortCompanyNameGenitive: '',
+        description: '',
+        shortDescription: '',
+        city: '',
+        country: {name: '', slug: '', key: 0} as FormattedCountry,
+        market: {name: '', slug: '', key: 0} as FormattedMarket,
+        sector: {name: '', slug: '', key: 0} as FormattedSector,
+        industry: {name: '', slug: '', key: 0} as FormattedIndustry,
+        created: '',
+        updated: '',
+        isVisible: false,
+        logo: {} as File,
+        isFund: false,
+        website: '',
+        founded: '',
+      } as FormattedDetailCompany,
   }),
   getters: {},
   mutations: {
       setCompanyUID(state, payload: string) {
         state.companyUID = payload
       },
+      setCompanyFormData(state, payload: FormattedDetailCompany) {
+        state.companyFormData = payload
+      },
+      setCompanyModelIsVisible(state, payload: boolean) {
+        state.companyFormData.isVisible = payload
+      },
+      setCompanyModelCompanyName(state, payload: string) {
+        state.companyFormData.companyName = payload
+      },
+      setCompanyModelShortCompanyName(state, payload: string) {
+        state.companyFormData.shortCompanyName = payload
+      },
+      setCompanyModelShortCompanyNameGenitive(state, payload: string) {
+        state.companyFormData.shortCompanyNameGenitive = payload
+      },
+      setCompanyModelTicker(state, payload: string) {
+        state.companyFormData.ticker = payload
+        state.companyFormData.slug = getSlug(_.kebabCase(payload))
+      },
+      setCompanyModelSlug(state, payload: string) {
+        state.companyFormData.slug = payload
+      },
+      setCompanyModelUID(state, payload: string) {
+        state.companyFormData.uid = payload
+      },
+      setCompanyModelCountry(state, payload: FormattedCountry) {
+        state.companyFormData.market = {name: '', slug: '', key: ''}
+        state.companyFormData.country = payload
+      },
+      setCompanyModelMarket(state, payload: FormattedMarket) {
+        state.companyFormData.market = payload
+      },
+      setCompanyModelSector(state, payload: FormattedSector) {
+        state.companyFormData.industry = {name: '', slug: '', key: ''}
+        state.companyFormData.sector = payload
+      },
+      setCompanyModelIndustry(state, payload: FormattedIndustry) {
+        state.companyFormData.industry = payload
+      },
+      setCompanyModelLogo(state, payload: File) {
+        state.companyFormData.logo = payload
+      },
+      setCompanyModelDescription(state, payload: string) {
+        state.companyFormData.description = payload
+      },
+      setCompanyModelShortDescription(state, payload: string) {
+        state.companyFormData.shorDescription = payload
+      },
+      setCompanyModelIsFund(state, payload: boolean) {
+        state.companyFormData.isFund = payload
+      },
+      setCompanyModelCity(state, payload: string) {
+        state.companyFormData.city = payload
+      },
+      setCompanyModelWebsite(state, payload: string) {
+        state.companyFormData.website = payload
+      },
+      setCompanyModelFounded(state, payload: number) {
+        state.companyFormData.founded = payload
+      },
   },
-  actions: {},
+  actions: {
+    async fetchCompany({state, commit}) {
+      const companyUID = state.companyUID
+      const company: FetchedDetailCompany = await axios
+        .get(`api/v1/admin/companies/${companyUID}`)
+        .then(response => response.data)
+        .catch(error => console.log(error))
+      let file = await fetch(company.logo).then(r => r.blob()).then(blobFile => new File([blobFile], "CompanyLogo", { type: "image/png" }))
+      const companyFormData: FormattedDetailCompany = {
+        ticker: company.ticker,
+        slug: company.slug,
+        uid: company.uid,
+        companyName: company.title,
+        shortCompanyName: company.short_title,
+        shortCompanyNameGenitive: company.short_title_genitive,
+        description: company.description,
+        shortDescription: company.short_description,
+        city: company.city,
+        country: {
+          name: company.country.name,
+          slug: company.country.name_iso,
+          key: company.country.id,
+          currency: {
+            name: company.country.currency.name,
+            symbol: company.country.currency.symbol
+          },
+          flagURL: company.country.flag_url
+        },
+        market: {
+          name: company.market.title,
+          slug: company.market.slug,
+          key: company.market.country
+        },
+        sector: {
+          name: company.sector.title,
+          slug: company.sector.slug,
+          key: company.sector.id
+        },
+        industry: {
+          name: company.industry.title,
+          slug: company.industry.slug,
+          key: company.industry.sector
+        },
+        created: company.created,
+        updated: company.updated,
+        isVisible: company.is_visible,
+        logo: file,
+        isFund: company.is_fund,
+        website: company.website,
+        founded: String(company.year_founded),
+      }
+      commit('setCompanyFormData', companyFormData)
+    },
+  },
   namespaced: true,
 } as Module<any, any>
