fd223ea71585ee1e05d0537c2149dd4f77d9b5c5
Pau4ok <pavel_mal99@mail.ru>
2024-08-20 14:16:39 +0300

    Add post and patch methods for company Models

--- backend/core/site_admin/api/views.py
+++ backend/core/site_admin/api/views.py
@@ -1,7 +1,9 @@
+import datetime
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAdminUser
 from .serializers import (
@@ -10,19 +12,30 @@ from .serializers import (
     CountrySerializer,
     MarketSerializer,
     SectorSerializer,
-    IndustrySerializer
+    IndustrySerializer,
 )
 from invest.models import Company, Country, Market, Sector, Industry
 
 
-class CompaniesListAPIView(ListAPIView):
+class CompaniesListAPIView(ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdminUser]
     serializer_class = CompaniesSerializer
     queryset = Company.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        # Prepare Data
+        data = get_data(request)
 
-class CompanyAPIView(RetrieveAPIView):
+        # Create Serializer
+        serializer = CompanySerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class CompanyAPIView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAdminUser]
     serializer_class = CompanySerializer
@@ -30,6 +43,22 @@ class CompanyAPIView(RetrieveAPIView):
     def get_object(self):
         company_uid = self.kwargs.get('company_uid')
         return Company.objects.get(uid=company_uid) if company_uid else None
+
+    def update(self, request, *args, **kwargs):
+        data = get_data(request)
+
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 
 class SelectorOptionsAPIView(APIView):
@@ -60,3 +89,25 @@ class SelectorOptionsAPIView(APIView):
             },
             status=status.HTTP_200_OK
         )
+
+
+def get_data(request):
+    data = {key: val for key, val in request.data.items() if val}
+
+    if data.get('country_name_iso'):
+        data['country'] = Country.objects.get(name_iso=data.pop('country_name_iso')).pk
+    if data.get('market_slug'):
+        data['market'] = Market.objects.get(slug=data.pop('market_slug')).pk
+    if data.get('sector_slug'):
+        data['sector'] = Sector.objects.get(slug=data.pop('sector_slug')).pk
+    if data.get('industry_slug'):
+        data['industry'] = Industry.objects.get(slug=data.pop('industry_slug')).pk
+    if type(data.get('logo')) == str and type(data.get('logo')) is not None:
+        data.pop('logo')
+
+    data['created_by'] = request.user.pk
+    data['updated_by'] = request.user.pk
+    data['created'] = datetime.datetime.utcnow()
+    data['updated'] = datetime.datetime.utcnow()
+
+    return data
