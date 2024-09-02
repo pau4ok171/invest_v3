5c4fc2bc33fbedd1e3e63c0d0ad2afae1b43f37a
Pau4ok <pavel_mal99@mail.ru>
2024-09-02 21:44:37 +0300

    Add users url to urls.py

--- backend/core/site_admin/api/urls.py
+++ backend/core/site_admin/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('companies/', views.CompaniesListAPIView.as_view()),
     path('companies/<slug:company_uid>/', views.CompanyAPIView.as_view()),
     path('selector_options/', views.SelectorOptionsAPIView.as_view()),
+    path('users/', views.UserRetrieveAPIView.as_view())
 ]
