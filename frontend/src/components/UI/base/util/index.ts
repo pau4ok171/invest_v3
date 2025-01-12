ebc59254094854a1f45d1b05b91e275d02396395
Pau4ok <pavel_mal99@mail.ru>
2025-01-12 17:10:46 +0300

    Add created utils into index.ts

--- frontend/src/components/UI/base/util/index.ts
+++ frontend/src/components/UI/base/util/index.ts
@@ -1,4 +1,10 @@
 export * from './animation'
-export * from './helpers'
+export * from './colorUtils'
+export * from './console'
 export * from './getCurrentInstance'
+export * from './globals'
+export * from './helpers'
+export * from './injectSelf'
+
+
 
