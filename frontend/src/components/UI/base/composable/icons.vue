d3408692765b250e9adc047cb8b3a57368005381
Pau4ok <pavel_mal99@mail.ru>
2025-01-12 17:14:48 +0300

    Create icons logic

---
+++ frontend/src/components/UI/base/composable/icons.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+import type {ComponentPublicInstance, FunctionalComponent, PropType} from "vue";
+
+export type ComposableComponent<Props = any> =
+    | { new (): ComponentPublicInstance<Props> }
+    | FunctionalComponent<Props>
+
+export type IconValue =
+  | string
+  | (string | [path: string, opacity: number])[]
+  | ComposableComponent
+export const IconValue = [String, Function, Object, Array] as PropType<IconValue>
+
+export interface IconProps {
+  tag: string,
+  icon?: IconValue,
+  disabled?: Boolean,
+}
+
+export interface IconAliases {
+  [name: string]: IconValue
+}
+
+type IconComponent = ComposableComponent<IconProps>
+
+export interface IconSet {
+  component: IconComponent
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/iconsets/default-svg.ts
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+
+// Composables
+import { BaseSvgIcon } from ''
+
+// Types
+import type { IconAliases, IconSet } from "@/components/UI/base/composable/icons.vue";
+
+const aliases: IconAliases = {
+
+}
+
+const defaultSet: IconSet = {
+  component: BaseSvgIcon,
+}
+
+export { aliases, defaultSet }
