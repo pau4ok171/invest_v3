c0c25d37ac89dcf500b773d12dfac665da765a05
Pau4ok <pavel_mal99@mail.ru>
2025-01-15 15:31:43 +0300

    Create NavTheme.vue to toggle site global theme

---
+++ frontend/src/components/base/header/navigation/NavTheme.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+// Utilities
+import BaseButton from "@/apps/visagiste/components/BaseButton/BaseButton.vue";
+import {useTheme } from "@/apps/visagiste/composables";
+import {computed} from "vue";
+
+// Types
+
+// Composables
+const theme = useTheme()
+const isDarkTheme = computed(() => theme.global.name.value === 'finargo-dark')
+const changeThemeMode = () => theme.global.name.value = isDarkTheme.value ? 'finargo-light' : 'finargo-dark'
+</script>
+
+<template>
+<div class="nav-theme">
+  <base-button
+    :icon="isDarkTheme ? 'DarkThemeIcon' : 'LightThemeIcon'"
+    variant="text"
+    rounded="x-small"
+    @click="changeThemeMode"
+/>
+</div>
+</template>
+
+<style scoped>
+.nav-theme {
+  display: flex;
+  color: #92969c;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 64px;
+  padding-left: 8px;
+  user-select: none;
+  cursor: pointer;
+}
+</style>
