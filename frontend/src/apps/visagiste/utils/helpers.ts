af8edb47a4cc9a1b221cae4aa1bdf2a2ad7d881c
Pau4ok <pavel_mal99@mail.ru>
2025-01-15 15:24:41 +0300

    Create convertToUnit function into helpers

--- frontend/src/apps/visagiste/utils/helpers.ts
+++ frontend/src/apps/visagiste/utils/helpers.ts
@@ -130,6 +130,20 @@ export function mergeDeep (
   return out
 }
 
+export function convertToUnit (str: number, unit?: string): string
+export function convertToUnit (str: string | number | null | undefined, unit?: string): undefined
+export function convertToUnit (str: string | number | null | undefined, unit = 'px'): string | undefined {
+  if (str === null || str === '') {
+    return undefined
+  } else if (isNaN(+str!)) {
+    return String(str)
+  } else if (!isFinite(+str!)) {
+    return undefined
+  } else {
+    return `${Number(str)}${unit}`
+  }
+}
+
 export function isPlainObject (obj: any): obj is Record<string, any> {
   let proto
   return obj !== null && typeof obj === 'object' && (
