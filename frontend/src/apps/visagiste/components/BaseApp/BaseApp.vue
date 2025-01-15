71a691f329fc4b4bb8454dcab8476da0ec972904
Pau4ok <pavel_mal99@mail.ru>
2025-01-15 15:20:42 +0300

    Create BaseApp component

---
+++ frontend/src/apps/visagiste/components/BaseApp/BaseApp.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import './BaseApp.scss';
+// Utilities
+import { themeProps, provideTheme } from '../../composables/theme';
+
+// Types
+
+// Composables
+const props = defineProps({
+  ...themeProps
+})
+
+const { themeClasses } = provideTheme(props)
+</script>
+
+<template>
+<div
+  :class="[
+    'base-application',
+     themeClasses
+  ]"
+>
+  <div class="base-application__wrap">
+    <slot/>
+  </div>
+</div>
+</template>
