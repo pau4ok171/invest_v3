c5399ac8b6698a0c8072f73580fc48d54524863c
Pau4ok <pavel_mal99@mail.ru>
2025-01-19 19:34:25 +0300

    move main container to new TheMain.vue component

---
+++ frontend/src/components/base/main/TheMain.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+import {mapState} from "vuex";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'TheMain',
+  computed: {
+    ...mapState({
+      lateralMenuIsOpen: "lateralMenuIsOpen",
+    }),
+  },
+})
+</script>
+
+<template>
+<div class="main">
+  <div class="main__inner" :class="{'main__inner--moved': lateralMenuIsOpen}">
+    <slot/>
+  </div>
+</div>
+</template>
+
+<style lang="scss">
+.main {
+  padding: 64px 24px 24px;
+  display: flex;
+  position: relative;
+  flex-wrap: wrap;
+  max-width: 100%;
+  flex: 1 0 auto;
+  justify-content: center;
+  transition: .2s cubic-bezier(.4, 0, .2, 1);
+}
+.main__inner {
+  position: relative;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.main__inner--moved {
+  max-width: 1582px;
+}
+</style>
--- frontend/src/layouts/AppLayoutDefault.vue
+++ frontend/src/layouts/AppLayoutDefault.vue
@@ -1,50 +1,10 @@
-<script lang="ts">
+<script setup lang="ts">
 import TheHeader from '@/components/base/header/TheHeader.vue'
-import {defineComponent} from "vue";
-import {mapState} from "vuex";
-
-export default defineComponent({
-  name: 'AppLayoutDefault',
-  components: {
-    TheHeader,
-  },
-  computed: {
-    ...mapState({
-      lateralMenuIsOpen: "lateralMenuIsOpen",
-    }),
-  },
-})
+import TheMain from "@/components/base/main/TheMain.vue";
 </script>
 
 <template>
 <TheHeader/>
 
-<div class="content">
-  <div class="content__inner" :class="{'content__inner--moved': lateralMenuIsOpen}">
-      <slot/>
-  </div>
-</div>
+<TheMain><slot/></TheMain>
 </template>
-
-<style scoped>
-.content {
-  padding-top: 64px;
-  display: flex;
-  position: relative;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-.content__inner {
-  position: relative;
-  max-width: 1200px;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  color: #fff;
-  padding: 0 24px 24px;
-  transition: all .6s cubic-bezier(.83, 0, .17, 1) 0s;
-}
-.content__inner--moved {
-  max-width: 1582px;
-}
-</style>
