b018c6a1e464b7f2886af34e9f49e3b1f0db00b5
Pau4ok <pavel_mal99@mail.ru>
2025-01-19 22:04:01 +0300

    Create BaseTable component

---
+++ frontend/src/apps/visagiste/components/BaseTable/BaseTable.vue
@@ -0,0 +1,74 @@
+<script lang="ts">
+// Styles
+import './BaseTable.scss';
+
+// Composables
+import { themeProps, provideTheme } from "@/apps/visagiste/composables/theme";
+import { densityProps, useDensity } from "@/apps/visagiste/composables/density";
+
+// Utilities
+import { defineComponent } from "vue";
+import {convertToUnit} from "@/apps/visagiste/utils";
+
+export default defineComponent({
+  name: 'BaseTable',
+  props: {
+    fixedHeader: Boolean,
+    fixedFooter: Boolean,
+    height: [Number, String],
+    hover: Boolean,
+    ...themeProps,
+    ...densityProps,
+  },
+  methods: {
+    convertToUnit
+  },
+  setup(props) {
+    const { themeClasses } = provideTheme(props)
+    const { densityClasses } = useDensity(props)
+
+    return {
+      themeClasses,
+      densityClasses,
+    }
+  },
+})
+
+</script>
+
+<template>
+<div
+  :class="[
+    'base-table',
+    {
+      'base-table--fixed-height': height,
+      'base-table--fixed-header': fixedHeader,
+      'base-table--fixed-footer': fixedFooter,
+      'base-table--has-top': $slots.top,
+      'base-table--has-bottom': $slots.bottom,
+      'base-table--hover': hover,
+    },
+    themeClasses,
+    densityClasses,
+  ]"
+>
+  <slot name="top"/>
+
+  <template v-if="$slots.default">
+    <div
+      class="base-table__wrapper"
+      :style="{height: convertToUnit(height)}"
+    >
+      <table>
+        <slot/>
+      </table>
+    </div>
+  </template>
+
+  <template v-else>
+    <slot name="wrapper"/>
+  </template>
+
+  <slot name="bottom"/>
+</div>
+</template>
