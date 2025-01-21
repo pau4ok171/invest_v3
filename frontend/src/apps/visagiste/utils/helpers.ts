c95caab5981b9934002349103a55feb14466c379
Pau4ok <pavel_mal99@mail.ru>
2025-01-21 10:38:10 +0300

    Create function getNestedValue, Create function getObjectValueByPath,

--- frontend/src/apps/visagiste/utils/helpers.ts
+++ frontend/src/apps/visagiste/utils/helpers.ts
@@ -2,6 +2,31 @@ import type {ComputedGetter, PropType, ToRefs} from "vue";
 import {computed, reactive, toRefs, watchEffect} from "vue";
 
 
+export function getNestedValue (obj: any, path: (string | number)[], fallback?: any): any {
+  const last = path.length - 1
+  if (last < 0) return obj === undefined ? fallback : obj
+
+  for (let i = 0; i < last; i++) {
+    if (obj == null) {
+      return fallback
+    }
+    obj = obj[path[i]]
+  }
+
+  if (obj == null) return fallback
+
+  return obj[path[last]] === undefined ? fallback : obj[path[last]]
+}
+
+export function getObjectValueByPath (obj: any, path?: string | null, fallback?: any): any {
+  // credit: http://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key#comment55278413_6491621
+  if (obj == null || !path || typeof path !== 'string') return fallback
+  if (obj[path] !== undefined) return obj[path]
+  path = path.replace(/\[(\w+)\]/g, '.$1') // convert indexes to properties
+  path = path.replace(/^\./, '') // strip a leading dot
+  return getNestedValue(obj, path.split('.'), fallback)
+}
+
 export type EventProp<T extends any[] = any[], F = (...args: T) => void> = F
 export const EventProp = <T extends any[] = any[]>() => [Function, Array] as PropType<EventProp<T>>
 
