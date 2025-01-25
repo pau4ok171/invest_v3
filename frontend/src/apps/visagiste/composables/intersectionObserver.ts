47686aed9cfe67be7d061b7a78fad286ef3cd2ae
Pau4ok <pavel_mal99@mail.ru>
2025-01-25 15:18:29 +0300

    create intersectionObserver composable

---
+++ frontend/src/apps/visagiste/composables/intersectionObserver.ts
@@ -0,0 +1,29 @@
+// Utilities
+import {onBeforeUnmount, ref, shallowRef, watch} from "vue";
+import {SUPPORTS_INTERSECTION} from "@/apps/visagiste/utils";
+
+export function useIntersectionObserver(callback?: IntersectionObserverCallback, options?: IntersectionObserverInit) {
+  const intersectionRef = ref<HTMLElement>();
+  const isIntersecting = shallowRef(false);
+  if (SUPPORTS_INTERSECTION) {
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      callback?.(entries, observer);
+
+      isIntersecting.value = !!entries.find(entry => entry.isIntersecting);
+    }, options);
+
+    onBeforeUnmount(() => {
+      observer.disconnect();
+    });
+    watch(intersectionRef, (newValue, oldValue) => {
+      if (oldValue) {
+        observer.unobserve(oldValue);
+        isIntersecting.value = false;
+      }
+      if (newValue) observer.observe(newValue);
+    }, {
+      flush: 'post',
+    });
+  }
+  return { intersectionRef, isIntersecting }
+}
