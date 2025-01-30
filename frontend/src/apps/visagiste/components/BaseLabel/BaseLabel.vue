b88e7f8496d110a0e78ab0f0ac4c601f22b3c5ff
Pau4ok <pavel_mal99@mail.ru>
2025-01-30 12:14:49 +0300

    create BaseLabel component

---
+++ frontend/src/apps/visagiste/components/BaseLabel/BaseLabel.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+// Styles
+import './_variables.scss';
+
+// Composables
+import {useComponentProps} from "@/apps/visagiste/composables/component";
+import {useThemeProps} from "@/apps/visagiste/composables/theme";
+
+// Utilities
+import {defineComponent, EventProp, propsFactory} from '@/apps/visagiste/utils';
+
+export const useBaseLabelProps = propsFactory({
+  text: String,
+  onClick: EventProp<[MouseEvent]>(),
+
+  ...useComponentProps(),
+  ...useThemeProps(),
+}, 'BaseLabel')
+
+export default defineComponent({
+  name: "BaseLabel",
+  props: useBaseLabelProps(),
+})
+</script>
+
+<template>
+<label
+  :class="[
+    'base-label',
+    {
+      'base-label--clickable': !!$props.onClick,
+    },
+    $props.class,
+  ]"
+  :style="$props.style"
+  @click="onClick"
+>
+  {{ $props.text }}
+
+  <slot/>
+</label>
+</template>
