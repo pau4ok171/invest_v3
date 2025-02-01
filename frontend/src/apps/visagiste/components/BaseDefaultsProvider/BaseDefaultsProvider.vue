8765ba41416b18243dfdcecc9f54f3aa78549dfc
Pau4ok <pavel_mal99@mail.ru>
2025-02-01 20:28:19 +0300

    create BaseDefaultsProvider component

---
+++ frontend/src/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+// Composables
+import {provideDefaults} from "@/apps/visagiste/composables/defaults";
+
+// Utilities
+import {toRefs} from "vue";
+import {defineComponent, propsFactory} from '@/apps/visagiste/utils';
+
+// Types
+import type {PropType} from "vue";
+import type {DefaultsOptions} from "@/apps/visagiste/composables/defaults";
+
+export const useBaseDefaultsProviderProps = propsFactory({
+  defaults: Object as PropType<DefaultsOptions>,
+  disabled: Boolean,
+  reset: [Number, String],
+  root: [Boolean, String],
+  scoped: Boolean,
+}, 'BaseDefaultsProvider')
+
+export default defineComponent({
+  name: "BaseDefaultsProvider",
+  props: useBaseDefaultsProviderProps(),
+  setup (props, { slots }) {
+    const { defaults, disabled, reset, root, scoped } = toRefs(props)
+
+    provideDefaults(defaults, {
+      reset,
+      root,
+      scoped,
+      disabled,
+    })
+
+    return () => slots.default?.()
+  }
+})
+</script>
