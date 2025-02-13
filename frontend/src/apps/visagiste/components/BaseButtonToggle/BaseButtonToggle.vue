eedf803938656d81c2e6c0b2be82583cfb01e84a
Pau4ok <pavel_mal99@mail.ru>
2025-02-13 17:41:50 +0300

    create BaseButtonToggle component

---
+++ frontend/src/apps/visagiste/components/BaseButtonToggle/BaseButtonToggle.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+// Styles
+import './BaseButtonToggle.scss'
+
+// Components
+import BaseButtonGroup, { useBaseButtonGroupProps } from '../BaseButtonGroup/BaseButtonGroup.vue'
+
+// Composables
+import {useGroup, useGroupProps} from "@/apps/visagiste/composables/group";
+
+// Utilities
+import {h} from "vue";
+import {defineComponent, propsFactory} from "@/apps/visagiste/utils";
+
+// Types
+import type {InjectionKey} from "vue";
+import type {GroupProvide} from "@/apps/visagiste/composables/group";
+
+export type ButtonToggleSlotProps = 'isSelected' | 'select' | 'selected' | 'next' | 'prev'
+export interface DefaultButtonToggleSlot extends Pick<GroupProvide, ButtonToggleSlotProps> {}
+
+export const BaseButtonToggleSymbol: InjectionKey<GroupProvide> = Symbol.for('visagiste:base-button-toggle')
+
+type BaseButtonToggleSlots = {
+  default: DefaultButtonToggleSlot
+}
+
+export const useBaseButtonToggleProps = propsFactory({
+  ...useBaseButtonGroupProps(),
+  ...useGroupProps(),
+}, 'BaseButtonToggle')
+
+export default defineComponent({
+  name: "BaseButtonToggle",
+  props: useBaseButtonToggleProps(),
+  setup (props, { slots }) {
+    const { isSelected, next, prev, select, selected } = useGroup(props, BaseButtonToggleSymbol)
+    return () => {
+      const buttonToggleProps = BaseButtonGroup.filterProps(props)
+
+      return h(
+        BaseButtonGroup,
+        {
+          class: ['base-button-toggle', props.class],
+          style: props.style,
+          ...buttonToggleProps,
+        },
+        {
+          default: () => slots.default?.(isSelected, next, prev, select, selected)
+        }
+      )
+    }
+  }
+
+})
+</script>
