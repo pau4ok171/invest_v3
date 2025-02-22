69bbba21474be78c8acea154c84342fd4fec52e6
Pau4ok <pavel_mal99@mail.ru>
2025-02-22 13:50:52 +0300

    refactor using BaseDialog and Base Menu

--- frontend/src/components/base/header/navigation/NavUser.vue
+++ frontend/src/components/base/header/navigation/NavUser.vue
@@ -1,64 +1,131 @@
 <script lang="ts">
-import NavUserDropDown from "@/components/base/header/navigation/NavUserDropDown.vue";
+// Components
 import AuthModalMenu from "@/components/base/auth/AuthModalMenu.vue";
-import {mapGetters} from "vuex";
-import {defineComponent} from "vue";
-import BaseButton from "@/apps/visagiste/components/BaseButton/BaseButton.vue";
-import BaseMenu from "@/apps/visagiste/components/BaseMenu/BaseMenu.vue";
-import BaseDialog from "@/apps/visagiste/components/BaseDialog/BaseDialog.vue";
+
+// Base Components
+import { BaseButton } from "@/apps/visagiste/components/BaseButton";
+import { BaseMenu } from "@/apps/visagiste/components/BaseMenu";
+import { BaseDialog } from "@/apps/visagiste/components/BaseDialog";
+import { BaseCard } from "@/apps/visagiste/components/BaseCard/";
+import { BaseList } from "@/apps/visagiste/components/BaseList";
+import { BaseListItem } from "@/apps/visagiste/components/BaseList";
+import { BaseDivider } from "@/apps/visagiste/components/BaseDivider";
+import { BaseToolbar } from "@/apps/visagiste/components/BaseToolbar";
+import { BaseToolbarTitle } from "@/apps/visagiste/components/BaseToolbar";
+
+// Utilities
+import { defineComponent } from "vue";
+import axios from "axios";
+import { mapGetters, mapMutations, mapState } from "vuex";
 
 export default defineComponent({
-  name: 'NavUser',
+  name: "NavUser",
   components: {
+    BaseToolbarTitle,
+    BaseToolbar,
+    BaseDivider,
+    BaseListItem,
+    BaseList,
+    BaseCard,
     BaseDialog,
     BaseMenu,
     BaseButton,
     AuthModalMenu,
-    NavUserDropDown,
+  },
+  data() {
+    return {
+      items: [
+        { title: "Profile", to: "/profile", id: "profile" },
+        { title: "Plan & Pricing", to: "/pricing", id: "pricing" },
+        { title: "Notifications", to: "/notifications", id: "notifications" },
+        { title: "HelpCenter", to: "/helpcenter", id: "help" },
+      ],
+      dialog: false,
+    };
   },
   computed: {
+    ...mapState({
+      userInfo: (state: any) => state.authModule.userInfo,
+    }),
     ...mapGetters({
-      isAuthenticated: 'authModule/getIsAuthenticated',
-    })
+      isAuthenticated: "authModule/getIsAuthenticated",
+    }),
   },
-})
+  methods: {
+    ...mapMutations({
+      removeToken: "authModule/removeToken",
+    }),
+    logout() {
+      axios.defaults.headers.common["Authorization"] = "";
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      localStorage.removeItem("userid");
+
+      this.removeToken();
+      document.location.reload();
+    },
+  },
+});
 </script>
 
 <template>
-<div class="nav-user__wrapper">
-  <template v-if="isAuthenticated">
-    <base-menu>
-      <template #activator>
-        <base-button
-          icon="UserIcon"
-          variant="text"
-          rounded="x-small"
-        />
-      </template>
-      <template #list>
-        <NavUserDropDown/>
-      </template>
-    </base-menu>
-  </template>
+  <div class="nav-user__wrapper">
+    <template v-if="isAuthenticated">
+      <base-menu>
+        <template #activator="{ props }">
+          <base-button
+            v-bind="props"
+            icon="$iUser"
+            variant="text"
+            size="large"
+            rounded="lg"
+          />
+        </template>
+        <template #default>
+          <base-card>
+            <base-list>
+              <base-list-item
+                v-if="userInfo.is_staff"
+                :to="{ name: 'admin' }"
+                title="Admin Panel"
+              />
+              <base-list-item
+                v-for="item in items"
+                :key="item.id"
+                :title="item.title"
+                :to="item.to"
+              />
+              <base-divider />
+              <base-list-item @click="logout" title="Logout" />
+            </base-list>
+          </base-card>
+        </template>
+      </base-menu>
+    </template>
 
-  <template v-else>
-    <base-dialog
-      max-width="700"
-      footer-type="withoutFooter"
-    >
-      <template #activator>
-        <base-button
-          text="login"
-          rounded="large"
-          theme="blue"
-        />
-      </template>
-      <template #dialog>
-        <AuthModalMenu/>
-      </template>
-    </base-dialog>
-  </template>
-</div>
+    <template v-else>
+      <base-dialog v-model="dialog" max-width="700">
+        <template #activator="{ props: activatorProps }">
+          <base-button
+            text="login"
+            rounded="large"
+            color="blue"
+            v-bind="activatorProps"
+          />
+        </template>
+        <template #default>
+          <base-card>
+            <base-toolbar>
+              <base-toolbar-title>Finargo</base-toolbar-title>
+              <base-button icon="$close" variant="text" @click="dialog = false" />
+            </base-toolbar>
+            <AuthModalMenu />
+          </base-card>
+        </template>
+      </base-dialog>
+    </template>
+  </div>
 </template>
 
 <style>
@@ -67,5 +134,4 @@ export default defineComponent({
   justify-content: center;
   min-width: 90px;
 }
-
 </style>
