d6944d10c0291f0997a5e9eb533a487604fe4cc3
Pau4ok <pavel_mal99@mail.ru>
2025-02-26 13:37:06 +0300

    add BaseDialogTransition export

--- frontend/src/apps/visagiste/components/transitions/index.ts
+++ frontend/src/apps/visagiste/components/transitions/index.ts
@@ -10,6 +10,8 @@ export const BaseFadeTransition = createCssTransition('fade-transition')
 export const BaseExpandTransition = createJavascriptTransition('expand-transition', ExpandTransitionGenerator())
 export const BaseExpandXTransition = createJavascriptTransition('expand-x-transition', ExpandTransitionGenerator('', true))
 
+export { default as BaseDialogTransition } from './dialog-transition'
+
 export type BaseExpandTransition = InstanceType<typeof BaseExpandTransition>
 export type BaseExpandXTransition = InstanceType<typeof BaseExpandXTransition>
 export type BaseSlideYTransition = InstanceType<typeof BaseSlideYTransition>
