67fdb89d4ada2e2a1def59a25abf3b3f1f41adab
Pau4ok <pavel_mal99@mail.ru>
2025-02-27 12:50:38 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseField/BaseField.vue
+++ frontend/src/apps/visagiste/components/BaseField/BaseField.vue
@@ -1,39 +1,36 @@
 <script lang="ts">
 // Styles
-import "./BaseField.scss";
+import './BaseField.scss'
 
 // Components
-import BaseFieldLabel from "./BaseFieldLabel.vue";
-import { BaseExpandXTransition } from "@/apps/visagiste/components/transitions";
-import BaseDefaultsProvider from "@/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue";
-import { useInputIcon } from "@/apps/visagiste/components/BaseInput/InputIcon.vue";
+import BaseFieldLabel from './BaseFieldLabel.vue'
+import { BaseExpandXTransition } from '@/apps/visagiste/components/transitions'
+import BaseDefaultsProvider from '@/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue'
+import { useInputIcon } from '@/apps/visagiste/components/BaseInput/InputIcon.vue'
 
 // Composables
-import { useFocus, useFocusProps } from "@/apps/visagiste/composables/focus";
-import {
-  provideTheme,
-  useThemeProps,
-} from "@/apps/visagiste/composables/theme";
+import { useFocus, useFocusProps } from '@/apps/visagiste/composables/focus'
+import { provideTheme, useThemeProps } from '@/apps/visagiste/composables/theme'
 import {
   useBackgroundColor,
   useTextColor,
-} from "@/apps/visagiste/composables/color";
-import { useComponentProps } from "@/apps/visagiste/composables/component";
+} from '@/apps/visagiste/composables/color'
+import { useComponentProps } from '@/apps/visagiste/composables/component'
 import {
-  LoaderSlot,
+  default as LoaderSlot,
   useLoader,
   useLoaderProps,
-} from "@/apps/visagiste/composables/loader";
+} from '@/apps/visagiste/composablesV2/loader.vue'
 import {
   useRounded,
   useRoundedProps,
-} from "@/apps/visagiste/composables/rounded";
-import { useRtl } from "@/apps/visagiste/composables";
-import { IconValue } from "@/apps/visagiste/composables/icons";
-import { useSlotIsEmpty } from "@/apps/visagiste/composables/slotIsEmpty";
+} from '@/apps/visagiste/composables/rounded'
+import { useRtl } from '@/apps/visagiste/composables'
+import { IconValue } from '@/apps/visagiste/composables/icons'
+import { useSlotIsEmpty } from '@/apps/visagiste/composables/slotIsEmpty'
 
 // Utilities
-import { computed, ref, watch } from "vue";
+import { computed, Fragment, h, ref, Text, watch } from 'vue'
 import {
   animate,
   convertToUnit,
@@ -43,33 +40,33 @@ import {
   nullifyTransforms,
   propsFactory,
   standardEasing,
-} from "@/apps/visagiste/utils";
+} from '@/apps/visagiste/utils'
 
 // Types
-import type { PropType, SlotsType, Ref } from "vue";
-import type { LoaderSlotProps } from "@/apps/visagiste/composables/loader";
+import type { PropType, Ref, SlotsType } from 'vue'
+import type { LoaderSlotProps } from '@/apps/visagiste/composablesV2/loader.vue'
 
 const allowedVariants = [
-  "underlined",
-  "outlined",
-  "filled",
-  "solo",
-  "solo-inverted",
-  "solo-filled",
-  "plain",
-] as const;
-type Variant = (typeof allowedVariants)[number];
+  'underlined',
+  'outlined',
+  'filled',
+  'solo',
+  'solo-inverted',
+  'solo-filled',
+  'plain',
+] as const
+type Variant = (typeof allowedVariants)[number]
 
 export interface DefaultInputSlot {
-  isActive: Ref<boolean>;
-  isFocused: Ref<boolean>;
-  controlRef: Ref<HTMLElement | undefined>;
-  focus: () => void;
-  blur: () => void;
+  isActive: Ref<boolean>
+  isFocused: Ref<boolean>
+  controlRef: Ref<HTMLElement | undefined>
+  focus: () => void
+  blur: () => void
 }
 
 export interface BaseFieldSlot extends DefaultInputSlot {
-  props: Record<string, unknown>;
+  props: Record<string, unknown>
 }
 
 export const useBaseFieldProps = propsFactory(
@@ -79,7 +76,7 @@ export const useBaseFieldProps = propsFactory(
     clearable: Boolean,
     clearIcon: {
       type: IconValue,
-      default: "$clear",
+      default: '$clear',
     },
     active: Boolean,
     centerAffix: {
@@ -102,42 +99,39 @@ export const useBaseFieldProps = propsFactory(
     singleLine: Boolean,
     variant: {
       type: String as PropType<Variant>,
-      default: "filled",
+      default: 'filled',
       validator: (v: any) => allowedVariants.includes(v),
     },
 
-    "onClick:clear": EventProp<[MouseEvent]>(),
-    "onClick:appendInner": EventProp<[MouseEvent]>(),
-    "onClick:prependInner": EventProp<[MouseEvent]>(),
+    'onClick:clear': EventProp<[MouseEvent]>(),
+    'onClick:appendInner': EventProp<[MouseEvent]>(),
+    'onClick:prependInner': EventProp<[MouseEvent]>(),
 
     ...useComponentProps(),
     ...useLoaderProps(),
     ...useRoundedProps(),
     ...useThemeProps(),
   },
-  "BaseField",
-);
+  'BaseField'
+)
 
 export type BaseFieldSlots = {
-  clear: DefaultInputSlot & { props: Record<string, any> };
-  "prepend-inner": DefaultInputSlot;
-  "append-inner": DefaultInputSlot;
+  clear: DefaultInputSlot & { props: Record<string, any> }
+  'prepend-inner': DefaultInputSlot
+  'append-inner': DefaultInputSlot
   label: DefaultInputSlot & {
-    label: string | undefined;
-    props: Record<string, any>;
-  };
-  loader: LoaderSlotProps;
-  default: BaseFieldSlot;
-};
+    label: string | undefined
+    props: Record<string, any>
+  }
+  loader: LoaderSlotProps
+  default: BaseFieldSlot
+}
 
 export default defineComponent({
-  name: "BaseField",
+  name: 'BaseField',
   computed: {
     BaseExpandXTransition() {
-      return BaseExpandXTransition;
-    },
-    LoaderSlot() {
-      return LoaderSlot;
+      return BaseExpandXTransition
     },
   },
   components: {
@@ -153,78 +147,76 @@ export default defineComponent({
   },
   slots: Object as SlotsType<BaseFieldSlots>,
   emits: {
-    "update:focused": (focused: boolean) => true,
-    "update:modelValue": (value: any) => true,
+    'update:focused': (focused: boolean) => true,
+    'update:modelValue': (value: any) => true,
   },
   setup(props, { slots }) {
-    const { themeClasses } = provideTheme(props);
-    const { loaderClasses } = useLoader(props);
-    const { focusClasses, isFocused, blur, focus } = useFocus(props);
-    const { InputIcon } = useInputIcon(props);
-    const { roundedClasses } = useRounded(props);
-    const { rtlClasses } = useRtl();
+    const { themeClasses } = provideTheme(props)
+    const { loaderClasses } = useLoader(props)
+    const { focusClasses, isFocused, blur, focus } = useFocus(props)
+    const { InputIcon } = useInputIcon(props)
+    const { roundedClasses } = useRounded(props)
+    const { rtlClasses } = useRtl()
 
-    const isActive = computed(() => props.dirty || props.active);
-    const isLabelEmpty = useSlotIsEmpty("label");
-    const hasLabel = computed(() => !!props.label || !isLabelEmpty.value);
-    const hasFloatingLabel = computed(
-      () => !props.singleLine && hasLabel.value,
-    );
+    const isActive = computed(() => props.dirty || props.active)
+    const isLabelEmpty = useSlotIsEmpty('label')
+    const hasLabel = computed(() => !!props.label || !isLabelEmpty.value)
+    const hasFloatingLabel = computed(() => !props.singleLine && hasLabel.value)
 
-    const uid = getUid();
-    const id = computed(() => props.id || `input-${uid}`);
-    const messagesId = computed(() => `${id.value}-messages`);
+    const uid = getUid()
+    const id = computed(() => props.id || `input-${uid}`)
+    const messagesId = computed(() => `${id.value}-messages`)
 
-    const labelRef = ref<BaseFieldLabel>();
-    const floatingLabelRef = ref<BaseFieldLabel>();
-    const controlRef = ref<HTMLElement>();
+    const labelRef = ref<InstanceType<typeof BaseFieldLabel>>()
+    const floatingLabelRef = ref<InstanceType<typeof BaseFieldLabel>>()
+    const controlRef = ref<HTMLElement>()
     const isPlainOrUnderlined = computed(() =>
-      ["plain", "underlined"].includes(props.variant),
-    );
+      ['plain', 'underlined'].includes(props.variant)
+    )
 
     const { backgroundColorClasses, backgroundColorStyles } =
-      useBackgroundColor(props, "bgColor");
+      useBackgroundColor(props, 'bgColor')
     const { textColorClasses, textColorStyles } = useTextColor(
       computed(() => {
         return props.error || props.disabled
           ? undefined
           : isActive.value && isFocused.value
             ? props.color
-            : props.baseColor;
-      }),
-    );
+            : props.baseColor
+      })
+    )
 
     watch(
       isActive,
       (val) => {
         if (hasFloatingLabel.value) {
-          const el: HTMLElement = labelRef.value!.$el;
-          const targetEl: HTMLElement = floatingLabelRef.value!.$el;
+          const el: HTMLElement = labelRef.value!.$el
+          const targetEl: HTMLElement = floatingLabelRef.value!.$el
 
           requestAnimationFrame(() => {
-            const rect = nullifyTransforms(el);
-            const targetRect = targetEl.getBoundingClientRect();
+            const rect = nullifyTransforms(el)
+            const targetRect = targetEl.getBoundingClientRect()
 
-            const x = targetRect.x - rect.x;
+            const x = targetRect.x - rect.x
             const y =
-              targetRect.y - rect.y - (rect.height / 2 - targetRect.height / 2);
+              targetRect.y - rect.y - (rect.height / 2 - targetRect.height / 2)
 
-            const targetWidth = targetRect.width / 0.75;
+            const targetWidth = targetRect.width / 0.75
             const width =
               Math.abs(targetWidth - rect.width) > 1
                 ? { maxWidth: convertToUnit(targetWidth) }
-                : undefined;
+                : undefined
 
-            const style = getComputedStyle(el);
-            const targetStyle = getComputedStyle(targetEl);
-            const duration = parseFloat(style.transitionDuration) * 1000 || 150;
+            const style = getComputedStyle(el)
+            const targetStyle = getComputedStyle(targetEl)
+            const duration = parseFloat(style.transitionDuration) * 1000 || 150
             const scale = parseFloat(
-              targetStyle.getPropertyValue("--base-field-label-scale"),
-            );
-            const color = targetStyle.getPropertyValue("color");
+              targetStyle.getPropertyValue('--base-field-label-scale')
+            )
+            const color = targetStyle.getPropertyValue('color')
 
-            el.style.visibility = "visible";
-            targetEl.style.visibility = "hidden";
+            el.style.visibility = 'visible'
+            targetEl.style.visibility = 'hidden'
 
             animate(
               el,
@@ -236,17 +228,17 @@ export default defineComponent({
               {
                 duration,
                 easing: standardEasing,
-                direction: val ? "normal" : "reverse",
-              },
+                direction: val ? 'normal' : 'reverse',
+              }
             ).finished.then(() => {
-              el.style.removeProperty("visibility");
-              targetEl.style.removeProperty("visibility");
-            });
-          });
+              el.style.removeProperty('visibility')
+              targetEl.style.removeProperty('visibility')
+            })
+          })
         }
       },
-      { flush: "post" },
-    );
+      { flush: 'post' }
+    )
 
     const slotProps = computed<DefaultInputSlot>(() => ({
       isActive,
@@ -254,46 +246,50 @@ export default defineComponent({
       controlRef,
       blur,
       focus,
-    }));
+    }))
 
     function onClick(e: MouseEvent) {
       if (e.target !== document.activeElement) {
-        e.preventDefault();
+        e.preventDefault()
       }
     }
 
-    const isOutlined = computed(() => props.variant === "outlined");
-    const isPrependInnerEmpty = useSlotIsEmpty("prepend-inner");
-    const isClearEmpty = useSlotIsEmpty("clear");
-    const isAppendInnerEmpty = useSlotIsEmpty("append-inner");
+    const isOutlined = computed(() => props.variant === 'outlined')
+    const isPrependInnerEmpty = useSlotIsEmpty('prepend-inner')
+    const isClearEmpty = useSlotIsEmpty('clear')
+    const isAppendInnerEmpty = useSlotIsEmpty('append-inner')
     const hasPrepend = computed(
-      () => !isPrependInnerEmpty.value || !!props.prependInnerIcon,
-    );
+      () => !isPrependInnerEmpty.value || !!props.prependInnerIcon
+    )
     const hasClear = computed(
-      () => (!!props.clearable || !isClearEmpty.value) && !props.disabled,
-    );
+      () => (!!props.clearable || !isClearEmpty.value) && !props.disabled
+    )
     const hasAppend = computed(
       () =>
-        !isAppendInnerEmpty.value || !!props.appendInnerIcon || hasClear.value,
-    );
-    const label = computed(() => {
-      if (!isLabelEmpty.value) {
-        return () =>
-          slots.label({
-            ...slotProps.value,
-            label: props.label,
-            props: { for: id.value },
-          });
-      }
-      return () => props.label;
-    });
+        !isAppendInnerEmpty.value || !!props.appendInnerIcon || hasClear.value
+    )
+    const label = computed(() =>
+      !isLabelEmpty.value
+        ? () =>
+            h(
+              Fragment,
+              slots.label?.({
+                ...slotProps.value,
+                label: props.label,
+                props: { for: id.value },
+              })
+            )
+        : () => h(Text, props.label)
+    )
 
     return {
+      labelRef,
+      controlRef,
+      floatingLabelRef,
       InputIcon,
       backgroundColorClasses,
       backgroundColorStyles,
       blur,
-      floatingLabelRef,
       focus,
       focusClasses,
       hasAppend,
@@ -306,7 +302,6 @@ export default defineComponent({
       isOutlined,
       isPlainOrUnderlined,
       label,
-      labelRef,
       loaderClasses,
       messagesId,
       onClick,
@@ -316,9 +311,9 @@ export default defineComponent({
       textColorClasses,
       textColorStyles,
       themeClasses,
-    };
+    }
   },
-});
+})
 </script>
 
 <template>
@@ -387,7 +382,7 @@ export default defineComponent({
       <template
         v-if="
           ['filled', 'solo', 'solo-inverted', 'solo-filled'].includes(
-            $props.variant,
+            $props.variant
           ) && hasFloatingLabel
         "
       >
@@ -435,8 +430,8 @@ export default defineComponent({
           v-show="$props.dirty"
           @mousedown="
             (e: MouseEvent) => {
-              e.preventDefault();
-              e.stopPropagation();
+              e.preventDefault()
+              e.stopPropagation()
             }
           "
         >
