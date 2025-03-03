8018f32cdf3220be31124f1ecdf11c637be956a0
Pau4ok <pavel_mal99@mail.ru>
2025-03-03 09:46:10 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseLabel/BaseLabel.vue
+++ frontend/src/apps/visagiste/components/BaseLabel/BaseLabel.vue
@@ -1,42 +1,49 @@
 <script lang="ts">
 // Styles
-import './_variables.scss';
+import './_variables.scss'
 
 // Composables
-import {useComponentProps} from "@/apps/visagiste/composables/component";
-import {useThemeProps} from "@/apps/visagiste/composables/theme";
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+import { useThemeProps } from '@/apps/visagiste/composables/theme'
 
 // Utilities
-import {defineComponent, EventProp, propsFactory} from '@/apps/visagiste/utils';
+import {
+  defineComponent,
+  EventProp,
+  propsFactory,
+} from '@/apps/visagiste/utils'
 
-export const useBaseLabelProps = propsFactory({
-  text: String,
-  onClick: EventProp<[MouseEvent]>(),
+export const useBaseLabelProps = propsFactory(
+  {
+    text: String,
+    onClick: EventProp<[MouseEvent]>(),
 
-  ...useComponentProps(),
-  ...useThemeProps(),
-}, 'BaseLabel')
+    ...useComponentProps(),
+    ...useThemeProps(),
+  },
+  'BaseLabel'
+)
 
 export default defineComponent({
-  name: "BaseLabel",
+  name: 'BaseLabel',
   props: useBaseLabelProps(),
 })
 </script>
 
 <template>
-<label
-  :class="[
-    'base-label',
-    {
-      'base-label--clickable': !!$props.onClick,
-    },
-    $props.class,
-  ]"
-  :style="$props.style"
-  @click="onClick"
->
-  {{ $props.text }}
+  <label
+    :class="[
+      'base-label',
+      {
+        'base-label--clickable': !!$props.onClick,
+      },
+      $props.class,
+    ]"
+    :style="$props.style"
+    @click="onClick"
+  >
+    {{ $props.text }}
 
-  <slot/>
-</label>
+    <slot />
+  </label>
 </template>
