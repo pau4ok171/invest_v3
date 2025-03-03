b230b823706dd8fd68be8ef644f67a2f6306a3de
Pau4ok <pavel_mal99@mail.ru>
2025-03-03 09:55:56 +0300

    create

--- frontend/src/apps/visagiste/components/BaseToolbar/index.ts
+++ frontend/src/apps/visagiste/components/BaseToolbar/index.ts
@@ -1,2 +1,3 @@
 export { default as BaseToolbar } from './BaseToolbar.vue'
 export { default as BaseToolbarItems } from './BaseToolbarItems.vue'
+export { default as BaseToolbarTitle } from './BaseToolbarTitle.vue'
