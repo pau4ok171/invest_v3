133077b289bd6ab53bf7fb0941bf679d2a959ab7
Pau4ok <pavel_mal99@mail.ru>
2025-03-03 09:51:20 +0300

    prettify

--- frontend/src/apps/visagiste/composables/icons.ts
+++ frontend/src/apps/visagiste/composables/icons.ts
@@ -2,21 +2,37 @@
 import { aliases, mdi } from '@/apps/visagiste/iconsets/mdi'
 
 // Utilities
-import {computed, h, inject, unref} from "vue";
-import {defineComponent, consoleWarn, mergeDeep, propsFactory} from "@/apps/visagiste/utils";
+import { computed, h, inject, unref } from 'vue'
+import {
+  defineComponent,
+  consoleWarn,
+  mergeDeep,
+  propsFactory,
+} from '@/apps/visagiste/utils'
 
 // Types
-import type {ComponentPublicInstance, FunctionalComponent, InjectionKey, PropType, Ref} from "vue";
+import type {
+  ComponentPublicInstance,
+  FunctionalComponent,
+  InjectionKey,
+  PropType,
+  Ref,
+} from 'vue'
 
 export type JSXComponent<Props = any> =
-    | { new (): ComponentPublicInstance<Props> }
-    | FunctionalComponent<Props>
+  | { new (): ComponentPublicInstance<Props> }
+  | FunctionalComponent<Props>
 
 export type IconValue =
   | string
   | (string | [path: string, opacity: number])[]
   | JSXComponent
-export const IconValue = [String, Function, Object, Array] as PropType<IconValue>
+export const IconValue = [
+  String,
+  Function,
+  Object,
+  Array,
+] as PropType<IconValue>
 
 export interface IconAliases {
   [name: string]: IconValue
@@ -58,9 +74,9 @@ export interface IconAliases {
 }
 
 export interface IconProps {
-  tag: string,
-  icon?: IconValue,
-  disabled?: Boolean,
+  tag: string
+  icon?: IconValue
+  disabled?: Boolean
 }
 
 type IconComponent = JSXComponent<IconProps>
@@ -82,17 +98,21 @@ type IconInstance = {
   icon?: IconValue
 }
 
-export const IconSymbol: InjectionKey<InternalIconOptions> = Symbol.for('visagiste:icons')
+export const IconSymbol: InjectionKey<InternalIconOptions> =
+  Symbol.for('visagiste:icons')
 
-export const useIconProps = propsFactory({
-   icon: {
+export const useIconProps = propsFactory(
+  {
+    icon: {
       type: IconValue,
     },
     tag: {
       type: String,
-      required: true
+      required: true,
     },
-}, 'icon')
+  },
+  'icon'
+)
 
 export const BaseComponentIcon = defineComponent({
   name: 'BaseComponentIcon',
@@ -100,13 +120,9 @@ export const BaseComponentIcon = defineComponent({
   setup(props, { slots }) {
     return () => {
       const Icon = props.icon as JSXComponent
-      return h(
-        props.tag,
-        null,
-        props.icon ? h(Icon) : slots.default?.()
-      )
+      return h(props.tag, null, props.icon ? h(Icon) : slots.default?.())
     }
-  }
+  },
 })
 export type BaseComponentIcon = InstanceType<typeof BaseComponentIcon>
 
@@ -117,26 +133,32 @@ export const BaseSvgIcon = defineComponent({
   setup(props, { attrs }) {
     return () => {
       const icon = Array.isArray(props.icon)
-        ? props.icon.map(path => {
+        ? props.icon.map((path) => {
             if (Array.isArray(path)) {
-              return h('path', {d: path[0], fillOpacity: path[1]});
+              return h('path', { d: path[0], fillOpacity: path[1] })
             } else {
-              return h('path', {d: path});
+              return h('path', { d: path })
             }
           })
-         : h('path', { d: props.icon });
+        : h('path', { d: props.icon })
 
-      return h(props.tag, { ...attrs, style: null} ,
-        h('svg', {
-          class: "base-icon__svg",
-          xmlns: "http://www.w3.org/2000/svg",
-          viewBox: "0 0 24 24",
-          role: "img",
-          'aria-hidden': "true",
-        }, icon)
+      return h(
+        props.tag,
+        { ...attrs, style: null },
+        h(
+          'svg',
+          {
+            class: 'base-icon__svg',
+            xmlns: 'http://www.w3.org/2000/svg',
+            viewBox: '0 0 24 24',
+            role: 'img',
+            'aria-hidden': 'true',
+          },
+          icon
+        )
       )
     }
-  }
+  },
 })
 export type BaseSvgIcon = InstanceType<typeof BaseSvgIcon>
 
@@ -160,7 +182,7 @@ export const BaseClassIcon = defineComponent({
 })
 export type BaseClassIcon = InstanceType<typeof BaseClassIcon>
 
-function generateDefaults (): Record<string, IconSet> {
+function generateDefaults(): Record<string, IconSet> {
   return {
     svg: {
       component: BaseSvgIcon,
@@ -172,7 +194,7 @@ function generateDefaults (): Record<string, IconSet> {
 }
 
 // Composables
-export function createIcons (options?: IconOptions) {
+export function createIcons(options?: IconOptions) {
   const sets = generateDefaults()
   const defaultSet = options?.defaultSet ?? 'mdi'
 
@@ -180,14 +202,17 @@ export function createIcons (options?: IconOptions) {
     sets.mdi = mdi
   }
 
-  return mergeDeep({
-    defaultSet,
-    sets,
-    aliases: {
-      ...aliases,
-     // Here can be set default svg path
-    }
-  }, options) as InternalIconOptions
+  return mergeDeep(
+    {
+      defaultSet,
+      sets,
+      aliases: {
+        ...aliases,
+        // Here can be set default svg path
+      },
+    },
+    options
+  ) as InternalIconOptions
 }
 
 export const useIcon = (props: Ref<IconValue | undefined>) => {
@@ -225,7 +250,7 @@ export const useIcon = (props: Ref<IconValue | undefined>) => {
     }
 
     const iconSetName = Object.keys(icons.sets).find(
-      setName => typeof icon === 'string' && icon.startsWith(`${setName}:`)
+      (setName) => typeof icon === 'string' && icon.startsWith(`${setName}:`)
     )
 
     const iconName = iconSetName ? icon.slice(iconSetName.length + 1) : icon
