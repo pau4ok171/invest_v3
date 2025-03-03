9dcf66806d49c8424182cc97c6fa88820e356584
Pau4ok <pavel_mal99@mail.ru>
2025-03-03 09:52:34 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseAvatar/BaseAvatar.vue
+++ frontend/src/apps/visagiste/components/BaseAvatar/BaseAvatar.vue
@@ -1,49 +1,62 @@
 <script lang="ts">
 // Styles
-import './BaseAvatar.scss';
+import './BaseAvatar.scss'
 
 // Components
-import BaseDefaultsProvider from "@/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue";
-import BaseImage from "@/apps/visagiste/components/BaseImage/BaseImage.vue";
-import BaseIcon from "@/apps/visagiste/components/BaseIcon/BaseIcon.vue";
+import BaseDefaultsProvider from '@/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue'
+import BaseImage from '@/apps/visagiste/components/BaseImage/BaseImage.vue'
+import { BaseIcon } from '@/apps/visagiste/components/BaseIcon'
 
 // Composables
-import {IconValue} from "@/apps/visagiste/composables/icons";
-import {useBorder, useBorderProps} from "@/apps/visagiste/composables/border";
-import {useComponentProps} from "@/apps/visagiste/composables/component";
-import {useDensity, useDensityProps} from "@/apps/visagiste/composables/density";
-import {useRounded, useRoundedProps} from "@/apps/visagiste/composables/rounded";
-import {useSize, useSizeProps} from "@/apps/visagiste/composables/size";
-import {useTagProps} from "@/apps/visagiste/composables/tag";
-import {provideTheme, useThemeProps} from "@/apps/visagiste/composables/theme";
-import {useVariant, useVariantProps, genOverlays} from "@/apps/visagiste/composables/variant";
+import { IconValue } from '@/apps/visagiste/composables/icons'
+import { useBorder, useBorderProps } from '@/apps/visagiste/composables/border'
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+import {
+  useDensity,
+  useDensityProps,
+} from '@/apps/visagiste/composables/density'
+import {
+  useRounded,
+  useRoundedProps,
+} from '@/apps/visagiste/composables/rounded'
+import { useSize, useSizeProps } from '@/apps/visagiste/composables/size'
+import { useTagProps } from '@/apps/visagiste/composables/tag'
+import { provideTheme, useThemeProps } from '@/apps/visagiste/composables/theme'
+import {
+  useVariant,
+  useVariantProps,
+  genOverlays,
+} from '@/apps/visagiste/composables/variant'
 
 // Utilities
-import {defineComponent, propsFactory} from "@/apps/visagiste/utils";
+import { defineComponent, propsFactory } from '@/apps/visagiste/utils'
 
-export const useBaseAvatarProps = propsFactory({
-  start: Boolean,
-  end: Boolean,
-  icon: IconValue,
-  image: String,
-  text: String,
+export const useBaseAvatarProps = propsFactory(
+  {
+    start: Boolean,
+    end: Boolean,
+    icon: IconValue,
+    image: String,
+    text: String,
 
-  ...useBorderProps(),
-  ...useComponentProps(),
-  ...useDensityProps(),
-  ...useRoundedProps(),
-  ...useSizeProps(),
-  ...useTagProps(),
-  ...useThemeProps(),
-  ...useVariantProps({ variant: 'flat' } as const),
-}, 'BaseAvatar')
+    ...useBorderProps(),
+    ...useComponentProps(),
+    ...useDensityProps(),
+    ...useRoundedProps(),
+    ...useSizeProps(),
+    ...useTagProps(),
+    ...useThemeProps(),
+    ...useVariantProps({ variant: 'flat' } as const),
+  },
+  'BaseAvatar'
+)
 
 export default defineComponent({
   name: 'BaseAvatar',
-  methods: {genOverlays},
-  components: {BaseDefaultsProvider, BaseIcon, BaseImage},
+  methods: { genOverlays },
+  components: { BaseDefaultsProvider, BaseIcon, BaseImage },
   props: useBaseAvatarProps(),
-  setup (props) {
+  setup(props) {
     const { themeClasses } = provideTheme(props)
     const { borderClasses } = useBorder(props)
     const { colorClasses, colorStyles, variantClasses } = useVariant(props)
@@ -62,61 +75,58 @@ export default defineComponent({
       sizeClasses,
       sizeStyles,
     }
-  }
+  },
 })
-
 </script>
 
 <template>
-<component
-  :is="$props.tag"
-  :class="[
-    'base-avatar',
-    {
-      'base-avatar--start': $props.start,
-      'base-avatar--end': $props.end,
-    },
-    themeClasses,
-    borderClasses,
-    colorClasses,
-    densityClasses,
-    roundedClasses,
-    sizeClasses,
-    variantClasses,
-    $props.class,
-  ]"
-  :style="[
-    colorStyles,
-    sizeStyles,
-    $props.style,
-  ]"
->
-
-  <BaseDefaultsProvider
-    v-if="$slots.default"
-    key="content-defaults"
-    :defaults="{
-      BaseImage: {
-        cover: true,
-        src: $props.image,
+  <component
+    :is="$props.tag"
+    :class="[
+      'base-avatar',
+      {
+        'base-avatar--start': $props.start,
+        'base-avatar--end': $props.end,
       },
-      BaseIcon: {
-        icon: $props.icon,
-      }
-    }"
+      themeClasses,
+      borderClasses,
+      colorClasses,
+      densityClasses,
+      roundedClasses,
+      sizeClasses,
+      variantClasses,
+      $props.class,
+    ]"
+    :style="[colorStyles, sizeStyles, $props.style]"
   >
-    <slot/>
-  </BaseDefaultsProvider>
+    <BaseDefaultsProvider
+      v-if="$slots.default"
+      key="content-defaults"
+      :defaults="{
+        BaseImage: {
+          cover: true,
+          src: $props.image,
+        },
+        BaseIcon: {
+          icon: $props.icon,
+        },
+      }"
+    >
+      <slot />
+    </BaseDefaultsProvider>
 
-  <template v-else>
+    <template v-else>
+      <BaseImage
+        v-if="$props.image"
+        key="image"
+        :src="$props.image"
+        alt=""
+        cover
+      />
 
-    <BaseImage v-if="$props.image" key="image" :src="$props.image" alt="" cover/>
+      <BaseIcon v-else key="icon" :icon="$props.icon" />
+    </template>
 
-    <BaseIcon v-else key="icon" :icon="$props.icon" />
-
-  </template>
-
-  <component :is="genOverlays(false, 'base-avatar')"/>
-
-</component>
+    <component :is="genOverlays(false, 'base-avatar')" />
+  </component>
 </template>
---
+++ frontend/src/apps/visagiste/components/BaseAvatar/index.ts
@@ -0,0 +1,1 @@
+export { default as BaseAvatar, useBaseAvatarProps } from './BaseAvatar.vue'
