b27467b707d45839947b905ff47cc55bf91d17d1
Pau4ok <pavel_mal99@mail.ru>
2025-03-03 09:44:54 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseCheckbox/BaseCheckbox.vue
+++ frontend/src/apps/visagiste/components/BaseCheckbox/BaseCheckbox.vue
@@ -1,37 +1,49 @@
 <script lang="ts">
 // Styles
-import './BaseCheckbox.scss';
+import './BaseCheckbox.scss'
 
 // Components
-import BaseCheckboxButton, { useBaseCheckboxButtonProps } from './BaseCheckboxButton.vue';
-import BaseInput, {useBaseInputProps} from "../BaseInput/BaseInput.vue";
+import BaseCheckboxButton, {
+  useBaseCheckboxButtonProps,
+} from './BaseCheckboxButton.vue'
+import BaseInput, { useBaseInputProps } from '../BaseInput/BaseInput.vue'
 
 // Composables
-import {useFocus} from "@/apps/visagiste/composables/focus";
-import {useProxiedModel} from "@/apps/visagiste/composables/proxiedModel";
+import { useFocus } from '@/apps/visagiste/composables/focus'
+import { useProxiedModel } from '@/apps/visagiste/composables/proxiedModel'
 
 // Utilities
-import {computed} from "vue";
-import {defineComponent, filterInputAttrs, getUid, omit, propsFactory} from "@/apps/visagiste/utils";
+import { computed } from 'vue'
+import {
+  defineComponent,
+  filterInputAttrs,
+  getUid,
+  omit,
+  propsFactory,
+} from '@/apps/visagiste/utils'
 
 // Types
-import type { SlotsType } from 'vue';
-import type {BaseSelectionControlSlots} from "../BaseSelectionControl/BaseSelectionControl.vue";
-import type { BaseInputSlots } from "../BaseInput/BaseInput.vue";
+import type { SlotsType } from 'vue'
+import type { BaseSelectionControlSlots } from '../BaseSelectionControl/BaseSelectionControl.vue'
+import type { BaseInputSlots } from '../BaseInput/BaseInput.vue'
 
-export type BaseCheckboxSlots = Omit<BaseInputSlots, 'default'> & BaseSelectionControlSlots
+export type BaseCheckboxSlots = Omit<BaseInputSlots, 'default'> &
+  BaseSelectionControlSlots
 
-export const useBaseCheckboxProps = propsFactory({
-  ...useBaseInputProps(),
-  ...omit(useBaseCheckboxButtonProps(), ['inline']),
-}, 'BaseCheckbox')
+export const useBaseCheckboxProps = propsFactory(
+  {
+    ...useBaseInputProps(),
+    ...omit(useBaseCheckboxButtonProps(), ['inline']),
+  },
+  'BaseCheckbox'
+)
 
 export default defineComponent({
-  name: "BaseCheckbox",
+  name: 'BaseCheckbox',
   slots: Object as SlotsType<BaseCheckboxSlots>,
   components: {
     BaseCheckboxButton,
-    BaseInput
+    BaseInput,
   },
   inheritAttrs: false,
   props: useBaseCheckboxProps(),
@@ -39,7 +51,7 @@ export default defineComponent({
     'update:modelValue': (value: any) => true,
     'update:focused': (value: any) => true,
   },
-  setup (props, { attrs }) {
+  setup(props, { attrs }) {
     const model = useProxiedModel(props, 'modelValue')
     const { isFocused, focus, blur } = useFocus(props)
 
@@ -61,60 +73,57 @@ export default defineComponent({
       inputProps,
       checkboxProps,
     }
-  }
+  },
 })
 </script>
 
 <template>
-<BaseInput
-  :class="[
-    'base-checkbox',
-    $props.class
-  ]"
-  v-bind="{...rootAttrs, ...inputProps}"
-  v-model="model"
-  :id="id"
-  :focused="isFocused"
-  :stype="$props.style"
->
-  <template #default="{ id, messagesId, isDisabled, isReadonly, isValid }">
-    <BaseCheckboxButton
-      v-model="model"
-      v-bind="{...checkboxProps, ...controlAttrs}"
-      :id="id.value"
-      :aria-describedby="messagesId.value"
-      :disabled="isDisabled.value"
-      :readonly="isReadonly.value"
-      :error="isValid.value === false"
-      @focus="focus"
-      @blur="blur"
-    >
-      <slot name="default"/>
+  <BaseInput
+    :class="['base-checkbox', $props.class]"
+    v-bind="{ ...rootAttrs, ...inputProps }"
+    v-model="model"
+    :id="id"
+    :focused="isFocused"
+    :stype="$props.style"
+  >
+    <template #default="{ id, messagesId, isDisabled, isReadonly, isValid }">
+      <BaseCheckboxButton
+        v-model="model"
+        v-bind="{ ...checkboxProps, ...controlAttrs }"
+        :id="id.value"
+        :aria-describedby="messagesId.value"
+        :disabled="isDisabled.value"
+        :readonly="isReadonly.value"
+        :error="isValid.value === false"
+        @focus="focus"
+        @blur="blur"
+      >
+        <slot name="default" />
 
-      <template #input>
-        <slot name="input"/>
-      </template>
+        <template #input>
+          <slot name="input" />
+        </template>
 
-      <template #label>
-        <slot name="label"/>
-      </template>
-    </BaseCheckboxButton>
-  </template>
+        <template #label>
+          <slot name="label" />
+        </template>
+      </BaseCheckboxButton>
+    </template>
 
-  <template #prepend>
-    <slot name="prepend"/>
-  </template>
+    <template #prepend>
+      <slot name="prepend" />
+    </template>
 
-  <template #append>
-    <slot name="append"/>
-  </template>
+    <template #append>
+      <slot name="append" />
+    </template>
 
-  <template #details>
-    <slot name="details"/>
-  </template>
+    <template #details>
+      <slot name="details" />
+    </template>
 
-  <template #message>
-    <slot name="message"/>
-  </template>
-</BaseInput>
+    <template #message>
+      <slot name="message" />
+    </template>
+  </BaseInput>
 </template>
--- frontend/src/apps/visagiste/components/BaseCheckbox/BaseCheckboxButton.vue
+++ frontend/src/apps/visagiste/components/BaseCheckbox/BaseCheckboxButton.vue
@@ -1,30 +1,35 @@
 <script lang="ts">
 // Components
-import BaseSelectionControl, { useBaseSelectionControlProps } from '@/apps/visagiste/components/BaseSelectionControl/BaseSelectionControl.vue'
+import BaseSelectionControl, {
+  useBaseSelectionControlProps,
+} from '@/apps/visagiste/components/BaseSelectionControl/BaseSelectionControl.vue'
 
 // Composables
-import {useProxiedModel} from "@/apps/visagiste/composables/proxiedModel";
-import {IconValue} from "@/apps/visagiste/composables/icons";
+import { useProxiedModel } from '@/apps/visagiste/composables/proxiedModel'
+import { IconValue } from '@/apps/visagiste/composables/icons'
 
 // Utilities
-import {computed} from "vue";
-import {propsFactory, defineComponent, omit} from "@/apps/visagiste/utils";
+import { computed } from 'vue'
+import { propsFactory, defineComponent, omit } from '@/apps/visagiste/utils'
 
-export const useBaseCheckboxButtonProps = propsFactory({
-  indeterminate: Boolean,
-  indeterminateIcon: {
-    type: IconValue,
-    default: '$checkboxIndeterminate',
+export const useBaseCheckboxButtonProps = propsFactory(
+  {
+    indeterminate: Boolean,
+    indeterminateIcon: {
+      type: IconValue,
+      default: '$checkboxIndeterminate',
+    },
+    ...useBaseSelectionControlProps({
+      falseIcon: '$checkboxOff',
+      trueIcon: '$checkboxOn',
+    }),
   },
-  ...useBaseSelectionControlProps({
-    falseIcon: '$checkboxOff',
-    trueIcon: '$checkboxOn',
-  }),
-}, 'BaseCheckboxButton')
+  'BaseCheckboxButton'
+)
 
 export default defineComponent({
-  name: "BaseCheckboxButton",
-  components: {BaseSelectionControl},
+  name: 'BaseCheckboxButton',
+  components: { BaseSelectionControl },
   props: useBaseCheckboxButtonProps(),
   emits: {
     'update:modelValue': (value: any) => true,
@@ -34,25 +39,23 @@ export default defineComponent({
     const indeterminate = useProxiedModel(props, 'indeterminate')
     const model = useProxiedModel(props, 'modelValue')
 
-    function onChange (v: any) {
+    function onChange(v: any) {
       if (indeterminate.value) {
         indeterminate.value = false
       }
     }
 
     const falseIcon = computed(() => {
-      return indeterminate.value
-        ? props.indeterminateIcon
-        : props.falseIcon
+      return indeterminate.value ? props.indeterminateIcon : props.falseIcon
     })
 
     const trueIcon = computed(() => {
-      return indeterminate.value
-        ? props.indeterminateIcon
-        : props.trueIcon
+      return indeterminate.value ? props.indeterminateIcon : props.trueIcon
     })
 
-    const controlProps = computed(() => omit(BaseSelectionControl.filterProps(props), ['modelValue']))
+    const controlProps = computed(() =>
+      omit(BaseSelectionControl.filterProps(props), ['modelValue'])
+    )
 
     return {
       model,
@@ -62,33 +65,28 @@ export default defineComponent({
       indeterminate,
       controlProps,
     }
-  }
+  },
 })
 </script>
 
 <template>
-<BaseSelectionControl
-  v-bind="controlProps"
-  v-model="model"
-  :class="[
-    'base-checkbox-button',
-    $props.class,
-  ]"
-  :style="$props.style"
-  type="checkbox"
-  @update:modelValue="onChange"
-  :false-icon="falseIcon"
-  :true-icon="trueIcon"
-  :aria-checked="indeterminate ? 'mixed' : undefined"
->
+  <BaseSelectionControl
+    v-bind="controlProps"
+    v-model="model"
+    :class="['base-checkbox-button', $props.class]"
+    :style="$props.style"
+    type="checkbox"
+    @update:modelValue="onChange"
+    :false-icon="falseIcon"
+    :true-icon="trueIcon"
+    :aria-checked="indeterminate ? 'mixed' : undefined"
+  >
+    <template #input>
+      <slot name="input" />
+    </template>
 
-  <template #input>
-    <slot name="input"/>
-  </template>
-
-  <template #label>
-    <slot name="label"/>
-  </template>
-
-</BaseSelectionControl>
+    <template #label>
+      <slot name="label" />
+    </template>
+  </BaseSelectionControl>
 </template>
