a1a4bbbef703a5ea1235a5ebd122fdd27bc535fe
Pau4ok <pavel_mal99@mail.ru>
2025-03-03 09:55:19 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseTable/BaseTable.vue
+++ frontend/src/apps/visagiste/components/BaseTable/BaseTable.vue
@@ -1,74 +1,109 @@
 <script lang="ts">
 // Styles
-import './BaseTable.scss';
+import './BaseTable.scss'
 
 // Composables
-import { useThemeProps, provideTheme } from "@/apps/visagiste/composables/theme";
-import { useDensityProps, useDensity } from "@/apps/visagiste/composables/density";
+import { useThemeProps, provideTheme } from '@/apps/visagiste/composables/theme'
+import {
+  useDensityProps,
+  useDensity,
+} from '@/apps/visagiste/composables/density'
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+import { useTagProps } from '@/apps/visagiste/composables/tag'
 
 // Utilities
-import { defineComponent } from "vue";
-import {convertToUnit} from "@/apps/visagiste/utils";
+import {
+  convertToUnit,
+  defineComponent,
+  propsFactory,
+} from '@/apps/visagiste/utils'
+import { useSlotIsEmpty } from '@/apps/visagiste/composables/slotIsEmpty'
+import { computed } from 'vue'
 
-export default defineComponent({
-  name: 'BaseTable',
-  props: {
+export type BaseTableSlots = {
+  default: never
+  top: never
+  bottom: never
+  wrapper: never
+}
+
+export const useBaseTableProps = propsFactory(
+  {
     fixedHeader: Boolean,
     fixedFooter: Boolean,
     height: [Number, String],
     hover: Boolean,
-    ...useThemeProps(),
+
+    ...useComponentProps(),
     ...useDensityProps(),
+    ...useTagProps(),
+    ...useThemeProps(),
   },
+  'BaseTable'
+)
+
+export default defineComponent({
+  name: 'BaseTable',
+  props: useBaseTableProps(),
   methods: {
-    convertToUnit
+    convertToUnit,
   },
   setup(props) {
     const { themeClasses } = provideTheme(props)
     const { densityClasses } = useDensity(props)
 
+    const topIsEmpty = useSlotIsEmpty('top')
+    const bottomIsEmpty = useSlotIsEmpty('bottom')
+
+    const hasTop = computed(() => !topIsEmpty.value)
+    const hasBottom = computed(() => !bottomIsEmpty.value)
+
     return {
       themeClasses,
       densityClasses,
+      hasTop,
+      hasBottom,
     }
   },
 })
-
 </script>
 
 <template>
-<div
-  :class="[
-    'base-table',
-    {
-      'base-table--fixed-height': height,
-      'base-table--fixed-header': fixedHeader,
-      'base-table--fixed-footer': fixedFooter,
-      'base-table--has-top': $slots.top,
-      'base-table--has-bottom': $slots.bottom,
-      'base-table--hover': hover,
-    },
-    themeClasses,
-    densityClasses,
-  ]"
->
-  <slot name="top"/>
+  <component
+    :is="$props.tag as string"
+    :class="[
+      'base-table',
+      {
+        'base-table--fixed-height': !!$props.height,
+        'base-table--fixed-header': $props.fixedHeader,
+        'base-table--fixed-footer': $props.fixedFooter,
+        'base-table--has-top': hasTop,
+        'base-table--has-bottom': hasBottom,
+        'base-table--hover': $props.hover,
+      },
+      themeClasses,
+      densityClasses,
+      $props.class,
+    ]"
+    :style="$props.style"
+  >
+    <slot name="top" />
 
-  <template v-if="$slots.default">
-    <div
-      class="base-table__wrapper"
-      :style="{height: convertToUnit(height)}"
-    >
-      <table>
-        <slot/>
-      </table>
-    </div>
-  </template>
+    <template v-if="$slots.default">
+      <div
+        class="base-table__wrapper"
+        :style="{ height: convertToUnit($props.height) }"
+      >
+        <table>
+          <slot />
+        </table>
+      </div>
+    </template>
 
-  <template v-else>
-    <slot name="wrapper"/>
-  </template>
+    <template v-else>
+      <slot name="wrapper" />
+    </template>
 
-  <slot name="bottom"/>
-</div>
+    <slot name="bottom" />
+  </component>
 </template>
