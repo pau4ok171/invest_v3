82bbafe43772f0f04da1c56f6303ebd885428954
Pau4ok <pavel_mal99@mail.ru>
2025-03-07 15:06:28 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseSlideGroup/BaseSlideGroup.vue
+++ frontend/src/apps/visagiste/components/BaseSlideGroup/BaseSlideGroup.vue
@@ -2,36 +2,47 @@
 // Styles
 
 // Components
-import { BaseFadeTransition } from "@/apps/visagiste/components/transitions";
+import { BaseFadeTransition } from '@/apps/visagiste/components/transitions'
 
 // Composables
-import {useDisplay, useDisplayProps} from "@/apps/visagiste/composables/display";
-import {useGroup, useGroupProps} from "@/apps/visagiste/composables/group";
-import {useComponentProps} from "@/apps/visagiste/composables/component";
-import {useGoTo} from "@/apps/visagiste/composables/goto";
-import {IconValue} from "@/apps/visagiste/composables/icons";
-import {useTagProps} from "@/apps/visagiste/composables/tag";
-import {useRtl} from "@/apps/visagiste/composables";
-import {useResizeObserver} from "@/apps/visagiste/composables/resizeObserver";
+import {
+  useDisplay,
+  useDisplayProps,
+} from '@/apps/visagiste/composables/display'
+import { useGroup, useGroupProps } from '@/apps/visagiste/composables/group'
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+import { useGoTo } from '@/apps/visagiste/composables/goto'
+import { IconValue } from '@/apps/visagiste/composables/icons'
+import { useTagProps } from '@/apps/visagiste/composables/tag'
+import { useRtl } from '@/apps/visagiste/composables'
+import { useResizeObserver } from '@/apps/visagiste/composables/resizeObserver'
 
 // Utilities
-import {computed, shallowRef, watch} from "vue";
-import {defineComponent, focusableChildren, IN_BROWSER, propsFactory} from '@/apps/visagiste/utils'
+import { computed, shallowRef, watch } from 'vue'
+import {
+  defineComponent,
+  focusableChildren,
+  IN_BROWSER,
+  propsFactory,
+} from '@/apps/visagiste/utils'
 import {
   calculateUpdatedTarget,
   calculateCenteredTarget,
   getOffsetSize,
   getScrollPosition,
-  getScrollSize, getClientSize
-} from "./helpers";
+  getScrollSize,
+  getClientSize,
+} from './helpers'
 
 // Types
-import type {InjectionKey, PropType} from "vue";
-import type {GroupProvide} from "@/apps/visagiste/composables/group";
-import type {GoToOptions} from "@/apps/visagiste/composables/goto";
-import BaseIcon from "@/apps/visagiste/components/BaseIcon/BaseIcon.vue";
+import type { InjectionKey, PropType } from 'vue'
+import type { GroupProvide } from '@/apps/visagiste/composables/group'
+import type { GoToOptions } from '@/apps/visagiste/composables/goto'
+import { BaseIcon } from '@/apps/visagiste/components/BaseIcon'
 
-export const BaseSlideGroupSymbol: InjectionKey<GroupProvide> = Symbol.for('visagiste:base-slide-group')
+export const BaseSlideGroupSymbol: InjectionKey<GroupProvide> = Symbol.for(
+  'visagiste:base-slide-group'
+)
 
 interface SlideGroupSlot {
   next: GroupProvide['next']
@@ -46,49 +57,47 @@ type BaseSlideGroupSlots = {
   next: SlideGroupSlot
 }
 
-export const useBaseSlideGroupProps = propsFactory({
-  centerActive: Boolean,
-  direction: {
-    type: String as PropType<'horizontal' | 'vertical'>,
-    default: 'horizontal',
-  },
-  symbol: {
-    type: null,
-    default: BaseSlideGroupSymbol,
-  },
-  nextIcon: {
-    type: IconValue,
-    default: '$next',
-  },
-  prevIcon: {
-    type: IconValue,
-    default: '$prev',
-  },
-  showArrows: {
-    type: [Boolean, String],
-    validator: (v: any) => (
-      typeof v === 'boolean' || [
-        'always',
-        'desktop',
-        'mobile',
-      ].includes(v)
-    )
-  },
+export const useBaseSlideGroupProps = propsFactory(
+  {
+    centerActive: Boolean,
+    direction: {
+      type: String as PropType<'horizontal' | 'vertical'>,
+      default: 'horizontal',
+    },
+    symbol: {
+      type: null,
+      default: BaseSlideGroupSymbol,
+    },
+    nextIcon: {
+      type: IconValue,
+      default: '$next',
+    },
+    prevIcon: {
+      type: IconValue,
+      default: '$prev',
+    },
+    showArrows: {
+      type: [Boolean, String],
+      validator: (v: any) =>
+        typeof v === 'boolean' || ['always', 'desktop', 'mobile'].includes(v),
+    },
 
-  ...useComponentProps(),
-  ...useDisplayProps({ mobile: null }),
-  ...useTagProps(),
-  ...useGroupProps({
-    selectedClass: 'base-slide-group-item--active',
-  })
-}, 'BaseSlideGroup')
+    ...useComponentProps(),
+    ...useDisplayProps({ mobile: null }),
+    ...useTagProps(),
+    ...useGroupProps({
+      selectedClass: 'base-slide-group-item--active',
+    }),
+  },
+  'BaseSlideGroup'
+)
 
 export default defineComponent({
-  name: "BaseSlideGroup",
+  name: 'BaseSlideGroup',
   computed: {
     sl() {
       return sl
-    }
+    },
   },
   components: {
     BaseIcon,
@@ -98,7 +107,7 @@ export default defineComponent({
   emits: {
     'update:modelValue': (value: any) => true,
   },
-  setup (props) {
+  setup(props) {
     const { isRtl } = useRtl()
     const { displayClasses, mobile } = useDisplay(props)
     const group = useGroup(props, props.symbol)
@@ -108,7 +117,8 @@ export default defineComponent({
     const contentSize = shallowRef(0)
     const isHorizontal = computed(() => props.direction === 'horizontal')
 
-    const { resizeRef: containerRef, contentRect: containerRect } = useResizeObserver()
+    const { resizeRef: containerRef, contentRect: containerRect } =
+      useResizeObserver()
     const { resizeRef: contentRef, contentRect } = useResizeObserver()
 
     const goTo = useGoTo()
@@ -123,42 +133,58 @@ export default defineComponent({
     const firstSelectedIndex = computed(() => {
       if (!group.selected.value.length) return -1
 
-      return group.items.value.findIndex(item => item.id === group.selected.value[0])
+      return group.items.value.findIndex(
+        (item) => item.id === group.selected.value[0]
+      )
     })
 
     const lastSelectedIndex = computed(() => {
       if (!group.selected.value.length) return -1
 
-      return group.items.value.findIndex(item => item.id === group.selected.value[group.selected.value.length - 1])
+      return group.items.value.findIndex(
+        (item) =>
+          item.id === group.selected.value[group.selected.value.length - 1]
+      )
     })
 
     if (IN_BROWSER) {
       let frame = -1
-      watch(() => [group.selected.value, containerRect.value, contentRect.value, isHorizontal.value], () => {
-        cancelAnimationFrame(frame)
-        frame = requestAnimationFrame(() => {
-          if (contentRect.value && contentRect.value) {
-            const sizeProperty = isHorizontal.value ? 'width' : 'height'
+      watch(
+        () => [
+          group.selected.value,
+          containerRect.value,
+          contentRect.value,
+          isHorizontal.value,
+        ],
+        () => {
+          cancelAnimationFrame(frame)
+          frame = requestAnimationFrame(() => {
+            if (contentRect.value && contentRect.value) {
+              const sizeProperty = isHorizontal.value ? 'width' : 'height'
 
-            containerSize.value = containerRect.value[sizeProperty]
-            contentSize.value = contentRect.value[sizeProperty]
+              containerSize.value = containerRect.value[sizeProperty]
+              contentSize.value = contentRect.value[sizeProperty]
 
-            isOverflowing.value = containerSize.value + 1 < containerSize.value
-          }
+              isOverflowing.value =
+                containerSize.value + 1 < containerSize.value
+            }
 
-          if (firstSelectedIndex.value >= 0 && contentRef.el) {
-            // TODO: Is this too naive? Should we store element references in group composable?
-            const selectedElement = contentRef.el.children[lastSelectedIndex.value] as HTMLElement
+            if (firstSelectedIndex.value >= 0 && contentRef.el) {
+              // TODO: Is this too naive? Should we store element references in group composable?
+              const selectedElement = contentRef.el.children[
+                lastSelectedIndex.value
+              ] as HTMLElement
 
-            scrollToChildren(selectedElement, props.centerActive)
-          }
-        })
-      })
+              scrollToChildren(selectedElement, props.centerActive)
+            }
+          })
+        }
+      )
     }
 
     const isFocused = shallowRef(false)
 
-    function scrollToChildren (children: HTMLElement, center?: boolean) {
+    function scrollToChildren(children: HTMLElement, center?: boolean) {
       let target = 0
 
       if (center) {
@@ -179,23 +205,28 @@ export default defineComponent({
       scrollToPosition(target)
     }
 
-    function scrollToPosition (newPosition: number) {
+    function scrollToPosition(newPosition: number) {
       if (!IN_BROWSER || !containerRef.el) return
 
       const offsetSize = getOffsetSize(isHorizontal.value, containerRef.el)
-      const scrollPosition = getScrollPosition(isHorizontal.value, isRtl.value, containerRef.el)
+      const scrollPosition = getScrollPosition(
+        isHorizontal.value,
+        isRtl.value,
+        containerRef.el
+      )
       const scrollSize = getScrollSize(isHorizontal.value, containerRef.el)
 
       if (
         scrollSize <= offsetSize ||
         // Prevent scrolling by only a couple of pixels, which doesn't look smooth
         Math.abs(newPosition - scrollPosition) < 16
-      ) return
+      )
+        return
 
       if (isHorizontal.value && isRtl.value && containerRef.el) {
         const { scrollWidth, offsetWidth: containerWidth } = containerRef.el!
 
-        newPosition = (scrollWidth - containerWidth) - newPosition
+        newPosition = scrollWidth - containerWidth - newPosition
       }
 
       if (isHorizontal.value) {
@@ -205,13 +236,13 @@ export default defineComponent({
       }
     }
 
-    function onScroll (e: Event) {
+    function onScroll(e: Event) {
       const { scrollTop, scrollLeft } = e.target as HTMLElement
 
-      scrollOffset.value = isHorizontal.value ? scrollLeft: scrollTop
+      scrollOffset.value = isHorizontal.value ? scrollLeft : scrollTop
     }
 
-    function onFocusin (e: FocusEvent) {
+    function onFocusin(e: FocusEvent) {
       isFocused.value = true
 
       if (!isOverflowing.value || !containerRef.el) return
@@ -228,30 +259,31 @@ export default defineComponent({
       }
     }
 
-    function onFocusout (e: FocusEvent) {
+    function onFocusout(e: FocusEvent) {
       isFocused.value = false
     }
 
     // Affix clicks produce onFocus that we have to ignore to avoid extra scrollToChildren
     let ignoreFocusEvent = false
-    function onFocus (e: FocusEvent) {
+    function onFocus(e: FocusEvent) {
       if (
         !ignoreFocusEvent &&
         !isFocused &&
         !(e.relatedTarget && contentRef.el?.contains(e.relatedTarget as Node))
-      ) focus()
+      )
+        focus()
 
       ignoreFocusEvent = false
     }
 
-    function onFocusAffixes () {
+    function onFocusAffixes() {
       ignoreFocusEvent = true
     }
 
-    function onKeydown (e: KeyboardEvent) {
+    function onKeydown(e: KeyboardEvent) {
       if (!contentRef.el) return
 
-      function toFocus (location: Parameters<typeof focus>[0]) {
+      function toFocus(location: Parameters<typeof focus>[0]) {
         e.preventDefault()
         focus(location)
       }
@@ -277,16 +309,21 @@ export default defineComponent({
       }
     }
 
-    function getSiblingElement (el: HTMLElement | null, location: 'next' | 'prev') {
+    function getSiblingElement(
+      el: HTMLElement | null,
+      location: 'next' | 'prev'
+    ) {
       if (!el) return undefined
       let sibling: HTMLElement | null = el
       do {
-        sibling = sibling?.[location === 'next' ? 'nextElementSibling' : 'previousElementSibling'] as HTMLElement | null
+        sibling = sibling?.[
+          location === 'next' ? 'nextElementSibling' : 'previousElementSibling'
+        ] as HTMLElement | null
       } while (sibling?.hasAttribute('disabled'))
       return sibling
     }
 
-    function focus (location?: 'next' | 'prev' | 'first' | 'last') {
+    function focus(location?: 'next' | 'prev' | 'first' | 'last') {
       if (!contentRef.el) return
 
       let el: HTMLElement | null | undefined
@@ -303,11 +340,11 @@ export default defineComponent({
 
         if (!el) return focus('last')
       } else if (location === 'first') {
-        el = (contentRef.el.firstElementChild as HTMLElement)
+        el = contentRef.el.firstElementChild as HTMLElement
 
         if (el?.hasAttribute('disabled')) el = getSiblingElement(el, 'next')
       } else if (location === 'last') {
-        el = (contentRef.el.lastElementChild as HTMLElement)
+        el = contentRef.el.lastElementChild as HTMLElement
 
         if (el?.hasAttribute('disabled')) el = getSiblingElement(el, 'prev')
       }
@@ -317,10 +354,11 @@ export default defineComponent({
       }
     }
 
-    function scrollTo (location: 'prev' | 'next') {
+    function scrollTo(location: 'prev' | 'next') {
       const direction = isHorizontal.value && isRtl.value ? -1 : 1
 
-      const offsetStep = (location === 'prev' ? -direction : direction) * containerSize.value
+      const offsetStep =
+        (location === 'prev' ? -direction : direction) * containerSize.value
 
       let newPosition = scrollOffset.value + offsetStep
 
@@ -344,28 +382,34 @@ export default defineComponent({
     const hasAffixes = computed(() => {
       switch (props.showArrows) {
         // Always show arrows on desktop & mobile
-        case 'always': return true
+        case 'always':
+          return true
 
         // Allways show arrows on desktop
-        case 'desktop': return !mobile.value
+        case 'desktop':
+          return !mobile.value
 
         // Show arrows on mobile when overflowing.
         // This matches the default 2.2 behavior
-        case true: return isOverflowing.value || Math.abs(scrollOffset.value) > 0
+        case true:
+          return isOverflowing.value || Math.abs(scrollOffset.value) > 0
 
         // Always show on mobile
-        case 'mobile': return (
-          mobile.value ||
-          (isOverflowing.value || Math.abs(scrollOffset.value) > 0)
-        )
+        case 'mobile':
+          return (
+            mobile.value ||
+            isOverflowing.value ||
+            Math.abs(scrollOffset.value) > 0
+          )
 
         // https://material.io/components/tabs#scrollable-tabs
         // Always show arrows when
         // overflowed on desktop
-        default: return (
-          !mobile.value &&
-          (isOverflowing.value || Math.abs(scrollOffset.value) > 0)
-        )
+        default:
+          return (
+            !mobile.value &&
+            (isOverflowing.value || Math.abs(scrollOffset.value) > 0)
+          )
       }
     })
 
@@ -407,78 +451,76 @@ export default defineComponent({
       onKeydown,
       scrollTo,
     }
-  }
+  },
 })
 </script>
 
 <template>
-<component
-  :is="$props.tag"
-  :class="[
-    'base-slide-group',
-    {
-      'base-slide-group--vertical': !isHorizontal,
-      'base-slide-group--has-affixes': hasAffixes,
-      'base-slide-group--is-overflowing': isOverflowing,
-    },
-    displayClasses,
-    $props.class,
-  ]"
-  :style="$props.style"
-  :tabindex="(isFocused || group.selected.length) ? -1 : 0"
-  @focus="onFocus"
->
-
-  <div
-    v-if="hasAffixes"
-    key="prev"
+  <component
+    :is="$props.tag"
     :class="[
-      'base-slide-group__prev',
-      { 'base-slide-group--disabled': !hasPrev },
+      'base-slide-group',
+      {
+        'base-slide-group--vertical': !isHorizontal,
+        'base-slide-group--has-affixes': hasAffixes,
+        'base-slide-group--is-overflowing': isOverflowing,
+      },
+      displayClasses,
+      $props.class,
     ]"
-    @mousedown="onFocusAffixes"
-    @click="() => hasPrev && scrollTo('prev')"
-  >
-    <slot name="prev" v-bind:prev="slotProps">
-      <BaseFadeTransition>
-        <BaseIcon :icon="isRtl ? $props.nextIcon : $props.prevIcon"/>
-      </BaseFadeTransition>
-    </slot>
-  </div>
-
-  <div
-    key="container"
-    :ref="containerRef"
-    class="base-slide-group__container"
-    @scroll="onScroll"
+    :style="$props.style"
+    :tabindex="isFocused || group.selected.length ? -1 : 0"
+    @focus="onFocus"
   >
     <div
-      :ref="contentRef"
-      class="base-slide-group__content"
-      @focusin="onFocusin"
-      @focusout="onFocusout"
-      @keydown="onKeydown"
+      v-if="hasAffixes"
+      key="prev"
+      :class="[
+        'base-slide-group__prev',
+        { 'base-slide-group--disabled': !hasPrev },
+      ]"
+      @mousedown="onFocusAffixes"
+      @click="() => hasPrev && scrollTo('prev')"
     >
-      <slot v-bind="slotProps"/>
+      <slot name="prev" v-bind:prev="slotProps">
+        <BaseFadeTransition>
+          <BaseIcon :icon="isRtl ? $props.nextIcon : $props.prevIcon" />
+        </BaseFadeTransition>
+      </slot>
     </div>
-  </div>
 
-  <div
-    v-if="hasAffixes"
-    key="next"
-    :class="[
-      'base-slide-group__next',
-      { 'base-slide-group--disabled': !hasNext },
-    ]"
-    @mousedown="onFocusAffixes"
-    @click="() => hasNext && scrollTo('next')"
-  >
-    <slot name="next" v-bind:prev="slotProps">
-      <BaseFadeTransition>
-        <BaseIcon :icon="isRtl ? $props.prevIcon : $props.nextIcon"/>
-      </BaseFadeTransition>
-    </slot>
-  </div>
+    <div
+      key="container"
+      :ref="containerRef"
+      class="base-slide-group__container"
+      @scroll="onScroll"
+    >
+      <div
+        :ref="contentRef"
+        class="base-slide-group__content"
+        @focusin="onFocusin"
+        @focusout="onFocusout"
+        @keydown="onKeydown"
+      >
+        <slot v-bind="slotProps" />
+      </div>
+    </div>
 
-</component>
+    <div
+      v-if="hasAffixes"
+      key="next"
+      :class="[
+        'base-slide-group__next',
+        { 'base-slide-group--disabled': !hasNext },
+      ]"
+      @mousedown="onFocusAffixes"
+      @click="() => hasNext && scrollTo('next')"
+    >
+      <slot name="next" v-bind:prev="slotProps">
+        <BaseFadeTransition>
+          <BaseIcon :icon="isRtl ? $props.prevIcon : $props.nextIcon" />
+        </BaseFadeTransition>
+      </slot>
+    </div>
+  </component>
 </template>
