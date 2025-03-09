f7b74bb88658713cb2c6e1a5038be0d8abf19d5f
Pau4ok <pavel_mal99@mail.ru>
2025-03-09 08:14:27 +0300

    create BaseGrid component

---
+++ frontend/src/apps/visagiste/components/BaseGrid/index.ts
@@ -0,0 +1,7 @@
+export {
+  default as BaseContainer,
+  useBaseContainerProps,
+} from './BaseContainer.vue'
+export { default as BaseRow, useBaseRowProps } from './BaseRow.vue'
+export { default as BaseCol, useBaseColProps } from './BaseCol.vue'
+export { default as BaseSpacer, useBaseSpacerProps } from './BaseSpacer.vue'
