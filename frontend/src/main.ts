e633d1d4f713acfa8a2d750af0bfcc288ff22162
Pau4ok <pavel_mal99@mail.ru>
2025-03-10 20:46:33 +0300

    prettify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,29 +1,29 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import axios from "axios";
-import VueAxios from "vue-axios";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-import Vue3Toastify, {toast, type ToastContainerOptions} from "vue3-toastify";
-import 'vue3-toastify/dist/index.css';
+import Vue3Toastify, { toast, type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
-import VueTippy, {directive} from 'vue-tippy';
-import 'tippy.js/dist/tippy.css';
+import VueTippy, { directive } from 'vue-tippy'
+import 'tippy.js/dist/tippy.css'
 
-import Vintersection from "@/directives/Vintersection";
-import Vdebounce from "@/directives/Vdebounce";
+import Vintersection from '@/directives/Vintersection'
+import Vdebounce from '@/directives/Vdebounce'
 
-import { createPinia } from 'pinia';
+import { createPinia } from 'pinia'
 
-import HighchartsVue from 'highcharts-vue';
+import HighchartsVue from 'highcharts-vue'
 import Highcharts from 'highcharts'
-import StockChart from 'highcharts/modules/stock';
+import StockChart from 'highcharts/modules/stock'
 import highchartsMore from 'highcharts/highcharts-more'
 import loadSolidGauge from 'highcharts/modules/solid-gauge'
-import {defaultChartOpts} from "@/components/charts/DefaultChartOpts";
-import Ripple from "@/directives/ripple";
-import visagiste from "@/plugins/visagiste";
+import { defaultChartOpts } from '@/components/charts/DefaultChartOpts'
+import Ripple from '@/directives/ripple'
+import visagiste from '@/plugins/visagiste'
 
 axios.defaults.baseURL = 'http://localhost:8000'
 
@@ -40,44 +40,38 @@ highchartsMore(hc)
 loadSolidGauge(hc)
 StockChart(hc)
 
-hc.Templating.helpers.abs = (value: number) => Math.abs(value);
+hc.Templating.helpers.abs = (value: number) => Math.abs(value)
 
 Highcharts.setOptions({
-  ...defaultChartOpts
-});
+  ...defaultChartOpts,
+})
 
 app
   .use(router)
   .use(store)
   .use(pinia)
   .use(VueAxios, axios)
-  .use(HighchartsVue, {tagName: 'charts'})
-  .use(
-    Vue3Toastify,
-    {
-      autoClose: 3000,
-      position: toast.POSITION.BOTTOM_RIGHT,
-      limit: 5,
-      closeButton: false,
-      transition: toast.TRANSITIONS.BOUNCE,
-      style: {
-        fontSize: '1.6rem',
-      },
-      theme: 'colored',
-    } as ToastContainerOptions
-  )
-  .use(
-    VueTippy,
-    {
-      directive: 'tippy',
-      component: 'tippy',
-      componentSingleton: 'tippy-singleton',
-      defaultProps: {
-        placement: 'top',
-        allowHTML: false,
-        followCursor: 'horizontal',
-      }
-    }
-  )
+  .use(HighchartsVue, { tagName: 'charts' })
+  .use(Vue3Toastify, {
+    autoClose: 3000,
+    position: toast.POSITION.BOTTOM_RIGHT,
+    limit: 5,
+    closeButton: false,
+    transition: toast.TRANSITIONS.BOUNCE,
+    style: {
+      fontSize: '1rem',
+    },
+    theme: 'colored',
+  } as ToastContainerOptions)
+  .use(VueTippy, {
+    directive: 'tippy',
+    component: 'tippy',
+    componentSingleton: 'tippy-singleton',
+    defaultProps: {
+      placement: 'top',
+      allowHTML: false,
+      followCursor: 'horizontal',
+    },
+  })
   .use(visagiste)
   .mount('#app')
