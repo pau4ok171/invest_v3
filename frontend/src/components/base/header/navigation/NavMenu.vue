74503b7683e2d787dbfcd99bd3ca1e0a3595f929
Pau4ok <pavel_mal99@mail.ru>
2025-03-10 20:54:21 +0300

    prettify

--- frontend/src/components/base/header/navigation/NavMenu.vue
+++ frontend/src/components/base/header/navigation/NavMenu.vue
@@ -1,30 +1,22 @@
-<script lang="ts">
-import {RoutesHeaderEnum} from "@/router/routes.types";
-import {defineComponent} from "vue";
-import BaseButton from "@/apps/visagiste/components/BaseButton/BaseButton.vue";
-
-export default defineComponent({
-  components: {
-    BaseButton
-  },
-  computed: {
-    RoutesHeaderEnum() {
-      return RoutesHeaderEnum
-    }
-  }
-})
+<script setup lang="ts">
+import { RoutesHeaderEnum } from '@/router/routes.types'
+import { BaseButton } from '@/apps/visagiste/components/BaseButton'
 </script>
 
 <template>
-<div class="navigation__nav-menu" >
-  <RouterLink  v-for="routeName in RoutesHeaderEnum" :key="routeName" :to="{name: routeName}">
-    <base-button
-      :text="routeName"
-      variant="text"
-      style="text-transform: capitalize; padding-inline: 8px"
-    />
-  </RouterLink>
-</div>
+  <div class="navigation__nav-menu">
+    <RouterLink
+      v-for="routeName in RoutesHeaderEnum"
+      :key="routeName"
+      :to="{ name: routeName }"
+    >
+      <base-button
+        :text="routeName"
+        variant="text"
+        style="text-transform: capitalize; padding-inline: 8px"
+      />
+    </RouterLink>
+  </div>
 </template>
 
 <style>
