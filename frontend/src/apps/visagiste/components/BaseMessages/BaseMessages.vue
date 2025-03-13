15e3487160df31d5ab5bf325768a79bf40192885
Pau4ok <pavel_mal99@mail.ru>
2025-03-13 22:33:16 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseMessages/BaseMessages.vue
+++ frontend/src/apps/visagiste/components/BaseMessages/BaseMessages.vue
@@ -1,21 +1,27 @@
 <script lang="ts">
 // Styles
-import './BaseMessages.scss';
+import './BaseMessages.scss'
 
 // Components
-import { BaseSlideYTransition } from '@/apps/visagiste/components/transitions';
+import { BaseSlideYTransition } from '@/apps/visagiste/components/transitions'
 
 // Composables
-import {useTextColor} from "@/apps/visagiste/composables/color";
-import {useComponentProps} from "@/apps/visagiste/composables/component";
+import { useTextColor } from '@/apps/visagiste/composables/color'
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+import MaybeTransition, {
+  useTransitionProps,
+} from '@/apps/visagiste/composablesV2/transition.vue'
 
 // Utilities
-import {computed} from "vue";
-import {defineComponent, propsFactory, wrapInArray} from "@/apps/visagiste/utils";
+import { computed } from 'vue'
+import {
+  defineComponent,
+  propsFactory,
+  wrapInArray,
+} from '@/apps/visagiste/utils'
 
 // Types
-import type {Component, PropType} from "vue";
-import MaybeTransition, { useTransitionProps } from "@/apps/visagiste/composables_v2/transition.vue";
+import type { Component, PropType } from 'vue'
 
 export type BaseMessageSlot = {
   message: string
@@ -25,33 +31,38 @@ export type BaseMessagesSlots = {
   message: BaseMessageSlot
 }
 
-export const useBaseMessagesProps = propsFactory({
-  active: Boolean,
-  color: String,
-  messages: {
-    type: [Array, String] as PropType<string | readonly string[]>,
-    default: () => ([]),
-  },
-
-  ...useComponentProps(),
-  ...useTransitionProps({
-    transition: {
-      component: BaseSlideYTransition as Component,
-      leaveAbsolute: true,
-      group: true,
+export const useBaseMessagesProps = propsFactory(
+  {
+    active: Boolean,
+    color: String,
+    messages: {
+      type: [Array, String] as PropType<string | readonly string[]>,
+      default: () => [],
     },
-  }),
-}, 'BaseMessages')
+
+    ...useComponentProps(),
+    ...useTransitionProps({
+      transition: {
+        component: BaseSlideYTransition as Component,
+        leaveAbsolute: true,
+        group: true,
+      },
+    }),
+  },
+  'BaseMessages'
+)
 
 export default defineComponent({
   name: 'BaseMessages',
   props: useBaseMessagesProps(),
   components: {
-    MaybeTransition
+    MaybeTransition,
   },
-  setup (props) {
+  setup(props) {
     const messages = computed(() => wrapInArray(props.messages))
-    const { textColorClasses, textColorStyles } = useTextColor(computed(() => props.color))
+    const { textColorClasses, textColorStyles } = useTextColor(
+      computed(() => props.color)
+    )
 
     return {
       messages,
@@ -63,29 +74,22 @@ export default defineComponent({
 </script>
 
 <template>
-<MaybeTransition
-  :transition="$props.transition"
-  tag="div"
-  :class="[
-    'base-messages',
-    textColorClasses,
-    $props.class,
-  ]"
-  :style="[
-    textColorStyles,
-    $props.style,
-  ]"
->
-  <template v-if="$props.active">
-    <div
-      v-for="(message, index) in messages"
-      class="base-messages__message"
-      :key="`${index}-${messages}`"
-    >
-      <slot name="message">
-        {{ message }}
-      </slot>
-    </div>
-  </template>
-</MaybeTransition>
+  <MaybeTransition
+    :transition="$props.transition"
+    tag="div"
+    :class="['base-messages', textColorClasses, $props.class]"
+    :style="[textColorStyles, $props.style]"
+  >
+    <template v-if="$props.active">
+      <div
+        v-for="(message, index) in messages"
+        class="base-messages__message"
+        :key="`${index}-${messages}`"
+      >
+        <slot name="message" v-bind="message">
+          {{ message }}
+        </slot>
+      </div>
+    </template>
+  </MaybeTransition>
 </template>
