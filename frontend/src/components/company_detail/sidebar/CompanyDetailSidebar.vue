2b26115a03c1d9fff2da1bccb658cb172a0550ff
Pau4ok <pavel_mal99@mail.ru>
2025-03-17 14:48:46 +0300

    refactor using script setup

--- frontend/src/components/company_detail/sidebar/CompanyDetailSidebar.vue
+++ frontend/src/components/company_detail/sidebar/CompanyDetailSidebar.vue
@@ -1,74 +1,70 @@
-<script lang="ts">
- import {defineComponent} from "vue";
- import CompanyDetailSidebarHeader from "@/components/company_detail/sidebar/CompanyDetailSidebarHeader.vue";
- import CompanyDetailSidebarMain from "@/components/company_detail/sidebar/CompanyDetailSidebarMain.vue";
- import CompanyDetailSidebarFooter from "@/components/company_detail/sidebar/CompanyDetailSidebarFooter.vue";
+<script setup lang="ts">
+// Components
+import CompanyDetailSidebarHeader from '@/components/company_detail/sidebar/CompanyDetailSidebarHeader.vue'
+import CompanyDetailSidebarMain from '@/components/company_detail/sidebar/CompanyDetailSidebarMain.vue'
+import CompanyDetailSidebarFooter from '@/components/company_detail/sidebar/CompanyDetailSidebarFooter.vue'
 
-export default defineComponent({
-  name: 'CompanyDetailSidebar',
-  components: {
-    CompanyDetailSidebarFooter,
-    CompanyDetailSidebarMain,
-    CompanyDetailSidebarHeader,
-  },
-  methods: {
-    changeSidebarOpacity() {
-      const opacity = String(this.calculateSidebarOpacity())
-      const sidebar_header = (this.$refs.sidebar_header as any).$el as HTMLElement
-      const sidebar_body = (this.$refs.sidebar_main as any).$el as HTMLElement
-      sidebar_header.style.setProperty('opacity', opacity)
-      sidebar_body.style.setProperty('opacity', opacity)
+// Utilities
+import { onMounted, onUnmounted, ref } from 'vue'
+import { debounce } from '@/apps/visagiste/utils'
 
-      if (Number(opacity) === 0) {
-        sidebar_header.style.setProperty('visibility', 'hidden')
-        sidebar_body.style.setProperty('visibility', 'hidden')
-      } else {
-        sidebar_header.style.setProperty('visibility', 'visible')
-        sidebar_body.style.setProperty('visibility', 'visible')
-      }
-    },
-    calculateSidebarOpacity(): Number {
-      const scrollVisible = 25
-      const scrollHidden = 200
-      const scrollY = window.scrollY
-      let opacity = 0
+const headerRef = ref<InstanceType<typeof CompanyDetailSidebarHeader>>()
+const bodyRef = ref<InstanceType<typeof CompanyDetailSidebarMain>>()
 
-      if (scrollY < scrollHidden && scrollY > scrollVisible) {
-        opacity = (scrollY - scrollVisible) /  (scrollHidden - scrollVisible)
-        return opacity
-      }
-      if (scrollY <= scrollVisible) {
-        opacity = 0
-        return opacity
-      }
-      if (scrollY >= scrollHidden) {
-        opacity = 1
-        return opacity
-      }
-      return opacity
-    },
-  },
-  mounted() {
-    window.addEventListener('scroll', this.changeSidebarOpacity)
-  },
-  unmounted() {
-    window.removeEventListener('scroll', this.changeSidebarOpacity)
-  },
- })
+function changeOpacity() {
+  const opacity = String(calculateOpacity())
+
+  if (!headerRef.value || !bodyRef.value) return
+
+  headerRef.value.$el.style.setProperty('opacity', opacity)
+  bodyRef.value.$el.style.setProperty('opacity', opacity)
+
+  headerRef.value.$el.style.setProperty(
+    'clip-path',
+    `inset(${(1 - Number(opacity)) * 100}% 0px 0px)`
+  )
+}
+
+function calculateOpacity() {
+  const scrollVisible = 25
+  const scrollHidden = 200
+  const scrollY = window.scrollY
+  let opacity = 0
+
+  if (scrollY < scrollHidden && scrollY > scrollVisible) {
+    opacity = (scrollY - scrollVisible) / (scrollHidden - scrollVisible)
+    return opacity
+  }
+  if (scrollY <= scrollVisible) {
+    opacity = 0
+    return opacity
+  }
+  if (scrollY >= scrollHidden) {
+    opacity = 1
+    return opacity
+  }
+  return opacity
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', debounce(changeOpacity, 1))
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', debounce(changeOpacity, 1))
+})
 </script>
 
 <template>
-<div class="detail-sidebar">
-<section class="detail-sidebar__inner">
+  <div class="detail-sidebar">
+    <section class="detail-sidebar__inner">
+      <CompanyDetailSidebarHeader ref="headerRef" />
 
-  <CompanyDetailSidebarHeader ref="sidebar_header"/>
+      <CompanyDetailSidebarMain ref="bodyRef" />
 
-  <CompanyDetailSidebarMain ref="sidebar_main"/>
-
-  <CompanyDetailSidebarFooter/>
-
-</section>
-</div>
+      <CompanyDetailSidebarFooter />
+    </section>
+  </div>
 </template>
 
 <style scoped>
@@ -82,7 +78,5 @@ export default defineComponent({
 .detail-sidebar__inner {
   position: fixed;
   top: 80px;
-  width: 298px;
-  padding: 8px 24px 0 0;
 }
 </style>
