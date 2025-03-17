cb96b1cdf29b0a7ff577a24d8e1e45f61db9e555
Pau4ok <pavel_mal99@mail.ru>
2025-03-17 10:33:58 +0300

    create BaseBreadcrumbs component

---
+++ frontend/src/apps/visagiste/components/BaseBreadcrumbs/BaseBreadcrumbsDivider.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+// Composables
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+
+// Utilities
+import { defineComponent, propsFactory } from '@/apps/visagiste/utils'
+
+const useBaseBreadcrumbsDividerProps = propsFactory(
+  {
+    divider: [Number, String],
+
+    ...useComponentProps(),
+  },
+  'BaseBreadcrumbsDivider'
+)
+
+export default defineComponent({
+  name: 'BaseBreadcrumbsDivider',
+  props: useBaseBreadcrumbsDividerProps(),
+})
+</script>
+
+<template>
+  <li
+    aria-hidden="true"
+    :class="['base-breadcrumbs-divider', $props.divider]"
+    :style="[$props.style]"
+  >
+    <slot name="divider">
+      {{ $props.divider }}
+    </slot>
+  </li>
+</template>
---
+++ frontend/src/apps/visagiste/components/BaseBreadcrumbs/index.ts
@@ -0,0 +1,12 @@
+export {
+  default as BaseBreadcrumbs,
+  useBaseBreadcrumbProps,
+} from './BaseBreadcrumbs.vue'
+export {
+  default as BaseBreadcrumbsItem,
+  useBaseBreadcrumbsItemProps,
+} from './BaseBreadcrumbsItem.vue'
+export {
+  default as BaseBreadcrumbsDivider,
+  useBreadcrumbsDividerProps,
+} from './BaseBreadcrumbsDivider.vue'
