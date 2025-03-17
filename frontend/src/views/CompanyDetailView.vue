b1fec04e1448485277c4bedc5b655a41f9453bcd
Pau4ok <pavel_mal99@mail.ru>
2025-03-17 10:46:25 +0300

    refactor using script setup

--- frontend/src/views/CompanyDetailView.vue
+++ frontend/src/views/CompanyDetailView.vue
@@ -1,77 +1,46 @@
-<script lang="ts">
-import CompanyDetailHeader from "@/components/company_detail/header/CompanyDetailHeader.vue";
-import CompanyDetailContent from "@/components/company_detail/CompanyDetailContent.vue";
-import {mapActions, mapGetters, mapMutations} from "vuex";
-import {defineComponent} from "vue";
-import {RouteNamesEnum} from "@/router/routes.types";
-import PageNotFound from "@/components/base/page_not_found/PageNotFound.vue";
+<script setup lang="ts">
+// Components
+import CompanyDetailHeader from '@/components/company_detail/header/CompanyDetailHeader.vue'
+import CompanyDetailContent from '@/components/company_detail/CompanyDetailContent.vue'
 
-export default defineComponent({
-  name: 'CompanyDetail',
-  components: {
-    PageNotFound,
-    CompanyDetailContent,
-    CompanyDetailHeader
-  },
-  async mounted() {
-    await this.initializeView()
-  },
-  unmounted() {
-    this.setPageNotFound(false)
-  },
-  methods: {
-    async initializeView() {
-      this.setIsLoading(true)
-      this.setPageIsReady(false)
-      const company_slug = this.$route.params.company_slug as String
-      await this.fetchCompany(company_slug)
-      if (!this.pageNotFound) {
-        await this.fetchPriceData(company_slug)
-        document.title = `${this.company.title} (${this.company.market.title}:${this.company.ticker}) - Обзор компании, Новости, Аналитика - Finargo`
-      }
-      this.setIsLoading(false)
-      this.setPageIsReady(true)
-    },
-    ...mapMutations({
-      setPageIsReady: 'companyDetail/setPageIsReady',
-      setPageNotFound: 'companyDetail/setPageNotFound',
-      setIsLoading: 'setIsLoading',
-    }),
-    ...mapActions({
-      fetchCompany: 'companyDetail/fetchCompany',
-      fetchPriceData: 'companyDetail/fetchPriceData',
-    }),
-  },
-  computed: {
-    ...mapGetters({
-      company: 'companyDetail/getCompany',
-      pageNotFound: 'companyDetail/getPageNotFound',
-    })
-  },
-  watch: {
-    $route(to) {
-      if (to.name === RouteNamesEnum.company_detail) {
-        this.initializeView()
-      }
-    }
-  },
+// Composables
+import { useCompanyDetailStore } from '@/store/companyDetail'
+import { usePageStore } from '@/store/page'
+import { useRoute } from 'vue-router'
+
+// Utilities
+import { onMounted, watch } from 'vue'
+import { RouteNamesEnum } from '@/router/routes.types'
+
+const route = useRoute()
+const pageStore = usePageStore()
+const companyDetailStore = useCompanyDetailStore()
+
+async function init() {
+  const companySlug = route.params?.companySlug as string
+
+  pageStore.loading = true
+
+  await companyDetailStore.fetchCompany(companySlug)
+  await companyDetailStore.fetchPriceData(companySlug)
+
+  pageStore.loading = false
+
+  document.title = `${companyDetailStore.company.title} (${companyDetailStore.company.market.title}:${companyDetailStore.company.ticker}) - Обзор компании, Новости, Аналитика - Finargo`
+}
+
+watch(route, (to) => {
+  if (to.name === RouteNamesEnum.company_detail) {
+    init()
+  }
+})
+
+onMounted(async () => {
+  await init()
 })
 </script>
 
 <template>
-<PageNotFound v-if="pageNotFound"/>
-
-<template v-else>
-  <div :class="{'detail--lateral-is-actif': true}" >
-    <CompanyDetailHeader/>
-    <CompanyDetailContent/>
-  </div>
+  <CompanyDetailHeader />
+  <CompanyDetailContent />
 </template>
-</template>
-
-<style scoped>
-.detail--lateral-is-actif {
-  width: 100%;
-  max-width: 1167px;
-}
-</style>
