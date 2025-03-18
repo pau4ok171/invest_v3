b300e51806f40584ac84a11af8cf134715e63c1b
Pau4ok <pavel_mal99@mail.ru>
2025-03-18 10:49:47 +0300

    refactor using script setup

--- frontend/src/components/company_detail/content_list/summary/CompanyDetailStockOverview.vue
+++ frontend/src/components/company_detail/content_list/summary/CompanyDetailStockOverview.vue
@@ -1,75 +1,69 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-import CompanyDetailSection from "@/components/company_detail/content_list/CompanyDetailSection.vue";
-import CompanyDetailContentGroup from "@/components/company_detail/content_list/CompanyDetailContentGroup.vue";
-import CompanyDetailSnowflakeTable from "@/components/company_detail/content_list/summary/CompanyDetailSnowflakeTable.vue";
-import SnowflakeChart from "@/components/charts/SnowflakeChart.vue";
-import CompanyDetailRiskRewardItem from "@/components/company_detail/content_list/summary/CompanyDetailRiskRewardItem.vue";
-import CompanyDetailRiskReward from "@/components/company_detail/content_list/summary/CompanyDetailRiskReward.vue";
-import DetailSectionTitle from "@/components/UI/text/DetailSectionTitle.vue";
-import DetailSectionText from "@/components/UI/text/DetailSectionText.vue";
-import {mapGetters} from "vuex";
-import BaseButton from "@/apps/visagiste/components/BaseButton/BaseButton.vue";
+<script setup lang="ts">
+// Components
+import { BaseButton } from '@/apps/visagiste/components/BaseButton'
+import SnowflakeChart from '@/components/charts/SnowflakeChart.vue'
+import CompanyDetailRiskReward from '@/components/company_detail/content_list/summary/CompanyDetailRiskReward.vue'
 
-export default defineComponent({
-  name: "CompanyDetailStockOverview",
-  components: {
-    BaseButton,
-    DetailSectionText,
-    DetailSectionTitle,
-    CompanyDetailRiskReward,
-    CompanyDetailRiskRewardItem,
-    SnowflakeChart,
-    CompanyDetailSnowflakeTable,
-    CompanyDetailContentGroup,
-    CompanyDetailSection
-  },
-  computed: {
-    ...mapGetters({
-      company: 'companyDetail/getCompany',
-      snowflake: 'companyDetail/getSnowflake',
-    }),
-  },
-  methods: {
-    scrollToAboutTheCompanySection() {
-      const goTo = (document as any).querySelector('#about-company-section').getBoundingClientRect()
-      const windowScroll = document.documentElement.scrollTop
-      const scrollTop = goTo.top + windowScroll - 80
-      window.scrollTo({top: scrollTop, behavior: 'smooth'})
-    }
-  },
-})
+// Composables
+import { useCompanyDetailStore } from '@/store/companyDetail'
+
+// Utilities
+import { computed } from 'vue'
+
+// Types
+import type { DetailCompany } from '@/types/invest'
+import BaseCard from "@/apps/visagiste/components/BaseCard/BaseCard.vue";
+import BaseCardTitle from "@/apps/visagiste/components/BaseCard/BaseCardTitle.vue";
+import BaseContainer from "@/apps/visagiste/components/BaseGrid/BaseContainer.vue";
+import BaseCol from "@/apps/visagiste/components/BaseGrid/BaseCol.vue";
+import BaseRow from "@/apps/visagiste/components/BaseGrid/BaseRow.vue";
+import BaseCardText from "@/apps/visagiste/components/BaseCard/BaseCardText.vue";
+import BaseCardActions from "@/apps/visagiste/components/BaseCard/BaseCardActions.vue";
+
+const companyDetailStore = useCompanyDetailStore()
+const company = computed<DetailCompany>(() => companyDetailStore.company)
+const snowflake = computed(() => companyDetailStore.snowflake)
+
+function onAboutCompanyClick() {
+  const goTo = (document as Document)
+    ?.querySelector('#about-company-section')
+    ?.getBoundingClientRect()
+
+  if (!goTo) return
+
+  const windowScroll = document.documentElement.scrollTop
+  const scrollTop = goTo.top + windowScroll - 80
+  window.scrollTo({ top: scrollTop, behavior: 'smooth' })
+}
 </script>
 
 <template>
-<CompanyDetailSection :useGrid="true" :useMinH="true">
-  <CompanyDetailContentGroup>
-    <DetailSectionTitle>{{ company.ticker }} Stock Overview</DetailSectionTitle>
-    <DetailSectionText>{{ company.short_description }}</DetailSectionText>
-
-    <base-button
-      prepend-icon="InfoIcon"
-      text="about the company"
-      variant="plain"
-      color="var(--blue)"
-      @click="scrollToAboutTheCompanySection"
-    />
-
-  </CompanyDetailContentGroup>
-
-  <CompanyDetailContentGroup :useGridRow="true">
-
-    <CompanyDetailSnowflakeTable v-if="false"/>
-
-    <SnowflakeChart :chartData="snowflake" v-else/>
-
-  </CompanyDetailContentGroup>
-
-  <CompanyDetailContentGroup :useGrid="true">
-
-    <CompanyDetailRiskReward/>
-
-  </CompanyDetailContentGroup>
-
-</CompanyDetailSection>
+  <base-card color="surface-light" class="mb-4">
+    <base-container>
+      <base-row>
+        <base-col cols="7">
+          <base-card-title>{{ company.ticker }} Stock Overview</base-card-title>
+          <base-card-text>{{ company.short_description }}</base-card-text>
+          <base-card-actions>
+            <base-button
+              prepend-icon="$iInfo"
+              text="about the company"
+              variant="plain"
+              color="info"
+              size="small"
+              @click="onAboutCompanyClick"
+            />
+          </base-card-actions>
+        </base-col>
+        <base-col>
+          <SnowflakeChart :chartData="snowflake as number[]" />
+        </base-col>
+      </base-row>
+      <base-row>
+        <base-col cols="12">
+          <CompanyDetailRiskReward />
+        </base-col>
+      </base-row>
+    </base-container>
+  </base-card>
 </template>
