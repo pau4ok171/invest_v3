06de65fd01ad018f6c4071669d8715d93b88af97
Pau4ok <pavel_mal99@mail.ru>
2025-03-20 12:59:20 +0300

    prettify

--- frontend/src/apps/visagiste/components/BaseToolbar/BaseToolbar.vue
+++ frontend/src/apps/visagiste/components/BaseToolbar/BaseToolbar.vue
@@ -1,57 +1,58 @@
 <script lang="ts">
+// Styles
+import './BaseToolbar.scss'
+
+// Components
+import BaseImage from '@/apps/visagiste/components/BaseImage/BaseImage.vue'
+import BaseDefaultsProvider from '@/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue'
+import { BaseExpandTransition } from '@/apps/visagiste/components/transitions'
+
 // Composables
-import { useBackgroundColor } from "@/apps/visagiste/composables/color";
-import { provideDefaults } from "@/apps/visagiste/composables/defaults";
-import { useBorder, useBorderProps } from "@/apps/visagiste/composables/border";
+import { useBackgroundColor } from '@/apps/visagiste/composables/color'
+import { provideDefaults } from '@/apps/visagiste/composables/defaults'
+import { useBorder, useBorderProps } from '@/apps/visagiste/composables/border'
+import { useSlotIsEmpty } from '@/apps/visagiste/composables/slotIsEmpty'
 import {
   useElevation,
   useElevationProps,
-} from "@/apps/visagiste/composables/elevation";
+} from '@/apps/visagiste/composables/elevation'
 import {
   useRounded,
   useRoundedProps,
-} from "@/apps/visagiste/composables/rounded";
-import {
-  provideTheme,
-  useThemeProps,
-} from "@/apps/visagiste/composables/theme";
-import { useRtl } from "@/apps/visagiste/composables/locale";
+} from '@/apps/visagiste/composables/rounded'
+import { provideTheme, useThemeProps } from '@/apps/visagiste/composables/theme'
+import { useRtl } from '@/apps/visagiste/composables/locale'
+import { useComponentProps } from '@/apps/visagiste/composables/component'
+import { useTagProps } from '@/apps/visagiste/composables/tag'
 
 // Utilities
-import { computed, defineComponent, shallowRef, toRef } from "vue";
+import { computed, shallowRef, toRef } from 'vue'
+import {
+  convertToUnit,
+  defineComponent,
+  propsFactory,
+} from '@/apps/visagiste/utils'
 
-// Styles
-import "./BaseToolbar.scss";
-import BaseImage from "@/apps/visagiste/components/BaseImage/BaseImage.vue";
-import BaseDefaultsProvider from "@/apps/visagiste/components/BaseDefaultsProvider/BaseDefaultsProvider.vue";
-import { convertToUnit, propsFactory } from "@/apps/visagiste/utils";
-import { BaseExpandTransition } from "@/apps/visagiste/components/transitions";
-import type { PropType, SlotsType } from "vue";
-import { useComponentProps } from "@/apps/visagiste/composables/component";
-import { useTagProps } from "@/apps/visagiste/composables/tag";
+// Types
+import type { PropType, SlotsType } from 'vue'
 
 export type BaseToolbarSlots = {
-  default: (props?: any) => void;
-  image: (props?: any) => void;
-  prepend: (props?: any) => void;
-  append: (props?: any) => void;
-  title: (props?: any) => void;
-  extension: (props?: any) => void;
-};
+  default: (props?: any) => void
+  image: (props?: any) => void
+  prepend: (props?: any) => void
+  append: (props?: any) => void
+  title: (props?: any) => void
+  extension: (props?: any) => void
+}
 
 const allowedDensities = [
   null,
-  "prominent",
-  "default",
-  "comfortable",
-  "compact",
-] as const;
-export type Density =
-  | null
-  | "prominent"
-  | "default"
-  | "comfortable"
-  | "compact";
+  'prominent',
+  'default',
+  'comfortable',
+  'compact',
+] as const
+export type Density = null | 'prominent' | 'default' | 'comfortable' | 'compact'
 
 export const useBaseToolbarProps = propsFactory(
   {
@@ -60,7 +61,7 @@ export const useBaseToolbarProps = propsFactory(
     color: String,
     density: {
       type: String as PropType<Density>,
-      default: "default",
+      default: 'default',
       validator: (v: any) => allowedDensities.includes(v),
     },
     extended: Boolean,
@@ -81,17 +82,17 @@ export const useBaseToolbarProps = propsFactory(
     ...useComponentProps(),
     ...useElevationProps(),
     ...useRoundedProps(),
-    ...useTagProps({ tag: "header" }),
+    ...useTagProps({ tag: 'header' }),
     ...useThemeProps(),
   },
-  "BaseToolbar",
-);
+  'BaseToolbar'
+)
 
 export default defineComponent({
-  name: "BaseToolbar",
+  name: 'BaseToolbar',
   computed: {
     BaseExpandTransition() {
-      return BaseExpandTransition;
+      return BaseExpandTransition
     },
   },
   methods: {
@@ -102,49 +103,52 @@ export default defineComponent({
   slots: Object as SlotsType<BaseToolbarSlots>,
   setup(props, { slots }) {
     const { backgroundColorClasses, backgroundColorStyles } =
-      useBackgroundColor(toRef(props, "color"));
-    const { borderClasses } = useBorder(props);
-    const { elevationClasses } = useElevation(props);
-    const { roundedClasses } = useRounded(props);
-    const { themeClasses } = provideTheme(props);
-    const { rtlClasses } = useRtl();
+      useBackgroundColor(toRef(props, 'color'))
+    const { borderClasses } = useBorder(props)
+    const { elevationClasses } = useElevation(props)
+    const { roundedClasses } = useRounded(props)
+    const { themeClasses } = provideTheme(props)
+    const { rtlClasses } = useRtl()
 
-    const isExtended = shallowRef(!!(props.extended || slots.extension?.()));
+    const isExtended = shallowRef(!!(props.extended || slots.extension?.()))
     const contentHeight = computed(() =>
       parseInt(
         Number(props.height) +
-          (props.density === "prominent" ? Number(props.height) : 0) -
-          (props.density === "comfortable" ? 8 : 0) -
-          (props.density === "compact" ? 16 : 0),
-        10,
-      ),
-    );
+          (props.density === 'prominent' ? Number(props.height) : 0) -
+          (props.density === 'comfortable' ? 8 : 0) -
+          (props.density === 'compact' ? 16 : 0),
+        10
+      )
+    )
     const extensionHeight = computed(() =>
       isExtended.value
         ? parseInt(
             Number(props.extensionHeight) +
-              (props.density === "prominent"
+              (props.density === 'prominent'
                 ? Number(props.extensionHeight)
                 : 0) -
-              (props.density === "comfortable" ? 4 : 0) -
-              (props.density === "compact" ? 8 : 0),
-            10,
+              (props.density === 'comfortable' ? 4 : 0) -
+              (props.density === 'compact' ? 8 : 0),
+            10
           )
-        : 0,
-    );
+        : 0
+    )
 
     provideDefaults({
       BaseBtn: {
-        variant: "text",
+        variant: 'text',
       },
-    });
+    })
 
-    const hasTitle = computed(() => !!(props.title || slots.image));
-    const hasImage = computed(() => !!(slots.image || props.image));
-    const extension = computed(() => slots.extension?.());
+    const titleIsEmpty = useSlotIsEmpty('title')
+    const imageIsEmpty = useSlotIsEmpty('image')
+
+    const hasTitle = computed(() => !!props.title || !titleIsEmpty.value)
+    const hasImage = computed(() => !!props.image || !imageIsEmpty.value)
+    const extension = computed(() => slots.extension?.())
     isExtended.value = computed(
-      () => !!(props.extended || extension.value),
-    ).value;
+      () => !!(props.extended || extension.value)
+    ).value
 
     return {
       backgroundColorClasses,
@@ -160,14 +164,14 @@ export default defineComponent({
       hasImage,
       extension,
       isExtended,
-    };
+    }
   },
-});
+})
 </script>
 
 <template>
   <component
-    :is="$props.tag"
+    :is="$props.tag as string"
     :class="[
       'base-toolbar',
       {
