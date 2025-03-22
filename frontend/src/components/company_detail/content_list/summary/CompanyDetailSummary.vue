9e1858768f4f8a494c1c4cdde1cc84219c36d990
Pau4ok <pavel_mal99@mail.ru>
2025-03-22 23:30:00 +0300

    refactor using script setup

--- frontend/src/components/company_detail/content_list/summary/CompanyDetailSummary.vue
+++ frontend/src/components/company_detail/content_list/summary/CompanyDetailSummary.vue
@@ -1,40 +1,20 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-import CompanyDetailStockOverview from "@/components/company_detail/content_list/summary/CompanyDetailStockOverview.vue";
-import CompanyDetailNotes from "@/components/company_detail/content_list/summary/notes/CompanyDetailNotes.vue";
-import CompanyDetailCompetitors from "@/components/company_detail/content_list/summary/competitors/CompanyDetailCompetitors.vue";
-import CompanyDetailPriceHistoryPerformance
-  from "@/components/company_detail/content_list/summary/CompanyDetailPriceHistoryPerformance.vue";
-import CompanyDetailAboutCompany from "@/components/company_detail/content_list/summary/CompanyDetailAboutCompany.vue";
-import CompanyDetailFundamentalSummary from "@/components/company_detail/content_list/summary/CompanyDetailFundamentalSummary.vue";
-
-export default defineComponent({
-  name: "CompanyDetailSummary",
-  components: {
-    CompanyDetailStockOverview,
-    CompanyDetailNotes,
-    CompanyDetailCompetitors,
-    CompanyDetailPriceHistoryPerformance,
-    CompanyDetailAboutCompany,
-    CompanyDetailFundamentalSummary,
-  },
-})
+<script setup lang="ts">
+// Components
+import CompanyDetailStockOverview from '@/components/company_detail/content_list/summary/CompanyDetailStockOverview.vue'
+import CompanyDetailNotes from '@/components/company_detail/content_list/summary/notes/CompanyDetailNotes.vue'
+import CompanyDetailCompetitors from '@/components/company_detail/content_list/summary/competitors/CompanyDetailCompetitors.vue'
+import CompanyDetailPriceHistoryPerformance from '@/components/company_detail/content_list/summary/CompanyDetailPriceHistoryPerformance.vue'
+import CompanyDetailAboutCompany from '@/components/company_detail/content_list/summary/CompanyDetailAboutCompany.vue'
+import CompanyDetailFundamentalSummary from '@/components/company_detail/content_list/summary/CompanyDetailFundamentalSummary.vue'
 </script>
 
 <template>
-<div class="detail__summary">
-
-  <CompanyDetailStockOverview/>
-
-  <CompanyDetailNotes/>
-
-  <CompanyDetailCompetitors/>
-
-  <CompanyDetailPriceHistoryPerformance/>
-
-  <CompanyDetailAboutCompany/>
-
-  <CompanyDetailFundamentalSummary/>
-
-</div>
+  <div class="detail__summary">
+    <CompanyDetailStockOverview />
+    <CompanyDetailNotes />
+    <CompanyDetailCompetitors />
+    <CompanyDetailPriceHistoryPerformance />
+    <CompanyDetailAboutCompany />
+    <CompanyDetailFundamentalSummary />
+  </div>
 </template>
