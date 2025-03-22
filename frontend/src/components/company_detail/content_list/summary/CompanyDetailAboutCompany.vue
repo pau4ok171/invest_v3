c9e5d3861c489a0ec1099c308ae497e11c56de49
Pau4ok <pavel_mal99@mail.ru>
2025-03-22 21:01:51 +0300

    refactor using script setup

--- frontend/src/components/company_detail/content_list/summary/CompanyDetailAboutCompany.vue
+++ frontend/src/components/company_detail/content_list/summary/CompanyDetailAboutCompany.vue
@@ -1,76 +1,115 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-import CompanyDetailAboutCompanyTable from "@/components/company_detail/content_list/summary/CompanyDetailAboutCompanyTable.vue";
-import CompanyDetailSection from "@/components/company_detail/content_list/CompanyDetailSection.vue";
-import DetailSectionTitle from "@/components/UI/text/DetailSectionTitle.vue";
-import {mapGetters} from "vuex";
-import BaseButton from "@/apps/visagiste/components/BaseButton/BaseButton.vue";
+<script setup lang="ts">
+// Components
+import BaseCard from '@/apps/visagiste/components/BaseCard/BaseCard.vue'
+import BaseCardTitle from '@/apps/visagiste/components/BaseCard/BaseCardTitle.vue'
+import BaseCardText from '@/apps/visagiste/components/BaseCard/BaseCardText.vue'
+import BaseCardActions from '@/apps/visagiste/components/BaseCard/BaseCardActions.vue'
+import BaseCardItem from '@/apps/visagiste/components/BaseCard/BaseCardItem.vue'
+import BaseTable from '@/apps/visagiste/components/BaseTable/BaseTable.vue'
+import BaseIcon from '@/apps/visagiste/components/BaseIcon/BaseIcon.vue'
+import { BaseButton } from '@/apps/visagiste/components/BaseButton'
 
-export default defineComponent({
-  name: "CompanyDetailAboutCompany",
-  components: {
-    BaseButton,
-    DetailSectionTitle,
-    CompanyDetailSection,
-    CompanyDetailAboutCompanyTable,
-  },
-  computed: {
-    ...mapGetters({
-      company: 'companyDetail/getCompany',
-    }),
-    get_description() {
-      this.descriptionIsAvailable = !!this.company.description;
-      return this.company.description
-    },
-  },
-  data() {
-    return {
-      isCutting: true,
-      descriptionIsAvailable: true
-    }
-  },
-})
+// Composables
+import { useCompanyDetailStore } from '@/store/companyDetail'
+
+// Utilities
+import { computed, shallowRef } from 'vue'
+
+// Types
+import type { DetailCompany } from '@/types/invest'
+
+const store = useCompanyDetailStore()
+const company = computed<DetailCompany>(() => store.company)
+
+const description = computed(() => company.value.description)
+const reduced = shallowRef(true)
+
+function humanize(val: number | string) {
+  if (typeof val === 'string') return val
+
+  return new Intl.NumberFormat('en-EN').format(val)
+}
 </script>
 
 <template>
-<CompanyDetailSection id="about-company-section">
+  <base-card color="surface-light" class="mb-4">
+    <base-card-title>About the Company</base-card-title>
 
-  <DetailSectionTitle>About the Company</DetailSectionTitle>
+    <base-card-item>
+      <base-table class="company-detail__about-company-table">
+        <thead>
+          <tr>
+            <th>Founded</th>
+            <th>Employees</th>
+            <th>CEO</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ company.year_founded || 'n/a' }}</td>
+            <td>
+              {{
+                humanize(
+                  company.reports.length
+                    ? company.reports[0].total_employees_figure
+                    : 'n/a'
+                )
+              }}
+            </td>
+            <td>n/a</td>
+            <td v-if="company.website">
+              <a
+                target="_blank"
+                class="detail-about-company-table__extra-link"
+                :href="company.website"
+                rel="noopener noreferrer nofollow"
+              >
+                {{ company.website }}
+                <base-icon icon="$iExtraLink" />
+              </a>
+            </td>
+            <td v-else>n/a</td>
+          </tr>
+        </tbody>
+      </base-table>
+    </base-card-item>
 
-  <CompanyDetailAboutCompanyTable/>
-    <div class="detail-about-company__description" :class="{'detail-about-company__description--cut': isCutting}">
-      {{ get_description }}
-    </div>
-
-  <base-button
-    v-if="descriptionIsAvailable"
-    :text="isCutting ? 'Show More' : 'Show Less'"
-    theme="dark-blue"
-    rounded="large"
-    lower
-    size="small"
-    @click="isCutting=!isCutting"
-  />
-
-</CompanyDetailSection>
+    <base-card-text>
+      <div
+        :class="[
+          'company-detail-about-company__description',
+          { 'company-detail-about-company__description--reduced': reduced },
+        ]"
+      >
+        {{ description }}
+      </div>
+    </base-card-text>
+    <base-card-actions>
+      <base-button
+        v-if="!!description"
+        :text="reduced ? 'Show More' : 'Show Less'"
+        color="info"
+        rounded="large"
+        size="small"
+        @click="reduced = !reduced"
+      />
+    </base-card-actions>
+  </base-card>
 </template>
 
-<style scoped>
-.detail-about-company__description {
+<style lang="scss" scoped>
+.company-detail-about-company__description {
   position: relative;
-  font-size: 1.6rem;
   line-height: 1.5;
-  margin-bottom: 24px;
-  transition: .5s ease-out;
+  transition: 0.5s ease-out;
 }
-.detail-about-company__description--cut {
-  font-size: 1.6rem;
+.company-detail-about-company__description--reduced {
   line-height: 1.5;
-  margin-bottom: 24px;
   max-height: 120px;
   overflow: hidden;
 }
-.detail-about-company__description--cut::after {
+.company-detail-about-company__description--reduced::after {
   content: '';
   position: absolute;
   bottom: 0;
@@ -80,5 +119,8 @@ export default defineComponent({
   height: 60px;
   opacity: 1;
   transition: 0.5s;
+}
+.company-detail__about-company-table {
+  background: inherit;
 }
 </style>
