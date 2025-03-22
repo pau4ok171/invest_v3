079458ea8f9d1b226c71aa6b3e45b5ab06486630
Pau4ok <pavel_mal99@mail.ru>
2025-03-22 20:31:11 +0300

    refactor using script setup

--- frontend/src/components/base/page_not_found/PageNotFound.vue
+++ frontend/src/components/base/page_not_found/PageNotFound.vue
@@ -1,24 +1,20 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
+<script setup lang="ts">
+// Utilities
+import { onMounted } from 'vue'
 
-export default defineComponent({
-  name: "PageNotFound",
-  mounted() {
-    document.title = 'Page Not Found - Finargo'
-  },
+onMounted(() => {
+  document.title = 'Page Not Found - Finargo'
 })
 </script>
 
 <template>
-<div class="page-not-found">
-  PAGE NOT FOUND
-</div>
+  <div class="page-not-found">PAGE NOT FOUND</div>
 </template>
 
 <style scoped>
 .page-not-found {
   display: flex;
-  font-size: 5rem;
+  font-size: 3.125rem;
   align-items: center;
   justify-content: center;
   margin: 100px 0;
