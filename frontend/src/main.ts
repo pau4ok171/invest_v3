e27602ff4ff1c3c0608005bc9a512439ca8baeb6
Pau4ok <pavel_mal99@mail.ru>
2025-03-24 13:41:41 +0300

    replace vuex by pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import axios from '@/axios'
 import VueAxios from 'vue-axios'
 
@@ -40,7 +39,6 @@ Highcharts.setOptions({
 
 app
   .use(router)
-  .use(store)
   .use(pinia)
   .use(VueAxios, axios)
   .use(HighchartsVue, { tagName: 'charts' })
