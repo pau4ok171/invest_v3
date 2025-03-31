418f45336a9a220e52f949f3f848577e38361a7a
Pau4ok <pavel_mal99@mail.ru>
2025-03-31 17:55:11 +0300

    prettify

--- frontend/src/components/company_detail/header/CompanyDetailHeaderInfoPanel.vue
+++ frontend/src/components/company_detail/header/CompanyDetailHeaderInfoPanel.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 // Components
 import SmallPriceChart from '@/components/charts/SmallPriceChart.vue'
-import CompanyDetailHeaderInfoItem from '@/components/company_detail/header/CompanyDetailHeaderInfoItem.vue'
-import CompanyDetailHeaderAnalystsButtonModalMenuContainer from '@/components/company_detail/header/CompanyDetailHeaderAnalystModalMenuContainer.vue'
+import CompanyDetailHeaderAnalystsDialog from '@/components/company_detail/header/CompanyDetailHeaderAnalystsDialog.vue'
 
 // Composables
 import { useCompanyDetailStore } from '@/store/companyDetail'
 
 // Utilities
 import { computed } from 'vue'
+
+// Types
 import type { DetailCompany } from '@/types/invest'
 
 const companyDetailStore = useCompanyDetailStore()
@@ -17,10 +18,7 @@ const loading = computed(() => companyDetailStore.fetchingCompany)
 
 const totalIdeas = computed(() => company.value.analyst_ideas.length)
 
-function humanizeFinancial(
-  val: number = 0,
-  currencySymbol: string = ''
-): string {
+function humanize(val: number = 0, currencySymbol: string = ''): string {
   if (val === 0) return 'n/a'
   for (const unit of ['', 't', 'M', 'B', 'T']) {
     if (Math.abs(val) < 1000) return currencySymbol + val.toFixed(2) + unit
@@ -31,138 +29,101 @@ function humanizeFinancial(
 </script>
 
 <template>
-  <div class="detail-header__info-panel" v-if="company.price_data">
-    <v-skeleton-loader
-      v-if="loading"
-      type="text"
-      width="100"
-      max-height="44"
-    />
-    <CompanyDetailHeaderInfoItem v-else key="info-price">
-      <template v-slot:title><p>Last price</p></template>
-      <template v-slot:value>
-        <span>
-          {{ company.formatting.primaryCurrencySymbol
-          }}{{ company.price_data.last_price?.toFixed(2) }}
-        </span>
+  <v-row
+    no-gutters
+    style="font-size: 0.75rem"
+    class="mb-1"
+    align-content="center"
+  >
+    <v-col cols="1" class="d-flex flex-column">
+      <v-skeleton-loader v-if="loading" type="text" max-height="40" />
+      <template v-else>
+        <span class="text-uppercase opacity-70">Last Price</span>
+        <span>{{
+          `${company.formatting.primaryCurrencySymbol}${company.price_data.last_price?.toFixed(2)}`
+        }}</span>
       </template>
-    </CompanyDetailHeaderInfoItem>
-
-    <v-skeleton-loader
-      v-if="loading"
-      type="text"
-      width="100"
-      max-height="44"
-    />
-    <CompanyDetailHeaderInfoItem v-else key="info-cap">
-      <template v-slot:title><p>Market cap</p></template>
-      <template v-slot:value>
-        <span>
-          {{
-            humanizeFinancial(
-              company.price_data.capitalisation,
-              company.formatting.primaryCurrencySymbol
-            )
-          }}
-        </span>
+    </v-col>
+    <v-col cols="1" class="d-flex flex-column">
+      <v-skeleton-loader v-if="loading" type="text" max-height="40" />
+      <template v-else>
+        <span class="text-uppercase opacity-70">Market cap</span>
+        <span>{{
+          humanize(
+            company.price_data.capitalisation,
+            company.formatting.primaryCurrencySymbol
+          )
+        }}</span>
       </template>
-    </CompanyDetailHeaderInfoItem>
-
-    <v-skeleton-loader
-      v-if="loading"
-      type="text"
-      width="100"
-      max-height="44"
-    />
-    <CompanyDetailHeaderInfoItem v-else key="info-7D">
-      <template v-slot:title><p>7D</p></template>
-      <template v-slot:value>
+    </v-col>
+    <v-col cols="1" class="d-flex flex-column">
+      <v-skeleton-loader v-if="loading" type="text" max-height="40" />
+      <template v-else>
+        <span class="text-uppercase opacity-70">7D</span>
         <span
           :class="[
             company.return_7d && company.return_7d > 0
-              ? 'detail-header__success-color'
-              : 'detail-header__error-color',
+              ? 'text-success'
+              : 'text-error',
           ]"
+          >{{ `${company.return_7d?.toFixed(2)}%` }}</span
         >
-          {{ company.return_7d?.toFixed(2) }}%
-        </span>
       </template>
-    </CompanyDetailHeaderInfoItem>
-
-    <v-skeleton-loader
-      v-if="loading"
-      type="text"
-      width="100"
-      max-height="44"
-    />
-    <CompanyDetailHeaderInfoItem v-else key="info-1Y">
-      <template v-slot:title><p>1Y</p></template>
-      <template v-slot:value>
+    </v-col>
+    <v-col cols="1" class="d-flex flex-column">
+      <v-skeleton-loader v-if="loading" type="text" max-height="40" />
+      <template v-else>
+        <span class="text-uppercase opacity-70">1Y</span>
         <span
           :class="[
             company.return_1y && company.return_1y > 0
-              ? 'detail-header__success-color'
-              : 'detail-header__error-color',
+              ? 'text-success'
+              : 'text-error',
           ]"
+          >{{ `${company.return_1y && company.return_1y.toFixed(2)}%` }}</span
         >
-          {{ company.return_1y && company.return_1y.toFixed(2) }}%
-        </span>
       </template>
-    </CompanyDetailHeaderInfoItem>
-
-    <CompanyDetailHeaderInfoItem key="info-chart">
-      <div
+    </v-col>
+    <v-col class="d-flex justify-center">
+      <v-progress-circular v-if="loading" indeterminate color="info" />
+      <small-price-chart v-else />
+    </v-col>
+    <v-col cols="2" class="d-flex align-center">
+      <span class="text-uppercase opacity-70 mr-1">Updated</span>
+      <v-skeleton-loader
         v-if="loading"
-        class="w-100 d-flex justify-center align-center bg-surface"
-        style="height: 44px"
-      >
-        <v-progress-circular indeterminate />
-      </div>
-      <SmallPriceChart v-else />
-    </CompanyDetailHeaderInfoItem>
-
-    <CompanyDetailHeaderInfoItem nowrap small key="info-updated">
-      <template v-slot:title><p>Updated</p></template>
-      <template v-slot:value>
-        <v-skeleton-loader v-if="loading" type="text" />
-        <span v-else>
-          <template v-if="company.reports.length">{{
-            company.reports[0]?.updated
-          }}</template>
-          <template v-else>n/a</template>
-        </span>
-      </template>
-    </CompanyDetailHeaderInfoItem>
-
-    <CompanyDetailHeaderInfoItem nowrap on-row small key="info-data">
-      <template v-slot:title><p>Data</p></template>
-      <template v-slot:value>
-        <v-skeleton-loader v-if="loading" type="text" width="150" />
-        <span v-else class="detail-header__info-panel-total-ideas d-flex">
-          <span>Financials</span>
-          <template v-if="totalIdeas > 0">
-            <span class="mx-1">+</span>
-            <CompanyDetailHeaderAnalystsButtonModalMenuContainer />
-          </template>
-        </span>
-      </template>
-    </CompanyDetailHeaderInfoItem>
-  </div>
+        type="text"
+        max-height="40"
+        width="100"
+      />
+      <span v-else>
+        <template v-if="company.reports.length">{{
+          company.reports[0]?.updated
+        }}</template>
+        <template v-else>n/a</template>
+      </span>
+    </v-col>
+    <v-col cols="2" class="d-flex align-center">
+      <span class="text-uppercase opacity-70 mr-1">Data</span>
+      <v-skeleton-loader
+        v-if="loading"
+        type="text"
+        max-height="40"
+        width="150"
+      />
+      <span v-else class="d-flex align-end">
+        <span>Financials</span>
+        <v-btn
+          class="text-capitalize text-decoration-underline"
+          style="margin-bottom: -1px; margin-left: -4px"
+          variant="text"
+          size="small"
+          density="comfortable"
+        >
+          <company-detail-header-analysts-dialog v-if="totalIdeas > 0" />
+          {{ `+ ${totalIdeas} Analysts` }}
+        </v-btn>
+      </span>
+    </v-col>
+  </v-row>
 </template>
-
-<style lang="scss" scoped>
-.detail-header__info-panel {
-  display: grid;
-  grid-template: 44px / repeat(4, min-content) 2fr 1fr 1fr;
-  gap: 16px;
-}
-.detail-header__info-panel-total-ideas {
-  align-items: flex-end;
-}
-.detail-header__error-color {
-  color: rgb(var(--v-theme-success));
-}
-.detail-header__success-color {
-  color: rgb(var(--v-theme-error));
-}
-</style>
