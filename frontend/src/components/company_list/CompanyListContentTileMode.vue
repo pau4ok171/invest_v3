73108e78a34198effeadb1b74ed2863f1555d224
Pau4ok <pavel_mal99@mail.ru>
2025-04-15 13:33:02 +0300

    create tileMode of content in new component

---
+++ frontend/src/components/company_list/CompanyListContentTileMode.vue
@@ -0,0 +1,218 @@
+<script setup lang="ts">
+// Components
+import SnowflakeChart from '@/components/charts/SnowflakeChart.vue'
+
+// Composables
+import { useCompanyListStore } from '@/store/companyList/companyList'
+import { useAuthStore } from '@/store/auth'
+
+// Types
+import type { PropType } from 'vue'
+import type { CompanyItem } from '@/store/companyList/types'
+
+const props = defineProps({
+  items: {
+    type: Object as PropType<CompanyItem[]>,
+  },
+})
+
+const store = useCompanyListStore()
+const authStore = useAuthStore()
+</script>
+
+<template>
+  <v-row style="max-width: 1200px">
+    <v-col v-for="item in items" :key="item.uid" cols="3">
+      <v-hover #="{ isHovering, props }">
+        <v-card v-bind="props" :to="item.to" rounded="lg">
+          <div class="company-list-tile-content" v-show="!isHovering">
+            <v-card-item
+              style="height: 70px"
+              :title="item.companyName"
+              :subtitle="item.marketCap"
+            >
+              <template #append>
+                <v-btn
+                  :icon="item.watchlisted ? '$ratingFull' : '$ratingEmpty'"
+                  variant="text"
+                  size="small"
+                  rounded="lg"
+                  :disabled="!authStore.isAuthenticated"
+                  @click.prevent.stop="store.toggleWatchlisted(item)"
+                />
+              </template>
+            </v-card-item>
+
+            <v-card-item style="height: 185px"
+              ><snowflake-chart
+                :data="item.snowflake"
+                size="154"
+                :interactive="false"
+            /></v-card-item>
+
+            <v-card-item style="height: 65px">
+              <v-row no-gutters>
+                <v-col
+                  ><div
+                    class="d-flex justify-center text-caption text-medium-emphasis"
+                  >
+                    {{ item.ticker }}
+                  </div></v-col
+                >
+                <v-col
+                  ><div
+                    class="d-flex justify-center text-caption text-medium-emphasis"
+                  >
+                    7D
+                  </div></v-col
+                >
+                <v-col
+                  ><div
+                    class="d-flex justify-center text-caption text-medium-emphasis"
+                  >
+                    1Y
+                  </div></v-col
+                >
+              </v-row>
+              <v-row no-gutters>
+                <v-col
+                  ><div class="d-flex justify-center">
+                    <v-chip label size="small">{{ item.lastPrice }}</v-chip>
+                  </div></v-col
+                >
+                <v-col
+                  ><div class="d-flex justify-center">
+                    <v-chip
+                      :color="
+                        !item.return7D.startsWith('-') ? 'success' : 'error'
+                      "
+                      label
+                      size="small"
+                      >{{ item.return7D }}</v-chip
+                    >
+                  </div></v-col
+                >
+                <v-col
+                  ><div class="d-flex justify-center">
+                    <v-chip
+                      :color="
+                        !item.return1Y.startsWith('-') ? 'success' : 'error'
+                      "
+                      label
+                      size="small"
+                      >{{ item.return1Y }}</v-chip
+                    >
+                  </div></v-col
+                >
+              </v-row>
+            </v-card-item>
+          </div>
+          <div class="company-list-tile-content" v-show="isHovering">
+            <v-card-item style="height: 70px">
+              <template #prepend>
+                <v-avatar
+                  :image="item.companyLogo"
+                  variant="tonal"
+                  size="small"
+                  rounded="lg"
+                />
+              </template>
+              <template #append>
+                <v-chip
+                  class="mr-2"
+                  color="info"
+                  label
+                  size="x-small"
+                  :text="item.sector"
+                />
+                <v-btn
+                  :icon="item.watchlisted ? '$ratingFull' : '$ratingEmpty'"
+                  variant="text"
+                  size="small"
+                  rounded="lg"
+                  :disabled="!authStore.isAuthenticated"
+                  @click.prevent.stop="store.toggleWatchlisted(item)"
+                />
+              </template>
+            </v-card-item>
+            <v-card-text style="height: 185px" class="text-caption">
+              <span class="company-list-tile-content__description">{{
+                item.description
+              }}</span>
+            </v-card-text>
+
+            <v-card-item style="height: 65px; font-size: 0.625rem">
+              <v-row no-gutters>
+                <v-col
+                  ><div
+                    class="d-flex justify-center text-caption text-medium-emphasis"
+                  >
+                    P/E
+                  </div></v-col
+                >
+                <v-col
+                  ><div
+                    class="d-flex justify-center text-caption text-medium-emphasis"
+                  >
+                    Growth
+                  </div></v-col
+                >
+                <v-col
+                  ><div
+                    class="d-flex justify-center text-caption text-medium-emphasis"
+                  >
+                    Target
+                  </div></v-col
+                >
+              </v-row>
+              <v-row no-gutters>
+                <v-col
+                  ><div class="d-flex justify-center">
+                    <v-chip label size="small">10.0x</v-chip>
+                  </div></v-col
+                >
+                <v-col
+                  ><div class="d-flex justify-center">
+                    <v-chip label size="small">10.0%</v-chip>
+                  </div></v-col
+                >
+                <v-col
+                  ><div class="d-flex justify-center">
+                    <v-chip label size="small">RUB2500</v-chip>
+                  </div></v-col
+                >
+              </v-row>
+            </v-card-item>
+          </div>
+        </v-card>
+      </v-hover>
+    </v-col>
+    <v-col
+      v-if="store.fetching"
+      v-for="i in 20"
+      :key="`tile-item-skeleton-${i}`"
+    >
+      <v-card width="276" height="320" rounded="lg">
+        <v-skeleton-loader
+          loading
+          type="article@2"
+          width="100%"
+          height="100%"
+          color="surface"
+        />
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped lang="scss">
+.company-list-tile-content {
+  .company-list-tile-content__description {
+    display: -webkit-box;
+    -webkit-line-clamp: 8;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+</style>
