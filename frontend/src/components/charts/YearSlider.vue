397059f03b6cbfde069404726e6ee84d1c486efe
Pau4ok <pavel_mal99@mail.ru>
2025-04-25 16:15:39 +0300

    create yearSlider with clickHandler

---
+++ frontend/src/components/charts/YearSlider.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+// Utilities
+import { computed, ref } from 'vue'
+import { indexOf } from 'lodash'
+
+const props = defineProps({
+  maxYear: {
+    type: Number,
+    required: true,
+  },
+  minYear: {
+    type: Number,
+    required: true,
+  },
+  modelValue: {
+    type: Number,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const trackRef = ref<HTMLElement | null>(null)
+const selectedYear = ref<number>(props.modelValue)
+
+const values: number[] = []
+for (let i = props.minYear; i <= props.maxYear; i++) {
+  values.push(i)
+}
+
+const left = computed(() => {
+  const index = indexOf(values, selectedYear.value)
+  const step = trackRef.value ? trackRef.value.clientWidth / 12 : null
+
+  if (index < 0 || step == null) return '0px'
+
+  return `${index * step}px`
+})
+
+const handleClick = (year: number) => {
+  selectedYear.value = year
+  emit('update:modelValue', selectedYear.value)
+}
+</script>
+
+<template>
+  <div class="year-slider">
+    <div ref="trackRef" class="year-slider__track">
+      <div class="year-slider__track-chart"></div>
+      <div
+        v-for="(year, i) in values"
+        :key="`year-${year}`"
+        class="w-100 h-100 d-flex align-center justify-center"
+        @click="handleClick(year)"
+      >
+        <p
+          :class="[
+            'year-slider__track-year',
+            {
+              'year-slider__track-year--active': selectedYear === year,
+            },
+          ]"
+        >
+          {{ i % 2 === 0 || selectedYear === year ? year : '' }}
+        </p>
+      </div>
+    </div>
+    <div class="year-slider__thumb" :style="{ left: left }" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.year-slider {
+  position: relative;
+  height: 40px;
+  user-select: none;
+  cursor: pointer;
+  appearance: none;
+  -moz-appearance: none;
+
+  &__track {
+    position: absolute;
+    left: 0;
+    right: 0;
+    display: grid;
+    width: 100%;
+    height: 40px;
+    background-color: rgb(var(--v-theme-surface-bright));
+    border-radius: 8px;
+    grid-template-columns: repeat(12, 1fr);
+    place-items: center;
+    -webkit-box-align: center;
+  }
+  &__track-chart {
+    position: absolute;
+    width: 100%;
+  }
+  &__track-year {
+    font-size: 0.75rem;
+    margin: 0;
+    line-height: 1.5;
+    color: rgb(var(--v-theme-on-surface-bright));
+
+    &--active {
+      font-weight: bold;
+      font-size: 0.875rem;
+    }
+  }
+  &__thumb {
+    position: absolute;
+    touch-action: none;
+    z-index: 1;
+    will-change: left;
+    cursor: grab;
+    width: calc(8.33333%);
+    height: 50px;
+    top: -5px;
+    border-radius: 6px;
+    background-color: rgba(var(--v-theme-hc-series1-color), 0.2);
+  }
+}
+</style>
