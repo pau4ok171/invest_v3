151589bc43561e1adda6c84f8f32bc8afe617f40
Pau4ok <pavel_mal99@mail.ru>
2025-04-29 10:37:34 +0300

    fix error with locales in vuetify

--- frontend/src/vuetify/index.ts
+++ frontend/src/vuetify/index.ts
@@ -12,6 +12,9 @@ import { themes } from '@/vuetify/themes'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import { aliases as iAliases } from '@/vuetify/iconsets/iIcons/i-svg'
 
+// Locales
+import { ru, fr, de, es } from 'vuetify/locale'
+
 export const vuetify = createVuetify({
   components: {
     VFileUpload,
@@ -22,6 +25,11 @@ export const vuetify = createVuetify({
       'finargo-dark': themes.finargoDarkTheme,
       'finargo-light': themes.finargoLightTheme,
     },
+  },
+  locale: {
+    locale: 'ru',
+    fallback: 'en',
+    messages: { ru, fr, de, es },
   },
   icons: {
     aliases: {
