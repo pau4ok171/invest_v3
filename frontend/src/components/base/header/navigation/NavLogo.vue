ccd184a4727139bb87e88d73b0cd8c4cbfcfc1bc
Pau4ok <pavel_mal99@mail.ru>
2025-05-11 01:29:45 +0300

    create new logo and make it responsive

--- frontend/src/components/base/header/navigation/NavLogo.vue
+++ frontend/src/components/base/header/navigation/NavLogo.vue
@@ -1,25 +1,23 @@
+<script setup lang="ts">
+// Components
+import Logo from '@/components/icons/Logo.vue'
+import SmallLogo from '@/components/icons/SmallLogo.vue'
+
+// Composables
+import { useDisplay } from 'vuetify'
+
+const { mdAndDown } = useDisplay()
+</script>
+
 <template>
-<div class="navigation__logo">
-  <RouterLink
-    :key="`home`"
-    :to="{ name: `home` }"
-  >
-    Finargo
-  </RouterLink>
-</div>
+  <div class="navigation__logo h-100">
+    <RouterLink
+      :key="`home`"
+      :to="{ name: `home` }"
+      class="d-flex align-center h-100 w-100"
+    >
+      <small-logo style="height: 32px" v-if="mdAndDown" />
+      <logo style="height: 32px" v-else />
+    </RouterLink>
+  </div>
 </template>
-
-<style lang="scss">
-.navigation__logo {
-  font-size: 30px;
-  font-weight: 900;
-  user-select: none;
-  color: rgb(var(--v-theme-on-background));
-  text-decoration: none;
-
-  > a {
-    text-decoration: none;
-    color: inherit;
-  }
-}
-</style>
---
+++ frontend/src/components/icons/SmallLogo.vue
@@ -0,0 +1,50 @@
+<template>
+  <svg
+    class="img-fluid"
+    id="outputsvg"
+    xmlns="http://www.w3.org/2000/svg"
+    style="transform-origin: 50% 50%;"
+    viewBox="0 0 650 770"
+  >
+    <g
+      id="l5ZI0xWCn5M2xNgpBI95cIg"
+      fill="rgb(105,90,186)"
+      style="transform: none"
+    >
+      <g>
+        <path
+          id="pbO67Z5h2"
+          d="M0 170 l0 -170 325 0 325 0 0 60 c0 33 -1 61 -2 62 -2 1 -122 4 -268 7 l-265 6 -3 103 -3 102 -54 0 -55 0 0 -170z"
+        ></path>
+      </g>
+    </g>
+    <g
+      id="l3wOcMHLHSMUlTUqTyhnW6A"
+      fill="rgb(8,192,254)"
+      style="transform: none"
+    >
+      <g>
+        <path
+          id="p1DSF31bbh"
+          d="M0 561 c0 -137 3 -170 14 -174 8 -3 101 -5 208 -4 l193 2 0 65 0 65 -147 3 -148 3 0 72 c0 39 -4 77 -8 82 -8 12 -90 55 -103 55 -5 0 -9 -75 -9 -169z"
+        ></path>
+      </g>
+    </g>
+    <g
+      id="l6BPQmtJrerppxDgnDab1yi"
+      fill="rgb(79,158,196)"
+      style="transform: none"
+    >
+      <g>
+        <path
+          id="pX6mZ4Dis"
+          d="M262 513 l147 -4 4 -49 4 -50 -1 52 -1 53 -150 0 -150 1 147 -3z"
+        ></path>
+        <path
+          id="pE040SaTh"
+          d="M150 276 c0 -49 4 -65 16 -70 9 -3 93 -3 188 1 l172 6 0 61 -1 61 -187 3 -188 2 0 -64z"
+        ></path>
+      </g>
+    </g>
+  </svg>
+</template>
