602ec9496d116abbda0572655c72ca628a9bc2ee
Pau4ok <pavel_mal99@mail.ru>
2025-05-11 22:32:07 +0300

    move static declarations in separated file

---
+++ frontend/src/assets/static/companyDetail.ts
@@ -0,0 +1,11 @@
+export const SECTIONS = [
+  { value: 'overview', title: 'Company Overview', shortTitle: 'overview' },
+  { value: 'value', title: '1 Valuation', shortTitle: 'valuation' },
+  { value: 'future', title: '2 Future Growth', shortTitle: 'future' },
+  { value: 'past', title: '3 Past Performance', shortTitle: 'past' },
+  { value: 'health', title: '4 Financial Health', shortTitle: 'health' },
+  { value: 'dividend', title: '5 Dividend', shortTitle: 'dividend' },
+  { value: 'management', title: '6 Management', shortTitle: 'management' },
+  { value: 'owners', title: '7 Ownership', shortTitle: 'ownership' },
+  { value: 'other', title: 'Other Information', shortTitle: 'information' },
+]
