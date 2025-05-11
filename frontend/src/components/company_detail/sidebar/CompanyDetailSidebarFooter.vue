2e3f1a95f42a72c9513a0c3ab0e0c77a3a0f8f3d
Pau4ok <pavel_mal99@mail.ru>
2025-05-11 22:31:16 +0300

    make responsive

--- frontend/src/components/company_detail/sidebar/CompanyDetailSidebarFooter.vue
+++ frontend/src/components/company_detail/sidebar/CompanyDetailSidebarFooter.vue
@@ -1,79 +1,18 @@
 <script setup lang="ts">
+// Composables
+import { useSectionContext } from '@/composables/sectionObserver'
+
 // Utilities
-import { onMounted, ref } from 'vue'
+import { SECTIONS } from '@/assets/static/companyDetail'
 
-const sections = ref([
-  { value: 'overview', title: 'Company Overview' },
-  { value: 'value', title: '1 Valuation' },
-  { value: 'future', title: '2 Future Growth' },
-  { value: 'past', title: '3 Past Performance' },
-  { value: 'health', title: '4 Financial Health' },
-  { value: 'dividend', title: '5 Dividend' },
-  { value: 'management', title: '6 Management' },
-  { value: 'owners', title: '7 Ownership' },
-  { value: 'other', title: 'Other Information' },
-])
-const activeSection = ref(['overview'])
-const prevSection = ref('other')
-const nextSection = ref('value')
-
-function changeSection(section: unknown) {
-  if (!Array.isArray(section)) return
-
-  activeSection.value[0] = section[0]
-  prevSection.value = section[0]
-  nextSection.value = section[0]
-  const goTo = (document as any)
-    .querySelector(`#${section[0]}`)
-    .getBoundingClientRect()
-  const windowScroll = document.documentElement.scrollTop
-  const scrollTop = goTo.top + windowScroll - 80
-  window.scrollTo({ top: scrollTop })
-}
-
-onMounted(() => {
-  const observer = new IntersectionObserver((entries) => {
-    const entry = entries[0]
-    if (entry.intersectionRatio > 0) {
-      prevSection.value =
-        entry.boundingClientRect.top >= 0
-          ? activeSection.value[0]
-          : prevSection.value
-      activeSection.value[0] = entry.target.getAttribute('id') as string
-      prevSection.value =
-        entry.boundingClientRect.top < 0
-          ? activeSection.value[0]
-          : prevSection.value
-      nextSection.value =
-        entry.boundingClientRect.top >= 0
-          ? activeSection.value[0]
-          : nextSection.value
-    } else {
-      if (entry.boundingClientRect.top < 0) {
-        activeSection.value[0] = nextSection.value
-      } else {
-        activeSection.value[0] = prevSection.value
-      }
-    }
-  })
-
-  document.querySelectorAll('.section_observer').forEach(
-    (section) => {
-      observer.observe(section)
-    },
-    {
-      rootMargin: '-50% 0px -50% 0px',
-      threshold: [0.1],
-    }
-  )
-})
+const { activeSection, changeSection } = useSectionContext()
 </script>
 
 <template>
   <v-list
-    :items="sections"
-    v-model:selected="activeSection"
+    :selected="[activeSection]"
     @update:selected="changeSection"
+    :items="SECTIONS"
     density="compact"
     bg-color="background"
   />
