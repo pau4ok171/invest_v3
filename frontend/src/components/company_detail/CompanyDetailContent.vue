88cc6cc33405163881b64292e8f52c6db85e8af9
Pau4ok <pavel_mal99@mail.ru>
2025-05-11 22:29:14 +0300

    make responsive

--- frontend/src/components/company_detail/CompanyDetailContent.vue
+++ frontend/src/components/company_detail/CompanyDetailContent.vue
@@ -4,16 +4,39 @@ import CompanyDetailSidebar from '@/components/company_detail/sidebar/CompanyDet
 import CompanyDetailContentList from '@/components/company_detail/content_list/CompanyDetailContentList.vue'
 
 // Composables
-import { useDisplay } from 'vuetify'
+import { useDisplay, useLayout } from 'vuetify'
+import { provideSectionContext } from '@/composables/sectionObserver'
+
+// Utilities
+import { SECTIONS } from '@/assets/static/companyDetail'
+
+const layout = useLayout()
+const { activeSection, changeSection } = provideSectionContext(layout)
 
 const { lgAndUp } = useDisplay()
 </script>
 
 <template>
   <div class="detail__content">
-    <CompanyDetailSidebar v-if="lgAndUp" />
+    <v-app-bar v-if="!lgAndUp">
+      <v-tabs
+        :model-value="[activeSection]"
+        @update:model-value="(section) => changeSection([section as string])"
+        selected-class="text-info"
+      >
+        <v-tab
+          v-for="section in SECTIONS"
+          :key="`section-tab-${section.value}`"
+          class="text-body-2 text-capitalize"
+          :text="section.shortTitle"
+          :value="section.value"
+        />
+      </v-tabs>
+    </v-app-bar>
 
-    <CompanyDetailContentList />
+    <company-detail-sidebar v-else />
+
+    <company-detail-content-list />
   </div>
 </template>
 
