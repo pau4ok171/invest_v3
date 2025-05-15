1bf97f49ade920c6b7b7a7765a9da118ba225d87
Pau4ok <pavel_mal99@mail.ru>
2025-05-15 12:48:40 +0300

    make responsive

--- frontend/src/components/base/header/TheHeader.vue
+++ frontend/src/components/base/header/TheHeader.vue
@@ -6,17 +6,23 @@ import NavSearch from '@/components/base/header/navigation/NavSearch.vue'
 import NavLang from '@/components/base/header/navigation/NavLang.vue'
 import NavUser from '@/components/base/header/navigation/NavUser.vue'
 import NavLogo from '@/components/base/header/navigation/NavLogo.vue'
+import NavMobileMenu from "@/components/base/header/navigation/NavMobileMenu.vue";
+
+// Composables
+import {useDisplay} from "vuetify";
+
+const { smAndDown } = useDisplay()
 </script>
 
 <template>
   <v-app-bar class="header px-md-3" color="background" flat :extended="false">
     <template #prepend>
-      <nav-logo />
-      <div class="px-1"></div>
+      <nav-logo class="pr-1" />
+      <nav-mobile-menu class="pr-1" v-if="smAndDown" />
       <nav-search />
     </template>
     <template #append>
-      <nav-menu />
+      <nav-menu v-if="!smAndDown" />
       <v-divider vertical inset class="mx-2 my-auto" length="16" />
       <nav-lang />
       <nav-theme />
---
+++ frontend/src/components/base/header/navigation/NavMobileMenu.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// Utilities
+import { RoutesHeaderEnum } from '@/router/routes.types'
+</script>
+
+<template>
+  <v-menu>
+    <template #activator="{ props }">
+      <v-app-bar-nav-icon v-bind="props" />
+    </template>
+
+    <v-list>
+      <v-list-item
+        v-for="routeName in RoutesHeaderEnum"
+        :key="routeName"
+        :to="{ name: routeName }"
+        variant="text"
+      >
+        <template #title>
+          <div class="text-capitalize">{{ routeName }}</div>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
