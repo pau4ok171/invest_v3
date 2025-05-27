e502bf54fac9d743a0d5f4ba8786e1ce11130701
PourLaRime <107425199+pau4ok171@users.noreply.github.com>
2025-05-27 14:59:37 +0300

    create UserProfileModel

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -1,18 +1,37 @@
+// Utilities
 import { defineStore } from 'pinia'
 import axios from 'axios'
+
+// Types
+import type { Portfolio } from '@/types/portfolios'
+
+export interface Profile {
+  id: string
+  name: string
+  email: string
+  first_name: string
+  last_name: string
+  avatar: string | null
+  locale: string
+  register_date: number
+  country_iso: string | null
+  currency: string | null
+  auth_provider: string
+  display_name: string | null
+  portfolios: Portfolio[]
+  watchlist: string[]
+  stock_view: string
+  is_staff: boolean
+}
+
+export type AuthMode = 'login' | 'registration'
 
 export const useAuthStore = defineStore({
   id: 'auth',
   state: () => ({
-    userInfo: {} as {
-      first_name: string
-      is_anonymous: boolean
-      is_staff: boolean
-      last_name: string
-      stage_in_days: number
-    },
     token: '',
-    hasAccount: true,
+    authMode: 'login' as AuthMode,
+    profile: {} as Profile,
   }),
   getters: {
     isAuthenticated: (state) => {
@@ -20,19 +39,39 @@ export const useAuthStore = defineStore({
     },
   },
   actions: {
-    init() {
-      const token = localStorage.getItem('token')
-      if (token) {
-        this.token = token
+    setToken(newToken: string | null = null) {
+      if (newToken) {
+        this.token = newToken
+        axios.defaults.headers.common['Authorization'] = `Token ${newToken}`
+        localStorage.setItem('token', newToken)
       } else {
-        this.token = ''
+        const token = localStorage.getItem('token')
+        this.token = token || ''
+        axios.defaults.headers.common['Authorization'] = `Token ${this.token}`
       }
     },
-    async fetchUserInfo() {
-      await axios
-        .get('api/v1/admin/users/')
-        .then((r) => (this.userInfo = r.data))
-        .catch((e) => console.log(e))
+    async login(token: string) {
+      this.setToken(token)
+      await this.fetchUserProfile()
+    },
+    logout() {
+      axios.defaults.headers.common['Authorization'] = ''
+
+      localStorage.removeItem('token')
+
+      this.token = ''
+      this.profile = {} as Profile
+    },
+    async fetchUserProfile() {
+      if (!this.token) return
+
+      try {
+        const response = await axios.get('api/v1/profile/me/')
+
+        this.profile = response.data
+      } catch (error) {
+        console.log(error)
+      }
     },
   },
 })
