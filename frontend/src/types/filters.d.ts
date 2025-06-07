9986b95c4933a82920fbbad0e94075bca15f4346
PourLaRime <107425199+pau4ok171@users.noreply.github.com>
2025-06-07 21:16:30 +0300

    delete unused types

--- frontend/src/types/filters.d.ts
+++
@@ -1,5 +0,0 @@
-export interface dropDownItem {
-    title: string,
-    slug: string,
-    isChosen: boolean,
-}
--- frontend/src/types/section.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Section {
-    num: number,
-    name: string,
-    desc: string,
-    area: string,
-}
