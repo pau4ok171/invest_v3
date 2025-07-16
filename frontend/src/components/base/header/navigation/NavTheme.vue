0a54a11bb6c394427a3cbe0deea781910bdbdf00
pau4ok <pau4ok17166@gmail.com>
2025-07-16 18:02:10 +0300

    refactor theme setting and control

--- frontend/src/components/base/header/navigation/NavTheme.vue
+++ frontend/src/components/base/header/navigation/NavTheme.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
 // Composables
 import { useTheme } from 'vuetify'
+import { useAuthStore } from '@/store/auth'
 
 // Utilities
 import { computed } from 'vue'
 
 const theme = useTheme()
-const isDarkTheme = computed(() => theme.global.name.value === 'finargo-dark')
-const changeThemeMode = () =>
-  (theme.global.name.value = isDarkTheme.value
-    ? 'finargo-light'
-    : 'finargo-dark')
+const authStore = useAuthStore()
+const isDarkTheme = computed(() => theme.name.value === 'finargo-dark')
+
+theme.change(
+  authStore.profile?.theme || localStorage.getItem('theme') || 'finargo-light'
+)
+
+const changeThemeMode = async () => {
+  const newTheme = isDarkTheme.value ? 'finargo-light' : 'finargo-dark'
+  theme.change(newTheme)
+  localStorage.setItem('theme', newTheme)
+
+  try {
+    if (authStore.isAuthenticated && authStore.profile) {
+      await authStore.patchProfile({ theme: newTheme })
+    }
+  } catch (error) {
+    console.error('Failed to change theme:', error)
+  }
+}
 </script>
 
 <template>
