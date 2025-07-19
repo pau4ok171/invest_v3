ea4eddcbe25a6794684f3d69910dbf8d6db3e383
pau4ok <pau4ok17166@gmail.com>
2025-07-20 01:26:41 +0300

    use CASCADE instead

--- backend/core/portfolio/models.py
+++ backend/core/portfolio/models.py
@@ -7,7 +7,7 @@ class Portfolio(models.Model):
     name = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     positions = models.ManyToManyField(invest_models.Company, through='PortfolioCompany')
 
     def __str__(self):
@@ -15,8 +15,8 @@ class Portfolio(models.Model):
 
 
 class PortfolioCompany(models.Model):
-    company = models.ForeignKey(invest_models.Company, on_delete=models.PROTECT)
-    portfolio = models.ForeignKey(Portfolio, on_delete=models.PROTECT)
+    company = models.ForeignKey(invest_models.Company, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
     average_price = models.FloatField(null=True, blank=True)
     currency = models.ForeignKey(invest_models.Currency, on_delete=models.PROTECT, null=True, blank=True)
     share_amount = models.IntegerField(null=True, blank=True)
