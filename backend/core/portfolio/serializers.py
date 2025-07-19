fd7273ec927cc7499d92a50b5022fcbad64daa1e
pau4ok <pau4ok17166@gmail.com>
2025-07-20 01:24:59 +0300

    performe portfolios serializer

--- backend/core/portfolio/serializers.py
+++ backend/core/portfolio/serializers.py
@@ -1,15 +1,53 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
 from portfolio.models import Portfolio
 
+from invest.models import Company
 
-class PortfolioPositionsSerializer(ModelSerializer):
+
+class PortfoliosSerializer(serializers.ModelSerializer):
+    positions = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Company.objects.filter(is_visible=True),
+        required=False
+    )
+
     class Meta:
         model = Portfolio
-        fields = '__all__'
-        depth = 1
+        fields = (
+            'id',
+            'name',
+            'created',
+            'updated',
+            'positions',
+        )
+        extra_kwargs = {
+            'id': { 'read_only': True },
+            'created': { 'read_only': True },
+            'updated': { 'read_only': True },
+        }
 
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
+        positions_data = validated_data.pop('positions', [])
 
-class PortfolioSerializer(ModelSerializer):
-    class Meta:
-        model = Portfolio
-        fields = '__all__'
+        portfolio = Portfolio.objects.create(**validated_data, user=user)
+
+        profile = user.profile
+        profile.portfolios.add(portfolio)
+
+        if positions_data:
+            portfolio.positions.set(positions_data)
+
+        return portfolio
+
+    def update(self, instance, validated_data):
+        positions_data = validated_data.pop('positions', None)
+
+        instance = super().update(instance, validated_data)
+
+        if positions_data is not None:
+            instance.positions.set(positions_data)
+
+        return instance
